ad76cc7b54cb13734b90c997baeb028fe5842d3c
marcopiraccini <marco.piraccini@gmail.com>
2022-10-05 19:29:34 +0200

    feat: nested routes in openapi

--- packages/sql-openapi/test/tap-snapshots/simple-openapi-1.cjs
+++ packages/sql-openapi/test/tap-snapshots/simple-openapi-1.cjs
@@ -35,7 +35,7 @@ Object {
   "paths": Object {
     "/pages/": Object {
       "get": Object {
-        "operationId": "getAllPage",
+        "operationId": "getPages",
         "parameters": Array [
           Object {
             "in": "query",
