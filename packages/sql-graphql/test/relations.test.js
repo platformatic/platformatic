fe4c05eacfadc86b460b9fce838f6c846c14ef18
bacebu4 <vasua14735@icloud.com>
2022-10-16 15:55:38 +0300

    test

--- packages/sql-graphql/test/relations.test.js
+++ packages/sql-graphql/test/relations.test.js
@@ -7,7 +7,7 @@ const { join } = require('path')
 const sqlGraphQL = require('..')
 const sqlMapper = require('@platformatic/sql-mapper')
 const fastify = require('fastify')
-const { isSQLite } = require('./helper')
+const { isSQLite, connInfo, clear } = require('./helper')
 
 if (!isSQLite) {
   skip('The db is not SQLite')
@@ -57,4 +57,160 @@ test('should fail when an unknown foreign key relationship exists', async ({ pas
         `
     }
   }), new Error('No foreign table named "subcategories" was found'))
+})
+
+test('should handle multi references', async ({ pass, teardown, same, equal }) => {
+  const app = fastify()
+  app.register(sqlMapper, {
+    ...connInfo,
+    async onDatabaseLoad (db, sql) {
+      pass('onDatabaseLoad called')
+
+      await clear(db, sql)
+
+      
+      await db.query(sql`
+          CREATE TABLE quotes (
+            id INTEGER PRIMARY KEY,
+            quote TEXT NOT NULL
+          );
+        );`)
+      await db.query(sql`
+          CREATE TABLE movies (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE
+          );
+        );`)
+      await db.query(sql`
+          ALTER TABLE quotes ADD COLUMN movie_id INTEGER REFERENCES movies(id);
+        );`)
+      await db.query(sql`
+          ALTER TABLE quotes ADD COLUMN another_movie_id INTEGER REFERENCES movies(id);
+        );`)
+    }
+  })
+  app.register(sqlGraphQL)
+  teardown(app.close.bind(app))
+
+  await app.ready()
+
+  const movies = [{
+    id: 1,
+    name: 'Harry Potter'
+  }]
+
+  const quotes = [{
+    id: 1,
+    quote: 'Hello Harry',
+    movieId: 1,
+    anotherMovieId: 1
+  }]
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            mutation batch($inputs : [MovieInput]!) {
+              insertMovies(inputs: $inputs) {
+                id
+                name
+              }
+            }
+          `,
+        variables: {
+          inputs: movies
+        }
+      }
+    })
+    equal(res.statusCode, 200, 'movies status code')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            mutation batch($inputs : [QuoteInput]!) {
+              insertQuotes(inputs: $inputs) {
+                id
+                quote
+              }
+            }
+          `,
+        variables: {
+          inputs: quotes
+        }
+      }
+    })
+    equal(res.statusCode, 200, 'quotes status code')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            query {
+              quotes {
+                id
+                movie {
+                  id
+                }
+                anotherMovie {
+                  id
+                }
+              }
+            }
+          `,
+      }
+    })
+    equal(res.statusCode, 200, 'query quotes')
+    same(res.json(), {
+      data: {
+        quotes: [{
+          id: 1,
+          movie: {
+            id: 1,
+          },
+          anotherMovie: {
+            id: 1
+          }
+        }]
+      }
+    }, 'query quote response')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            query {
+              movies {
+                id
+                quotes {
+                  id
+                }
+              }
+            }
+          `,
+      }
+    })
+    equal(res.statusCode, 200, 'query movies')
+    same(res.json(), {
+      data: {
+        movies: [{
+          id: 1,
+          quotes: [{
+            id: 1
+          }]
+        }]
+      }
+    }, 'query movie response')
+  }
 })
