abad507de163f9cdb47c9012947e90b62e875d05
bacebu4 <32079387+bacebu4@users.noreply.github.com>
2022-10-17 08:15:37 +0300

    Fixing #188: Allow multi reference (#191)

--- packages/sql-graphql/test/relations.test.js
+++ packages/sql-graphql/test/relations.test.js
@@ -7,14 +7,12 @@ const { join } = require('path')
 const sqlGraphQL = require('..')
 const sqlMapper = require('@platformatic/sql-mapper')
 const fastify = require('fastify')
-const { isSQLite } = require('./helper')
-
-if (!isSQLite) {
-  skip('The db is not SQLite')
-  process.exit(0)
-}
+const { isSQLite, connInfo, isMysql, clear } = require('./helper')
 
 test('should fail when an unknown foreign key relationship exists', async ({ pass, rejects, same, teardown }) => {
+  if (!isSQLite) {
+    skip('The db is not SQLite')
+  }
   const file = join(tmpdir(), randomUUID())
   const app = fastify()
   app.register(sqlMapper, {
@@ -57,4 +55,187 @@ test('should fail when an unknown foreign key relationship exists', async ({ pas
         `
     }
   }), new Error('No foreign table named "subcategories" was found'))
+})
+
+test('should handle multi references', async ({ pass, teardown, same, equal }) => {
+  const app = fastify()
+  app.register(sqlMapper, {
+    ...connInfo,
+    async onDatabaseLoad (db, sql) {
+      pass('onDatabaseLoad called')
+
+      await clear(db, sql)
+
+      if (isMysql) {
+        await db.query(sql`
+          CREATE TABLE authors (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255)
+          );
+          CREATE TABLE books (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(42),
+            author_id BIGINT UNSIGNED,
+            another_author_id BIGINT UNSIGNED,
+            FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE,
+            FOREIGN KEY (another_author_id) REFERENCES authors(id) ON DELETE CASCADE
+          );
+        `)
+      } else if (isSQLite) {
+        await db.query(sql`
+          CREATE TABLE authors (
+            id INTEGER PRIMARY KEY,
+            name VARCHAR(255)
+          );
+        `)
+        await db.query(sql`
+          CREATE TABLE books (
+            id INTEGER PRIMARY KEY,
+            title VARCHAR(42),
+            author_id BIGINT UNSIGNED,
+            another_author_id BIGINT UNSIGNED,
+            FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE,
+            FOREIGN KEY (another_author_id) REFERENCES authors(id) ON DELETE CASCADE
+          );
+        `)
+      } else {
+        await db.query(sql`
+          CREATE TABLE authors (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(42)
+          );
+          CREATE TABLE books (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(42),
+            author_id INTEGER REFERENCES authors(id),
+            another_author_id INTEGER REFERENCES authors(id)
+          );
+        `)
+      }
+    }
+  })
+  app.register(sqlGraphQL)
+  teardown(app.close.bind(app))
+
+  await app.ready()
+
+  const authors = [{
+    id: 1,
+    name: 'Mark'
+  }]
+
+  const books = [{
+    id: 1,
+    title: 'Harry',
+    authorId: 1,
+    anotherAuthorId: 1
+  }]
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            mutation batch($inputs : [AuthorInput]!) {
+              insertAuthors(inputs: $inputs) {
+                id
+                name
+              }
+            }
+          `,
+        variables: {
+          inputs: authors
+        }
+      }
+    })
+    equal(res.statusCode, 200, 'authors status code')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            mutation batch($inputs : [BookInput]!) {
+              insertBooks(inputs: $inputs) {
+                id
+                title
+              }
+            }
+          `,
+        variables: {
+          inputs: books
+        }
+      }
+    })
+    equal(res.statusCode, 200, 'books status code')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            query {
+              books {
+                id
+                author {
+                  id
+                }
+                anotherAuthor {
+                  id
+                }
+              }
+            }
+          `
+      }
+    })
+    equal(res.statusCode, 200, 'query books')
+    same(res.json(), {
+      data: {
+        books: [{
+          id: 1,
+          author: {
+            id: 1
+          },
+          anotherAuthor: {
+            id: 1
+          }
+        }]
+      }
+    }, 'query book response')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            query {
+              authors {
+                id
+                books {
+                  id
+                }
+              }
+            }
+          `
+      }
+    })
+    equal(res.statusCode, 200, 'query authors')
+    same(res.json(), {
+      data: {
+        authors: [{
+          id: 1,
+          books: [{
+            id: 1
+          }]
+        }]
+      }
+    }, 'query authors response')
+  }
 })
