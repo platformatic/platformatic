7b29a023322ee5796dfc0e38441d2dfde2a84f08
Riccardo Fiorenza <ricfio.professional@gmail.com>
2022-10-29 00:54:19 +0200

    build: add build script for ra-data-rest

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,8 @@ const { exec } = require('child_process')
 
 if (!process.env.CI) {
   console.log('Running dashboard:build script')
-  const child = exec('pnpm run dashboard:build')
-  child.stdout.pipe(process.stdout)
+  exec('pnpm run dashboard:build').stdout.pipe(process.stdout)
+
+  console.log('Running ra-data-rest:build script')
+  exec('pnpm run ra-data-rest:build').stdout.pipe(process.stdout)
 }
