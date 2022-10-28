bb9b2c23ba8840140cf012ba992dc7a32cf87051
Riccardo Fiorenza <ricfio.professional@gmail.com>
2022-10-28 22:44:28 +0200

    feat: add missing ReactAdmin.css

--- packages/db-dashboard/src/pages/ReactAdmin.jsx
+++ packages/db-dashboard/src/pages/ReactAdmin.jsx
@@ -1,6 +1,7 @@
 'use strict'
 import React from 'react'
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import './ReactAdmin.css'
 
 import platformaticDbRestProvider from '@platformatic/db-ra-data-rest'
 
