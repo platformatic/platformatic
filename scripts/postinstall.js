60ba5357cf3df5f2c029ccd0a7b41636a5bd6b03
Riccardo Fiorenza <ricfio.professional@gmail.com>
2022-10-29 01:14:09 +0200

    ci: build ra-data-rests before dashboard

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,9 +2,9 @@
 const { exec } = require('child_process')
 
 if (!process.env.CI) {
-  console.log('Running dashboard:build script')
-  exec('pnpm run dashboard:build').stdout.pipe(process.stdout)
-
   console.log('Running ra-data-rest:build script')
   exec('pnpm run ra-data-rest:build').stdout.pipe(process.stdout)
+
+  console.log('Running dashboard:build script')
+  exec('pnpm run dashboard:build').stdout.pipe(process.stdout)
 }
