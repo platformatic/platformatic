5e22922f92591b6a83dccc2ce0e9eb27d2f5a810
giacomocerquone <cerquone96@hotmail.it>
2022-10-30 18:51:24 +0100

    fix: switch to vite build

--- packages/db-ra-data-rest/vite.config.js
+++ packages/db-ra-data-rest/vite.config.js
@@ -1,10 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // https://github.com/vitejs/vite/issues/6215#issuecomment-1076980852
+  plugins: [react({ jsxRuntime: "classic" })],
   build: {
-    outDir: 'build'
-  }
-})
+    outDir: "dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "db-ra-data-rest",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
+});
