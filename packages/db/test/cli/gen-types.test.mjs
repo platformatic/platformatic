8f13ef80f8c9efc493d14cf585f01972293d27c9
Riccardo Fiorenza <ricfio.professional@gmail.com>
2022-11-05 13:13:24 +0100

    test: add gen-types test for no plugin use case

--- packages/db/test/cli/gen-types.test.mjs
+++ packages/db/test/cli/gen-types.test.mjs
@@ -107,6 +107,32 @@ t.test('run migrate command with type generation', async (t) => {
   t.pass()
 })
 
+t.test('run migrate command with type generation without plugin in config', async (t) => {
+  const testDir = path.join(urlDirname(import.meta.url), '..', 'fixtures', 'auto-gen-types-no-plugin')
+  const cwd = path.join(urlDirname(import.meta.url), '..', 'tmp', 'auto-gen-types-clone-3')
+
+  await mkdir(cwd)
+  await cp(testDir, cwd, { recursive: true })
+
+  t.teardown(async () => {
+    await rm(cwd, { force: true, recursive: true })
+  })
+
+  try {
+    const child = await execa('node', [cliPath, 'migrate'], { cwd })
+    t.equal(child.stdout.includes('Generated type for Graph entity.'), true)
+    t.equal(child.stdout.includes('Please run `npm i --save'), true)
+
+    await execa(pathToTSD, { cwd })
+  } catch (err) {
+    console.log(err.stdout)
+    console.log(err.stderr)
+    t.fail(err.stderr)
+  }
+
+  t.pass()
+})
+
 t.test('missing config file', async ({ equal, match }) => {
   try {
     await execa('node', [cliPath, 'seed'])
@@ -120,7 +146,7 @@ t.test('generate types on start', async ({ plan, equal, teardown, fail, pass }) 
   plan(2)
 
   const testDir = path.join(urlDirname(import.meta.url), '..', 'fixtures', 'auto-gen-types')
-  const cwd = path.join(urlDirname(import.meta.url), '..', 'tmp', 'auto-gen-types-clone-3')
+  const cwd = path.join(urlDirname(import.meta.url), '..', 'tmp', 'auto-gen-types-clone-4')
 
   await mkdir(cwd)
   await cp(testDir, cwd, { recursive: true })
