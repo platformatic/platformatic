05d0ac630334d305b5fd0852d1c1532901027ab9
Riccardo Fiorenza <ricfio.professional@gmail.com>
2022-11-10 01:47:12 +0100

    feat: add create feature in react-admin app

--- packages/db-dashboard/src/pages/ReactAdmin.jsx
+++ packages/db-dashboard/src/pages/ReactAdmin.jsx
@@ -1,12 +1,53 @@
 'use strict'
 import React, { useState, useEffect } from 'react'
 import { Card, CardContent, CardHeader } from '@mui/material'
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin'
+import { Admin, BooleanInput, Create, EditGuesser, ListGuesser, NumberInput, required, Resource, ShowGuesser, SimpleForm, TextInput } from 'react-admin'
 import './ReactAdmin.css'
 
+import camelcase from 'camelcase'
 import { pluralize } from 'inflected'
 
 import platformaticDbRestProvider from '@platformatic/db-ra-data-rest'
+
+const FieldInput = (props) => {
+  const { source, attributes } = props
+
+  const validate = []
+  if (attributes.required) {
+    validate.push(required())
+  }
+
+  switch (attributes.type) {
+    case 'number':
+    case 'integer':
+      return <NumberInput source={source} validate={validate} />
+    case 'boolean':
+      return <BooleanInput source={source} validate={validate} />
+    case 'string':
+      return <TextInput source={source} validate={validate} />
+    default:
+      return <TextInput source={source} validate={validate} />
+  }
+}
+
+const CreateGuesser = (props) => {
+  const schema = props.schema
+  const properties = schema.properties
+  const requiredList = schema.required ?? []
+
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        {Object.entries(properties).map(([fieldname, attributes]) => {
+          attributes.required = (requiredList.indexOf(fieldname) !== -1)
+          return (
+            <FieldInput key={fieldname} source={fieldname} attributes={attributes} />
+          )
+        })}
+      </SimpleForm>
+    </Create>
+  )
+}
 
 function buildDashboard (swagger) {
   return () => (
@@ -17,11 +58,13 @@ function buildDashboard (swagger) {
   )
 }
 
-export default function ReactAdmin (props) {
+const ReactAdmin = (props) => {
+  const { basename, apiUrl, swaggerDocUrl } = props
+
   const [swagger, setSwagger] = useState(false)
   useEffect(() => {
     async function loadSwagger () {
-      const response = await fetch(props.swaggerDocUrl)
+      const response = await fetch(swaggerDocUrl)
       if (response.status === 200) {
         const body = await response.json()
         setSwagger(body)
@@ -33,15 +76,24 @@ export default function ReactAdmin (props) {
     return <h1>React Admin is Loading...</h1>
   }
 
+  const schemas = swagger.components.schemas
   const dashboard = buildDashboard(swagger)
-  const entities = Object.keys(swagger.components.schemas).map((name) => pluralize(name.toLowerCase()))
-  const dataProvider = platformaticDbRestProvider(props.apiUrl)
+  const dataProvider = platformaticDbRestProvider(apiUrl)
 
   return (
-    <Admin basename={props.basename} dataProvider={dataProvider} dashboard={dashboard}>
-      {entities.map((entity) =>
-        <Resource key={entity} name={entity} list={ListGuesser} show={ShowGuesser} edit={EditGuesser} />
+    <Admin basename={basename} dataProvider={dataProvider} dashboard={dashboard}>
+      {Object.keys(schemas).map((entity) =>
+        <Resource
+          key={entity}
+          name={pluralize(camelcase(entity))}
+          list={ListGuesser}
+          show={ShowGuesser}
+          edit={EditGuesser}
+          create={<CreateGuesser schema={schemas[entity]} />}
+        />
       )}
     </Admin>
   )
 }
+
+export default ReactAdmin
