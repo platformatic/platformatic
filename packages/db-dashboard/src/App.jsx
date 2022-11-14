1d2e39d16048e05166dff4e19dd1843d88847edd
Leonardo Rossi <leonardo.rossi@gmail.com>
2022-11-14 14:48:14 +0100

    Turn to tabbed window

--- packages/db-dashboard/src/App.jsx
+++ packages/db-dashboard/src/App.jsx
@@ -1,12 +1,10 @@
-import { Navigate, Routes, Route } from 'react-router-dom'
 import { createContext, useState } from 'react'
-// import Layout from './components/Layout'
-import {Layout} from '@platformatic/ui-components'
-import Home from './pages/Home'
+import {Button, Layout, TabbedWindow} from '@platformatic/ui-components'
 import GraphiQLPage from './pages/GQL'
 import ConfigViewer from './pages/ConfigViewer'
 import SwaggerViewer from './pages/SwaggerViewer'
 import ReactAdmin from './pages/ReactAdmin'
+import Title from './elements/Title'
 
 import '@platformatic/ui-components/dist/main.css'
 const AppContext = createContext({})
@@ -19,6 +17,50 @@ function App () {
   const [logged, setLogged] = useState(false)
   const [adminSecret, setAdminSecret] = useState(null)
   const urlPrefix = import.meta.env.VITE_SERVER_URL || getCurrentUrl()
+
+  const tabs = [
+    {
+      label: 'Main',
+      component: () => <Button onClick={onRestartClicked} primary="true" label="Restart Server"/>
+    },
+    {
+      label: 'Configuration',
+      component: () => <ConfigViewer />
+    },
+    { 
+      label: 'GraphiQL',
+      component: () => <GraphiQLPage graphqlEndpoint={`${urlPrefix}/graphql`} />
+    },
+    { 
+      label: 'Swagger Docs',
+      component: () => <SwaggerViewer swaggerDocUrl={`${urlPrefix}/documentation/json`} />
+    },
+    { 
+      label: 'React Admin',
+      component: () => <ReactAdmin basename='/react-admin' apiUrl={`${urlPrefix}`} swaggerDocUrl={`${urlPrefix}/documentation/json`} />
+    },
+    {
+      label: 'Admin API Docs',
+      component: () => <SwaggerViewer swaggerDocUrl={`${urlPrefix}/_admin/documentation/json`} />
+    }
+  ]
+
+  async function onRestartClicked () {
+    const res = await fetch('/_admin/restart', {
+      method: 'POST'
+    })
+    if (res.status === 200) {
+      notify({
+        message: 'Server restarted',
+        type: 'success'
+      })
+    } else {
+      notify({
+        message: 'There was an error...',
+        type: 'error'
+      })
+    }
+  }
   return (
     <AppContext.Provider
       value={{
@@ -33,7 +75,7 @@ function App () {
     >
       
       <Layout>
-        <Routes>
+        {/* <Routes>
           <Route path='/' exact element={<Navigate to='/dashboard' />} />
           <Route path='/dashboard' element={<Home />} />
           <Route path='/giql' element={<GraphiQLPage graphqlEndpoint={`${urlPrefix}/graphql`} />} />
@@ -41,7 +83,9 @@ function App () {
           <Route path='/swagger-docs' element={<SwaggerViewer swaggerDocUrl={`${urlPrefix}/documentation/json`} />} />
           <Route path='/swagger-plt-db-docs' element={<SwaggerViewer swaggerDocUrl={`${urlPrefix}/_admin/documentation/json`} />} />
           <Route path='/react-admin/*' element={<ReactAdmin basename='/react-admin' apiUrl={`${urlPrefix}`} swaggerDocUrl={`${urlPrefix}/documentation/json`} />} />
-        </Routes>
+        </Routes> */}
+
+        <TabbedWindow tabs={tabs}/>
       </Layout>
     </AppContext.Provider>
   )
--- packages/db-dashboard/src/main.jsx
+++ packages/db-dashboard/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bulma/css/bulma.min.css'
+import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'))
