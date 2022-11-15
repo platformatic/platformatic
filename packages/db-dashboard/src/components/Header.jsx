2d3a11d8b4bddc07af13996b40108a033965a351
Matteo Collina <hello@matteocollina.com>
2022-11-15 17:10:53 +0000

    Update logo in dashboard
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/db-dashboard/src/components/Header.jsx
+++ packages/db-dashboard/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header () {
   return (
     <div className={styles.header}>
       <a href='/dashboard' data-testid='header-home-link' className={styles.brand}>
-        <img className={styles.logo} src='/images/logo-192x192.png' />
+        <img className={styles.logo} src='/images/00_Platformatic_Squared_Logo_Blue_Transparent_300dpi.png' />
       </a>
       {logged && userName && (
         <Button color='red' primary='false' onClick={onLogoutButtonClicked} label={`Logout ${userName}`} />
