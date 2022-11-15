644d01f7014e5dda8d56fcd7bec226b45891f269
Leonardo Rossi <leonardo.rossi@gmail.com>
2022-11-15 09:33:39 +0100

    fixed tests

--- packages/db-dashboard/vitest.config.js
+++ packages/db-dashboard/vitest.config.js
@@ -6,8 +6,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    deps: {
+      // See: https://vitest.dev/config/#deps-inline
+      inline: ['@platformatic/ui-components']
+    },
     globals: true,
     environment: 'happy-dom',
-    exclude: ['**/test/e2e/**', 'node_modules/**', 'src/components/Header.test.jsx']
+    exclude: ['**/test/e2e/**', 'node_modules/**']
   }
 })
