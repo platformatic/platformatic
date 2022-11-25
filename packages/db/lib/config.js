1e3310b567d2437d138b04f91bca51ff5ad714f9
Roberto Bianchi <roberto.bianchi@spendesk.com>
2022-11-25 12:33:29 +0100

    fix(config): Set correct schema for version table

--- packages/db/lib/config.js
+++ packages/db/lib/config.js
@@ -23,10 +23,14 @@ class DBConfigManager extends ConfigManager {
       this.current.core.connectionString = 'sqlite://' + sqliteFullPath
     }
 
+    const versionsTableName = this.current.core.schema.length > 0
+      ? 'public.versions'
+      : 'versions'
+
     // relative-to-absolute migrations path
     if (this.current.migrations) {
       this.current.migrations.dir = this._fixRelativePath(this.current.migrations.dir)
-      this.current.migrations.table = this.current.migrations.table || 'versions'
+      this.current.migrations.table = this.current.migrations.table || versionsTableName
     }
 
     if (this.current.migrations && this.current.core) {
@@ -34,7 +38,7 @@ class DBConfigManager extends ConfigManager {
       /* c8 ignore next 4 */
       this.current.core.ignore = this.current.core.ignore || {}
       this.current.core.ignore = Object.assign({}, {
-        [this.current.migrations.table || 'versions']: true
+        [this.current.migrations.table || versionsTableName]: true
       }, this.current.core.ignore)
     }
   }
