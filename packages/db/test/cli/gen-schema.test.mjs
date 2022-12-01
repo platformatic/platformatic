41c32cbbf97bc2d61eb99ea36dc8d76c6b1732f0
Roberto Bianchi <roberto.bianchi@spendesk.com>
2022-12-01 22:58:00 +0100

    Remove double quotes
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/db/test/cli/gen-schema.test.mjs
+++ packages/db/test/cli/gen-schema.test.mjs
@@ -1,12 +1,12 @@
-import { test } from "tap"
+import { test } from 'tap'
 import fs from 'fs/promises'
-import { generateJsonSchemaConfig } from "../../lib/gen-schema.mjs"
+import { generateJsonSchemaConfig } from '../../lib/gen-schema.mjs'
 
-test("generateJsonSchemaConfig generates the file", async (t) => {
-  process.chdir("./test/tmp")
+test('generateJsonSchemaConfig generates the file', async (t) => {
+  process.chdir('./test/tmp')
   await generateJsonSchemaConfig()
 
-  const configSchema = await fs.readFile("platformatic.db.schema.json", "utf8")
+  const configSchema = await fs.readFile('platformatic.db.schema.json', 'utf8')
   const { required, additionalProperties } = JSON.parse(configSchema)
   t.has(required, ['core', 'server'])
   t.has(additionalProperties, { watch: {} })
