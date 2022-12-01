c42152c4727409d91031f04a9b0dfa120eb9aa95
Roberto Bianchi <roberto.bianchi@spendesk.com>
2022-12-01 22:55:35 +0100

    Add gen-schema.test.mjs
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

---
+++ packages/db/test/cli/gen-schema.test.mjs
@@ -0,0 +1,13 @@
+import { test } from "tap"
+import fs from 'fs/promises'
+import { generateJsonSchemaConfig } from "../../lib/gen-schema.mjs"
+
+test("generateJsonSchemaConfig generates the file", async (t) => {
+  process.chdir("./test/tmp")
+  await generateJsonSchemaConfig()
+
+  const configSchema = await fs.readFile("platformatic.db.schema.json", "utf8")
+  const { required, additionalProperties } = JSON.parse(configSchema)
+  t.has(required, ['core', 'server'])
+  t.has(additionalProperties, { watch: {} })
+})
