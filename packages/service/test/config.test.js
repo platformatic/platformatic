01eda0cdac0892307e222c18792d3f57f3ef8fb1
Matteo Collina <hello@matteocollina.com>
2022-12-02 18:23:04 +0100

    override restart so when the app is restarted when the config is updated (#424)
    * override restart so when the config file is restarted
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * Fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * Fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/service/test/config.test.js
+++ packages/service/test/config.test.js
@@ -9,7 +9,7 @@ const os = require('os')
 const { writeFile } = require('fs/promises')
 
 test('config reloads', async ({ teardown, equal, pass, same }) => {
-  const file = join(os.tmpdir(), `some-plugin-${process.pid}.js`)
+  const file = join(os.tmpdir(), `${process.pid}-1.js`)
 
   await writeFile(file, `
     module.exports = async function (app, options) {
@@ -62,8 +62,8 @@ test('config reloads', async ({ teardown, equal, pass, same }) => {
 })
 
 test('config reloads from a written file', async ({ teardown, equal, pass, same }) => {
-  const config = join(os.tmpdir(), `some-config-${process.pid}-2.json`)
-  const file = join(os.tmpdir(), `some-plugin-${process.pid}-2.js`)
+  const config = join(os.tmpdir(), `${process.pid}-2.json`)
+  const file = join(os.tmpdir(), `${process.pid}-2.js`)
 
   await writeFile(config, JSON.stringify({
     server: {
@@ -109,6 +109,74 @@ test('config reloads from a written file', async ({ teardown, equal, pass, same 
   })
 
   await server.restart()
+
+  {
+    const res = await request(`${server.url}/`)
+    equal(res.statusCode, 200, 'add status code')
+    same(await res.body.text(), 'ciao mondo', 'response')
+  }
+})
+
+test('config reloads from a written file from a route', async ({ teardown, equal, pass, same }) => {
+  const config = join(os.tmpdir(), `${process.pid}-3.json`)
+  const file = join(os.tmpdir(), `${process.pid}-3.js`)
+
+  await writeFile(config, JSON.stringify({
+    server: {
+      hostname: '127.0.0.1',
+      logger: { level: 'error' },
+      port: 0
+    },
+    plugin: {
+      path: file,
+      options: {
+        message: 'hello'
+      }
+    },
+    metrics: false
+  }))
+
+  await writeFile(file, `
+    module.exports = async function (app, options) {
+      app.get('/', () => options.message)
+
+      app.post('/restart', async (req, res) => {
+        await app.platformatic.configManager.update({
+          server: {
+            hostname: '127.0.0.1',
+            port: 0
+          },
+          plugin: {
+            path: '${file.replace(/\\/g, '\\\\')}',
+            options: {
+              message: 'ciao mondo'
+            }
+          },
+          metrics: false
+        })
+
+        await app.restart()
+
+        return true
+      })
+    }`)
+
+  const server = await buildServer(config)
+  teardown(server.stop)
+  await server.listen()
+
+  {
+    const res = await request(`${server.url}/`)
+    equal(res.statusCode, 200, 'add status code')
+    same(await res.body.text(), 'hello', 'response')
+  }
+
+  {
+    const res = await request(`${server.url}/restart`, {
+      method: 'POST'
+    })
+    equal(res.statusCode, 200, 'add status code')
+  }
 
   {
     const res = await request(`${server.url}/`)
