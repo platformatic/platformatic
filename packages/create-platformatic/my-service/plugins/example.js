2178b7e8a81b880761239300a69c1168d55a1e46
marcopiraccini <marco.piraccini@gmail.com>
2022-12-16 14:11:58 +0100

    removed accidentally added folder
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- packages/create-platformatic/my-service/plugins/example.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-/** @param {import('fastify').FastifyInstance} fastify */
-module.exports = async function (fastify, opts) {
-  fastify.decorate('example', 'foobar')
-}
-module.exports[Symbol.for('skip-override')] = true
--- packages/create-platformatic/my-service/routes/root.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-/** @param {import('fastify').FastifyInstance} fastify */
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async (request, reply) => {
-    return { hello: fastify.example }
-  })
-}
