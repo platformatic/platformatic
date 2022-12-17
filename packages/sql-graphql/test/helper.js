1e1f10f6bf7b334ff0a4cf8f416d3efee1ef99b2
Kivanç Bilen <kivanc_bilen@hotmail.com>
2022-12-17 13:18:31 +0100

    drop table generated_test is added to sql-graphql and sql-openapi
    Signed-off-by: Kivanç Bilen <kivanc_bilen@hotmail.com>

--- packages/sql-graphql/test/helper.js
+++ packages/sql-graphql/test/helper.js
@@ -111,4 +111,9 @@ module.exports.clear = async function (db, sql) {
     await db.query(sql`DROP SCHEMA "myschema"`)
   } catch (err) {
   }
+
+  try {
+    await db.query(sql`DROP TABLE generated_test`)
+  } catch (err) {
+  }
 }
