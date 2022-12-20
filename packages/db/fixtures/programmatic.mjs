27296c0799ae4e56d3fbee619037f28b054d3342
Matteo Collina <hello@matteocollina.com>
2022-12-20 15:29:25 +0100

    Add docs for starting Platformatic DB programmaticaly (#520)
    * Add docs for starting Platformatic DB programmaticaly
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ packages/db/fixtures/programmatic.mjs
@@ -0,0 +1,21 @@
+import { buildServer } from '../index.js'
+import { join } from 'desm'
+
+const server = await buildServer({
+  server: {
+    hostname: '127.0.0.1',
+    port: 0
+  },
+  core: {
+    connectionString: 'sqlite://::memory::'
+  },
+  migrations: {
+    dir: join(import.meta.url, './migrations'),
+    table: 'versions',
+    autoApply: true
+  }
+})
+
+await server.listen() // this will start our server
+
+console.log('URL', server.url)
