28c6381e0077189ad4f7f906170adb53bddbf5f8
Roberto Bianchi <roberto.bianchi@spendesk.com>
2022-12-23 14:24:13 +0100

    fix(graphql): Handle duplicated enum names (#528)
    * fix(graphql): Handle duplicated enum names
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>
    
    * Update packages/sql-graphql/lib/entity-to-type.js
    
    Co-authored-by: Matteo Collina <matteo.collina@gmail.com>
    
    * Add camelcase
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>
    
    * DCO Remediation Commit for Roberto Bianchi <roberto.bianchi@spendesk.com>
    
    I, Roberto Bianchi <roberto.bianchi@spendesk.com>, hereby add my Signed-off-by to this commit: b3d1e9ef56bcd43f951cbd7287bde22be5a50f0b
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>
    
    * Update packages/sql-graphql/lib/entity-to-type.js
    
    Signed-off-by: Matteo Collina hello@matteocollina.com
    
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>
    Signed-off-by: Matteo Collina hello@matteocollina.com
    Co-authored-by: Matteo Collina <matteo.collina@gmail.com>

--- packages/sql-graphql/test/enum.test.js
+++ packages/sql-graphql/test/enum.test.js
@@ -46,3 +46,55 @@ test('should properly setup the enum types', { skip: isSQLite }, async ({ pass, 
     same(true, false, 'Previous call should never fail')
   }
 })
+
+test('should not fail if tables have duplicate enum names', { skip: isSQLite }, async ({ pass, teardown, same }) => {
+  const app = fastify()
+  app.register(sqlMapper, {
+    ...connInfo,
+    async onDatabaseLoad (db, sql) {
+      pass('onDatabaseLoad called')
+
+      await clear(db, sql)
+
+      if (isPg) {
+        await db.query(sql`
+        CREATE TYPE custom_enum AS ENUM('1', '2', '3');
+        CREATE TYPE simple_enum AS ENUM('4', '5', '6');
+        
+        CREATE TABLE enum_tests (
+          id INTEGER NOT NULL,
+          test_enum custom_enum,
+          PRIMARY KEY (id)
+        );
+        CREATE TABLE simple_types (
+          pk INTEGER NOT NULL,
+          test_enum simple_enum,
+          PRIMARY KEY (pk)
+        );`)
+      } else {
+        await db.query(sql`
+        CREATE TABLE enum_tests (
+          id INTEGER NOT NULL,
+          test_enum ENUM ('1', '2', '3') DEFAULT NULL,
+          PRIMARY KEY (id)
+        );
+        CREATE TABLE simple_types (
+          pk INTEGER NOT NULL,
+          test_enum ENUM ('4', '5', '6') DEFAULT NULL,
+          PRIMARY KEY (pk)
+        );`)
+      }
+    }
+  })
+
+  app.register(sqlGraphQL)
+  teardown(app.close.bind(app))
+
+  try {
+    await app.ready()
+    same(true, true, 'test_enum is used twice but app not throws')
+  } catch (err) {
+    console.log(err)
+    same(true, false, 'Previous call should never fail')
+  }
+})
