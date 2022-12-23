985d29274405808ea629e5bd4a98537ed004defc
marcopiraccini <marco.piraccini@gmail.com>
2022-12-23 12:41:31 +0100

    fixup
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- packages/service/test/cli/gen-schema.test.mjs
+++ packages/service/test/cli/gen-schema.test.mjs
@@ -1,9 +1,22 @@
-import { test } from 'tap'
+import { test, beforeEach, afterEach } from 'tap'
 import fs from 'fs/promises'
+import { mkdtempSync } from 'fs'
+import { tmpdir } from 'os'
 import { generateJsonSchemaConfig } from '../../lib/gen-schema.js'
+import { join } from 'path'
+
+let tmpDir
+beforeEach(async () => {
+  tmpDir = await mkdtempSync(join(tmpdir(), 'test-create-platformatic-'))
+})
+
+afterEach(() => {
+  fs.rmdir(tmpDir, { recursive: true, force: true })
+  process.env = {}
+})
 
 test('generateJsonSchemaConfig generates the file', async (t) => {
-  process.chdir('./test/tmp')
+  process.chdir(tmpDir)
   await generateJsonSchemaConfig()
 
   const configSchema = await fs.readFile('platformatic.service.schema.json', 'utf8')
