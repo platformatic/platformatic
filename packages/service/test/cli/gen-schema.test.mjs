e9c7f19984af982f7448513a0b1bcc859181cdcc
Marco Piraccini <marco.piraccini@gmail.com>
2022-12-23 14:12:21 +0100

    fixup test
    Signed-off-by: Marco Piraccini <marco.piraccini@gmail.com>

--- packages/service/test/cli/gen-schema.test.mjs
+++ packages/service/test/cli/gen-schema.test.mjs
@@ -1,21 +1,12 @@
-import { test, beforeEach, afterEach } from 'tap'
+import { test } from 'tap'
 import fs from 'fs/promises'
 import { mkdtempSync } from 'fs'
 import { tmpdir } from 'os'
 import { generateJsonSchemaConfig } from '../../lib/gen-schema.js'
 import { join } from 'path'
 
-let tmpDir
-beforeEach(async () => {
-  tmpDir = await mkdtempSync(join(tmpdir(), 'test-create-platformatic-'))
-})
-
-afterEach(() => {
-  fs.rmdir(tmpDir, { recursive: true, force: true })
-  process.env = {}
-})
-
 test('generateJsonSchemaConfig generates the file', async (t) => {
+  const tmpDir = await mkdtempSync(join(tmpdir(), 'test-create-platformatic-'))
   process.chdir(tmpDir)
   await generateJsonSchemaConfig()
 
