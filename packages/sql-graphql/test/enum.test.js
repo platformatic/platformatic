3dd6b241315e2cc1bd1556797841138e777eae35
Roberto Bianchi <roberto.bianchi@spendesk.com>
2023-01-11 17:59:41 +0100

    fix(sql-graphql): Enum with special chars
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/sql-graphql/test/enum.test.js
+++ packages/sql-graphql/test/enum.test.js
@@ -98,3 +98,128 @@ test('should not fail if tables have duplicate enum names', { skip: isSQLite }, 
     same(true, false, 'Previous call should never fail')
   }
 })
+
+test('should not fail if tables have enum with special characters', { skip: isSQLite }, async ({ pass, teardown, same, equal }) => {
+  const app = fastify()
+  app.register(sqlMapper, {
+    ...connInfo,
+    async onDatabaseLoad (db, sql) {
+      pass('onDatabaseLoad called')
+
+      await clear(db, sql)
+
+      if (isPg) {
+        await db.query(sql`
+          CREATE TYPE custom_enum AS ENUM('.', ',');
+
+          CREATE TABLE enum_tests (
+            id INTEGER NOT NULL,
+            test_enum custom_enum,
+            PRIMARY KEY (id)
+          );
+        `)
+      } else {
+        await db.query(sql`
+          CREATE TABLE enum_tests (
+            id INTEGER NOT NULL,
+            test_enum ENUM ('.', ',') DEFAULT NULL,
+            PRIMARY KEY (id)
+          );
+        `)
+      }
+    }
+  })
+
+  app.register(sqlGraphQL)
+  teardown(app.close.bind(app))
+
+  try {
+    await app.ready()
+    same(true, true, 'custom_enum have special chars, but app does not throws')
+  } catch (err) {
+    console.log(err)
+    same(true, false, 'Previous call should never fail')
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            mutation ($input: EnumTestInput!) {
+              saveEnumTest(input: $input) {
+                id
+              }
+            }
+          `,
+        variables: { input: { id: 1 } }
+      }
+    })
+    equal(res.statusCode, 200)
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            mutation ($input: EnumTestInput!) {
+              saveEnumTest(input: $input) {
+                id
+              }
+            }
+          `,
+        variables: { input: { id: 2 } }
+      }
+    })
+    equal(res.statusCode, 200)
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            query { enumTests { id } }
+          `
+      }
+    })
+    equal(res.statusCode, 200)
+    same(res.json(), { data: { enumTests: [{ id: 1 }, { id: 2 }] } })
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            mutation ($input: EnumTestWhereArgumentsid!) {
+              deleteEnumTests(where: {id: $input}) {
+                id
+              }
+            }
+          `,
+        variables: { input: { eq: 1 } }
+      }
+    })
+    equal(res.statusCode, 200)
+  }
+
+  {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query: `
+            query { enumTests { id } }
+          `
+      }
+    })
+    equal(res.statusCode, 200)
+    same(res.json(), { data: { enumTests: [{ id: 2 }] } })
+  }
+})
