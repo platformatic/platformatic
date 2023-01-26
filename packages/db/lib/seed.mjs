2cdd12224bdc9db7504240b842ff9ca86669796d
Ivan Tymoshenko <bugagashenkj@gmail.com>
2023-01-26 15:09:41 +0100

    feat: throw an error on seed command if there are migrations to apply (#626)
    * feat: throw an error on seed command if there are migrations to apply
    
    * fixup
    
    Signed-off-by: HassanBahati <mukisabahati@gmail.com>

--- packages/db/lib/seed.mjs
+++ packages/db/lib/seed.mjs
@@ -2,10 +2,11 @@ import pino from 'pino'
 import pretty from 'pino-pretty'
 import { access } from 'fs/promises'
 import { setupDB } from './utils.js'
+import { Migrator } from './migrator.mjs'
 import { SeedError } from './errors.mjs'
-import { execute as migrator } from './migrate.mjs'
 import { pathToFileURL } from 'url'
 import loadConfig from './load-config.mjs'
+
 async function execute (logger, args, config) {
   const { db, sql, entities } = await setupDB(logger, config.core)
 
@@ -42,7 +43,19 @@ async function seed (_args) {
     await configManager.parseAndValidate()
     const config = configManager.current
 
-    await migrator(logger, args, config)
+    if (config.migrations !== undefined) {
+      const migrator = new Migrator(config.migrations, config.core, logger)
+
+      try {
+        const hasMigrationsToApply = await migrator.hasMigrationsToApply()
+        if (hasMigrationsToApply) {
+          throw new SeedError('You have migrations to apply. Please run `platformatic db migrations apply` first.')
+        }
+      } finally {
+        await migrator.close()
+      }
+    }
+
     await execute(logger, args, config)
   } catch (err) {
     if (err instanceof SeedError) {
