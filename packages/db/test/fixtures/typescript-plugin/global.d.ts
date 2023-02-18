e60ab1c6af954a8b24878c2a7577f84109c6d019
Roberto Bianchi <roberto.bianki@gmail.com>
2023-02-18 01:08:05 +0100

    Update test
    Signed-off-by: Roberto Bianchi <roberto.bianki@gmail.com>

--- packages/db/test/fixtures/typescript-plugin/global.d.ts
+++ packages/db/test/fixtures/typescript-plugin/global.d.ts
@@ -2,6 +2,19 @@ import { Entity } from '@platformatic/sql-mapper';
 import graphqlPlugin from '@platformatic/sql-graphql'
 import { Movie } from './types/Movie'
 
+declare module 'fastify' {
+  interface FastifyInstance {
+    getSchema(schemaId: 'Movie'): {
+      '$id': string,
+      title: string,
+      description: string,
+      type: string,
+      properties: object,
+      required: string[]
+    };
+  }
+}
+
 declare module '@platformatic/sql-mapper' {
   interface Entities {
     movie: Entity<Movie>,
