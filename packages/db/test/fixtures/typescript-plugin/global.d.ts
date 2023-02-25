5280b88cc893cc4f0e4d4d079cadce2bd6598941
Marc Cremer <marc@cremer.dev>
2023-02-25 09:49:01 +0100

    Feat/types/generate better get schema return (#728)

--- packages/db/test/fixtures/typescript-plugin/global.d.ts
+++ packages/db/test/fixtures/typescript-plugin/global.d.ts
@@ -1,15 +1,18 @@
 import { Entity } from '@platformatic/sql-mapper';
 import graphqlPlugin from '@platformatic/sql-graphql'
-import { Movie } from './types/Movie'
+import { EntityTypes, Movie } from './types'
+
 
 declare module 'fastify' {
   interface FastifyInstance {
-    getSchema(schemaId: 'Movie'): {
+    getSchema<T extends 'Movie'>(schemaId: T): {
       '$id': string,
       title: string,
       description: string,
       type: string,
-      properties: object,
+      properties: {
+        [x in keyof EntityTypes[T]]: { type: string, nullable?: boolean }
+      },
       required: string[]
     };
   }
