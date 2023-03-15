c0d8aac764836c26e08840dfb9de1325f892405a
Matteo Collina <hello@matteocollina.com>
2023-03-15 01:33:22 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/service/test/cli/gen-schema.test.mjs
+++ packages/service/test/cli/gen-schema.test.mjs
@@ -15,7 +15,7 @@ test('generateJsonSchemaConfig generates the file', async (t) => {
 
   const configSchema = await fs.readFile('platformatic.service.schema.json', 'utf8')
   const schema = JSON.parse(configSchema)
-  const { required, additionalProperties } = schema
+  const { required } = schema
   t.has(required, ['server'])
   const { $id, type } = schema
   t.equal($id, `https://platformatic.dev/schemas/v${pkg.version}/service`)
