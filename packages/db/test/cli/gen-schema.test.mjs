5aa1408c3c1d05280b17d454ea86c1d7515e557d
Matteo Collina <hello@matteocollina.com>
2023-03-15 09:27:23 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/db/test/cli/gen-schema.test.mjs
+++ packages/db/test/cli/gen-schema.test.mjs
@@ -1,13 +1,16 @@
 import { test } from 'tap'
 import fs from 'fs/promises'
 import { generateJsonSchemaConfig } from '../../lib/gen-schema.mjs'
+import Ajv from 'ajv'
+import { schema } from '../../lib/schema.js'
 
 test('generateJsonSchemaConfig generates the file', async (t) => {
   process.chdir('./test/tmp')
   await generateJsonSchemaConfig()
 
-  const configSchema = await fs.readFile('platformatic.db.schema.json', 'utf8')
-  const { required, additionalProperties } = JSON.parse(configSchema)
-  t.has(required, ['core', 'server'])
-  t.has(additionalProperties, { watch: {} })
+  const configSchema = JSON.parse(await fs.readFile('platformatic.db.schema.json', 'utf8'))
+  const ajv = new Ajv()
+  // this should not throw
+  ajv.compile(schema)
+  t.same(configSchema, schema)
 })
