9b92ff6b6f57f136658f2dc96c7b58bf2ddd42a6
cjihrig <cjihrig@gmail.com>
2023-03-16 18:40:10 -0400

    handle relative paths

--- packages/service/test/https.test.js
+++ packages/service/test/https.test.js
@@ -2,7 +2,7 @@
 
 const { mkdtempSync, writeFileSync } = require('fs')
 const { tmpdir } = require('os')
-const { join } = require('path')
+const { isAbsolute, join, relative } = require('path')
 const selfCert = require('self-cert')
 const { test } = require('tap')
 const { Agent, setGlobalDispatcher, request } = require('undici')
@@ -10,12 +10,13 @@ const { buildServer } = require('..')
 const { buildConfig } = require('./helper')
 
 test('supports https options', async ({ teardown, equal, same, plan }) => {
-  plan(6)
+  plan(7)
 
   const { certificate, privateKey } = selfCert({})
   const tmpDir = mkdtempSync(join(tmpdir(), 'plt-service-https-test-'))
   const privateKeyPath = join(tmpDir, 'plt.key')
   const certificatePath = join(tmpDir, 'plt.cert')
+  const certificateRelativePath = relative(__dirname, certificatePath)
 
   writeFileSync(privateKeyPath, privateKey)
   writeFileSync(certificatePath, certificate)
@@ -32,7 +33,7 @@ test('supports https options', async ({ teardown, equal, same, plan }) => {
       port: 0,
       https: {
         key: privateKey,
-        cert: [{ path: certificatePath }]
+        cert: [{ path: certificateRelativePath }]
       }
     }
   }))
@@ -40,6 +41,7 @@ test('supports https options', async ({ teardown, equal, same, plan }) => {
   teardown(server.stop)
   await server.listen()
 
+  equal(isAbsolute(server.app.platformatic.configManager.current.server.https.cert[0].path), true)
   equal(server.url.startsWith('https://'), true)
   let res = await (request(`${server.url}/`))
   equal(res.statusCode, 200)
