842ff6e9fc1b5e91c4c73febca293e5cbab90115
Matteo Collina <hello@matteocollina.com>
2023-04-14 09:54:14 +0200

    Update packages/service/test/fixtures/nested-directories/modules/catalogue/routes/products.js
    Co-authored-by: Colin Ihrig <cjihrig@gmail.com>

--- packages/service/test/fixtures/nested-directories/modules/catalogue/routes/products.js
+++ packages/service/test/fixtures/nested-directories/modules/catalogue/routes/products.js
@@ -1,7 +1,7 @@
 export default async function (fastify, opts) {
   fastify.get('/products', async (request, reply) => {
     const data = [{ sku: 42, name: 'foo' }, { sku: 43, name: 'bar' }]
-    // Currentlyn slow and inefficient, but ok for the demo
+    // Currently slow and inefficient, but ok for the demo
     for (const product of data) {
       product.inStore = await fastify.inventory.howManyInStore(product.sku)
     }
