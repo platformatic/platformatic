ad611ce77c1bb7826dbdddce162f7ecd176bf321
Marcelo Mollaj <marcelomollaj@gmail.com>
2023-04-15 16:49:59 +0300

    feat(#847): add docs for or conditions in sql-openapi and remove or check nesting
    Signed-off-by: Marcelo Mollaj <marcelomollaj@gmail.com>

--- packages/sql-openapi/lib/shared.js
+++ packages/sql-openapi/lib/shared.js
@@ -84,35 +84,35 @@ function rootEntityRoutes (app, entity, whereArgs, orderByArgs, entityLinks, ent
 
     for (let i = 0; i < queryKeys.length; i++) {
       const key = queryKeys[i]
+      if (key.startsWith('where.or')) {
+        const orParam = query[key][0]
+        // NOTE: Remove the first and last character which are the brackets
+        // each or condition is separated by a pipe '|'
+        // the conditions inside the or statement are the same as it would normally be in the where statement
+        // except that the field name is not prefixed with 'where.'
+        // e.g. where.or=(name.eq=foo|name.eq=bar)
+        // e.g. where.or=(name.eq=foo|name.eq=bar|name.eq=baz)
+        //
+        // Also, the or statement supports in and nin operators
+        // e.g. where.or=(name.in=foo,bar|name.eq=baz)
+        const parsed = orParam.substring(1, orParam.length - 1).split('|').map((v) => v.split('=')).reduce((acc, [k, v]) => {
+          const [field, modifier] = k.split('.')
+          if (modifier === 'in' || modifier === 'nin') {
+            // TODO handle escaping of ,
+            v = v.split(',')
+            /* istanbul ignore next */
+            if (mapSQLTypeToOpenAPIType(entity.fields[field].sqlType) === 'integer') {
+              v = v.map((v) => parseInt(v))
+            }
+          }
+          acc.push({ [field]: { [modifier]: v } })
+          return acc
+        }, [])
+        where.or = parsed
+        continue
+      }
 
       if (key.startsWith('where.')) {
-        if (key.startsWith('where.or')) {
-          const orParam = query[key][0]
-          // NOTE: Remove the first and last character which are the brackets
-          // each or condition is separated by a pipe '|'
-          // the conditions inside the or statement are the same as it would normally be in the where statement
-          // except that the field name is not prefixed with 'where.'
-          // e.g. where.or=(name.eq=foo|name.eq=bar)
-          // e.g. where.or=(name.eq=foo|name.eq=bar|name.eq=baz)
-          //
-          // Also, the or statement supports in and nin operators
-          // e.g. where.or=(name.in=foo,bar|name.eq=baz)
-          const parsed = orParam.substring(1, orParam.length - 1).split('|').map((v) => v.split('=')).reduce((acc, [k, v]) => {
-            const [field, modifier] = k.split('.')
-            if (modifier === 'in' || modifier === 'nin') {
-              // TODO handle escaping of ,
-              v = v.split(',')
-              /* istanbul ignore next */
-              if (mapSQLTypeToOpenAPIType(entity.fields[field].sqlType) === 'integer') {
-                v = v.map((v) => parseInt(v))
-              }
-            }
-            acc.push({ [field]: { [modifier]: v } })
-            return acc
-          }, [])
-          where.or = parsed
-          continue
-        }
         const [, field, modifier] = key.split('.')
         where[field] ||= {}
         let value = query[key]
