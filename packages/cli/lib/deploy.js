b811220e394c0c35d3c119cd3cb5953cfd42fb25
Ivan Tymoshenko <bugagashenkj@gmail.com>
2023-04-18 14:40:42 +0200

    feat: add platfromatic deploy command

---
+++ packages/cli/lib/deploy.js
@@ -0,0 +1,143 @@
+'use strict'
+
+import { isAbsolute, dirname, relative } from 'path'
+
+import pino from 'pino'
+import pretty from 'pino-pretty'
+import inquirer from 'inquirer'
+import parseArgs from 'minimist'
+import deployClient from '@platformatic/deploy-client'
+
+export const DEPLOY_SERVICE_HOST = 'https://plt-production-deploy-service.fly.dev'
+
+const WORKSPACE_TYPES = ['static', 'dynamic']
+const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+
+const logger = pino(pretty({
+  translateTime: 'SYS:HH:MM:ss',
+  ignore: 'hostname,pid'
+}))
+
+export async function deploy (argv) {
+  try {
+    const args = parseArgs(argv, {
+      alias: {
+        config: 'c',
+        type: 't',
+        label: 'l',
+        env: 'e',
+        secrets: 's'
+      },
+      string: [
+        'type',
+        'label',
+        'workspace-id',
+        'workspace-key',
+        'env',
+        'secrets',
+        'deploy-service-host'
+      ],
+      default: {
+        'deploy-service-host': DEPLOY_SERVICE_HOST
+      }
+    })
+
+    let workspaceType = args.type
+    /* c8 ignore next 9 */
+    if (!workspaceType) {
+      const answer = await inquirer.prompt({
+        type: 'list',
+        name: 'workspaceType',
+        message: 'Select workspace type:',
+        choices: WORKSPACE_TYPES
+      })
+      workspaceType = answer.workspaceType
+    }
+
+    if (!WORKSPACE_TYPES.includes(workspaceType)) {
+      throw new Error(
+        `Invalid workspace type provided: "${workspaceType}". ` +
+        `Type must be one of: ${WORKSPACE_TYPES.join(', ')}.`
+      )
+    }
+
+    let workspaceId = args['workspace-id']
+    /* c8 ignore next 8 */
+    if (!workspaceId) {
+      const answer = await inquirer.prompt({
+        type: 'input',
+        name: 'workspaceId',
+        message: 'Enter workspace id:'
+      })
+      workspaceId = answer.workspaceId
+    }
+
+    if (!UUID_REGEX.test(workspaceId)) {
+      throw new Error('Invalid workspace id provided. Workspace id must be a valid uuid.')
+    }
+
+    let workspaceKey = args['workspace-key']
+    /* c8 ignore next 9 */
+    if (!workspaceKey) {
+      const answer = await inquirer.prompt({
+        type: 'password',
+        name: 'workspaceKey',
+        message: 'Enter workspace key:',
+        mask: '*'
+      })
+      workspaceKey = answer.workspaceKey
+    }
+
+    let label = args.label
+    if (workspaceType === 'dynamic') {
+    /* c8 ignore next 9 */
+      if (!label) {
+        const answer = await inquirer.prompt({
+          type: 'input',
+          name: 'label',
+          message: 'Enter deploy label:',
+          default: 'cli:deploy-1'
+        })
+        label = answer.label
+      }
+
+      const labelPrefix = label.split(':')[0]
+      const labelPrefixes = ['cli', 'github-pr']
+      if (!labelPrefix || !labelPrefixes.includes(labelPrefix)) {
+        throw new Error(
+          `Invalid deploy label provided: "${label}". ` +
+          `Label must be prefixed with one of: ${labelPrefixes.join(', ')}.`
+        )
+      }
+    }
+
+    let pathToConfig = args.config
+    let pathToProject = process.cwd()
+
+    if (pathToConfig && isAbsolute(pathToConfig)) {
+      pathToProject = dirname(pathToConfig)
+      pathToConfig = relative(pathToProject, pathToConfig)
+    }
+
+    const pathToEnvFile = args.env || '.env'
+    const pathToSecretsFile = args.secrets || '.secrets.env'
+    const deployServiceHost = args['deploy-service-host']
+
+    await deployClient.deploy({
+      deployServiceHost,
+      workspaceId,
+      workspaceKey,
+      pathToProject,
+      pathToConfig,
+      pathToEnvFile,
+      pathToSecretsFile,
+      secrets: {},
+      variables: {},
+      label,
+      logger
+    })
+  } catch (err) {
+    logger.error(err.message)
+    process.exit(1)
+  }
+}
---
+++ packages/cli/test/deploy.test.js
@@ -0,0 +1,174 @@
+import { test } from 'tap'
+import { join } from 'desm'
+import { execa } from 'execa'
+
+import { cliPath, startDeployService, startMachine } from './helper.js'
+
+test('should deploy to a static workspace to the cloud', async (t) => {
+  const workspaceType = 'static'
+  const workspaceId = 'b3d7f7e0-8c03-11e8-9eb6-529269fb1459'
+  const workspaceKey = 'b3d7f7e08c0311e89eb6529269fb1459'
+  const pathToConfig = join(import.meta.url, './fixtures/app-to-deploy/platformatic.db.json')
+
+  const machineHost = await startMachine(t)
+  const deployServiceHost = await startDeployService(t, {
+    createBundleCallback: (request, reply) => {
+      t.equal(request.headers['x-platformatic-workspace-id'], workspaceId)
+      t.equal(request.headers['x-platformatic-api-key'], workspaceKey)
+      t.match(request.body, {
+        bundle: {
+          appType: 'db',
+          configPath: 'platformatic.db.json'
+        }
+      })
+      t.ok(request.body.bundle.checksum)
+    },
+    createDeploymentCallback: (request, reply) => {
+      t.equal(request.headers['x-platformatic-workspace-id'], workspaceId)
+      t.equal(request.headers['x-platformatic-api-key'], workspaceKey)
+      t.same(
+        request.body,
+        {
+          variables: {
+            PLT_ENV_VARIABLE1: 'platformatic_variable1',
+            PLT_ENV_VARIABLE2: 'platformatic_variable2'
+          },
+          secrets: {
+            PLT_SECRET_1: 'platformatic_secret_1',
+            PLT_SECRET_2: 'platformatic_secret_2'
+          }
+        }
+      )
+      reply.code(200).send({ entryPointUrl: machineHost })
+    }
+  })
+
+  await execa('node', [
+    cliPath, 'deploy',
+    '--type', workspaceType,
+    '--config', pathToConfig,
+    '--workspace-id', workspaceId,
+    '--workspace-key', workspaceKey,
+    '--deploy-service-host', deployServiceHost
+  ])
+})
+
+test('should deploy to a dynamic workspace to the cloud', async (t) => {
+  const workspaceType = 'dynamic'
+  const workspaceId = 'b3d7f7e0-8c03-11e8-9eb6-529269fb1459'
+  const workspaceKey = 'b3d7f7e08c0311e89eb6529269fb1459'
+  const pathToConfig = join(import.meta.url, './fixtures/app-to-deploy/platformatic.db.json')
+  const label = 'cli:deploy-2'
+
+  const machineHost = await startMachine(t)
+  const deployServiceHost = await startDeployService(t, {
+    createBundleCallback: (request, reply) => {
+      t.equal(request.headers['x-platformatic-workspace-id'], workspaceId)
+      t.equal(request.headers['x-platformatic-api-key'], workspaceKey)
+      t.match(request.body, {
+        bundle: {
+          appType: 'db',
+          configPath: 'platformatic.db.json'
+        }
+      })
+      t.ok(request.body.bundle.checksum)
+    },
+    createDeploymentCallback: (request, reply) => {
+      t.equal(request.headers['x-platformatic-workspace-id'], workspaceId)
+      t.equal(request.headers['x-platformatic-api-key'], workspaceKey)
+      t.same(
+        request.body,
+        {
+          label,
+          variables: {
+            PLT_ENV_VARIABLE1: 'platformatic_variable1',
+            PLT_ENV_VARIABLE2: 'platformatic_variable2'
+          },
+          secrets: {
+            PLT_SECRET_1: 'platformatic_secret_1',
+            PLT_SECRET_2: 'platformatic_secret_2'
+          }
+        }
+      )
+      reply.code(200).send({ entryPointUrl: machineHost })
+    }
+  })
+
+  await execa('node', [
+    cliPath, 'deploy',
+    '--type', workspaceType,
+    '--label', label,
+    '--config', pathToConfig,
+    '--workspace-id', workspaceId,
+    '--workspace-key', workspaceKey,
+    '--deploy-service-host', deployServiceHost
+  ])
+})
+
+test('should fail if workspace id is not a uuid', async (t) => {
+  const workspaceType = 'static'
+  const workspaceId = 'not-a-uuid'
+  const workspaceKey = 'b3d7f7e08c0311e89eb6529269fb1459'
+  const pathToConfig = join(import.meta.url, './fixtures/app-to-deploy/platformatic.db.json')
+
+  try {
+    await execa('node', [
+      cliPath, 'deploy',
+      '--type', workspaceType,
+      '--config', pathToConfig,
+      '--workspace-id', workspaceId,
+      '--workspace-key', workspaceKey,
+      '--deploy-service-host', 'http://localhost:5555'
+    ])
+    t.fail('should have failed')
+  } catch (err) {
+    t.ok(err.message.includes('Invalid workspace id provided. Workspace id must be a valid uuid.'))
+  }
+})
+
+test('should fail if invalid workspace type provided', async (t) => {
+  const workspaceType = 'wrong'
+  const workspaceId = 'b3d7f7e0-8c03-11e8-9eb6-529269fb1459'
+  const workspaceKey = 'b3d7f7e08c0311e89eb6529269fb1459'
+  const pathToConfig = join(import.meta.url, './fixtures/app-to-deploy/platformatic.db.json')
+
+  try {
+    await execa('node', [
+      cliPath, 'deploy',
+      '--type', workspaceType,
+      '--config', pathToConfig,
+      '--workspace-id', workspaceId,
+      '--workspace-key', workspaceKey,
+      '--deploy-service-host', 'http://localhost:5555'
+    ])
+    t.fail('should have failed')
+  } catch (err) {
+    t.ok(err.message.includes('Invalid workspace type provided'))
+  }
+})
+
+test('should fail if deploy label does not start with a "cli:" prefix', async (t) => {
+  const workspaceType = 'dynamic'
+  const workspaceId = 'b3d7f7e0-8c03-11e8-9eb6-529269fb1459'
+  const workspaceKey = 'b3d7f7e08c0311e89eb6529269fb1459'
+  const pathToConfig = join(import.meta.url, './fixtures/app-to-deploy/platformatic.db.json')
+  const label = 'my label'
+
+  try {
+    await execa('node', [
+      cliPath, 'deploy',
+      '--type', workspaceType,
+      '--label', label,
+      '--config', pathToConfig,
+      '--workspace-id', workspaceId,
+      '--workspace-key', workspaceKey,
+      '--deploy-service-host', 'http://localhost:5555'
+    ])
+    t.fail('should have failed')
+  } catch (err) {
+    t.ok(err.message.includes(
+      `Invalid deploy label provided: "${label}". ` +
+      'Label must be prefixed with one of: cli, github-pr.'
+    ))
+  }
+})
