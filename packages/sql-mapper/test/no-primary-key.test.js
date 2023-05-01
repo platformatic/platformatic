be59e5132525cf0702c959642a0e700ee5f2764c
Vipul BhasiN <vipulb@sent.as>
2023-05-01 10:34:32 +0530

    remove weird tests
    Signed-off-by: Vipul BhasiN <vipulb@sent.as>

--- packages/sql-mapper/test/no-primary-key.test.js
+++ packages/sql-mapper/test/no-primary-key.test.js
@@ -10,43 +10,6 @@ const fakeLogger = {
   error: () => {}
 }
 
-test('unique key', async ({ equal, not, same, teardown }) => {
-  async function onDatabaseLoad (db, sql) {
-    await clear(db, sql)
-    teardown(() => db.dispose())
-
-    const table = sql`
-      CREATE TABLE pages (
-        xx INTEGER DEFAULT NULL UNIQUE,
-        name varchar(75) DEFAULT NULL UNIQUE
-      );
-    `
-
-    await db.query(table)
-  }
-  const mapper = await connect({
-    connectionString: connInfo.connectionString,
-    log: fakeLogger,
-    onDatabaseLoad,
-    ignore: {},
-    hooks: {}
-  })
-  const pageEntity = mapper.entities.page
-  not(pageEntity, undefined)
-  equal(pageEntity.name, 'Page')
-  equal(pageEntity.singularName, 'page')
-  equal(pageEntity.pluralName, 'pages')
-  if (isMysql8) {
-    same(pageEntity.primaryKeys, new Set(['name']))
-    equal(pageEntity.camelCasedFields.name.primaryKey, true)
-  } else {
-    same(pageEntity.primaryKeys, new Set(['xx']))
-    equal(pageEntity.camelCasedFields.xx.primaryKey, true)
-  }
-  equal(pageEntity.camelCasedFields.xx.unique, true)
-  equal(pageEntity.camelCasedFields.name.unique, true)
-})
-
 test('no key', async ({ same, teardown, pass, equal, plan }) => {
   plan(3)
   async function onDatabaseLoad (db, sql) {
