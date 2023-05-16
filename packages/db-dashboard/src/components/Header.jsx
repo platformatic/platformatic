a1427c7670c3bc428cd85f5625fceb692edbb897
Antonio Sonis <anthonysonis@gmail.com>
2023-05-16 09:22:09 +0200

    fix: using new props for ui-components (#1003)
    Signed-off-by: Antonio Sonis <anthonysonis@gmail.com>

--- packages/db-dashboard/src/components/Header.jsx
+++ packages/db-dashboard/src/components/Header.jsx
@@ -2,6 +2,8 @@ import styles from './Header.module.css'
 import { AppContext } from '../App'
 import React, { useContext } from 'react'
 import { Button } from '@platformatic/ui-components'
+import { WHITE, ERROR_RED, BOX_SHADOW } from '@platformatic/ui-components/src/components/constants'
+
 export default function Header () {
   const { userName, setUsername, logged, setLogged, dashboardPath } = useContext(AppContext)
   function onLogoutButtonClicked (event) {
@@ -15,7 +17,7 @@ export default function Header () {
         <img className={styles.logo} src='/images/00_Platformatic_Squared_Logo_Blue_Transparent_300dpi.png' />
       </a>
       {logged && userName && (
-        <Button color='red' primary='false' onClick={onLogoutButtonClicked} label={`Logout ${userName}`} />
+        <Button color={WHITE} backgroundColor={ERROR_RED} onClick={onLogoutButtonClicked} label={`Logout ${userName}`} bold hoverEffect={BOX_SHADOW} />
       )}
     </div>
   )
