2aaba56d01c0996d0897599885c2eb126a372145
Matteo Collina <hello@matteocollina.com>
2023-05-22 19:09:17 +0200

    Do not ask the workspace id during the wizard (#1017)
    * Do not ask the workspace id during the wizard
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    ---------
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/cli/test/gh.test.js
+++ packages/cli/test/gh.test.js
@@ -8,7 +8,6 @@ import { dirname, join } from 'path'
 import mkdirp from 'mkdirp'
 
 let count = 0
-const workspaceId = 'WORKSPACE-ID-TEST'
 
 test('creates a deploy config for static workspace', async (t) => {
   const dest = join(tmpdir(), `test-cli-gh-${process.pid}-${count++}`)
@@ -19,13 +18,13 @@ test('creates a deploy config for static workspace', async (t) => {
     join(dirname(fileURLToPath(import.meta.url)), 'fixtures', 'v0.16.0.db.json'),
     join(dest, 'platformatic.db.json'))
 
-  await execa('node', [cliPath, 'gh', '-w', workspaceId], {
+  await execa('node', [cliPath, 'gh'], {
     cwd: dest
   })
 
   const deployWorkflow = await readFile(join(dest, '.github', 'workflows', 'platformatic-static-workspace-deploy.yml'), 'utf8')
+  t.ok(deployWorkflow.indexOf('PLATFORMATIC_STATIC_WORKSPACE_ID') !== -1)
   t.ok(deployWorkflow.indexOf('PLATFORMATIC_STATIC_WORKSPACE_API_KEY') !== -1)
-  t.ok(deployWorkflow.indexOf('WORKSPACE-ID-TEST') !== -1)
 })
 
 test('creates a deploy config for dynamic workspace', async (t) => {
@@ -36,31 +35,13 @@ test('creates a deploy config for dynamic workspace', async (t) => {
     join(dirname(fileURLToPath(import.meta.url)), 'fixtures', 'v0.16.0.db.json'),
     join(dest, 'platformatic.db.json'))
 
-  await execa('node', [cliPath, 'gh', '-w', workspaceId, '-t', 'dynamic'], {
+  await execa('node', [cliPath, 'gh', '-t', 'dynamic'], {
     cwd: dest
   })
 
   const deployWorkflow = await readFile(join(dest, '.github', 'workflows', 'platformatic-dynamic-workspace-deploy.yml'), 'utf8')
+  t.ok(deployWorkflow.indexOf('PLATFORMATIC_DYNAMIC_WORKSPACE_ID') !== -1)
   t.ok(deployWorkflow.indexOf('PLATFORMATIC_DYNAMIC_WORKSPACE_API_KEY') !== -1)
-  t.ok(deployWorkflow.indexOf('WORKSPACE-ID-TEST') !== -1)
-})
-
-test('creation fails if workspace is missing', async (t) => {
-  const dest = join(tmpdir(), `test-cli-gh-${process.pid}-${count++}`)
-
-  await mkdirp(dest)
-  await cp(
-    join(dirname(fileURLToPath(import.meta.url)), 'fixtures', 'v0.16.0.db.json'),
-    join(dest, 'platformatic.db.json'))
-
-  try {
-    await execa('node', [cliPath, 'gh'], {
-      cwd: dest
-    })
-    t.fail('should have failed')
-  } catch ({ stdout }) {
-    t.ok(stdout.indexOf('Workspace ID is required') !== -1)
-  }
 })
 
 test('creation fails if workspace type is not static or dynamic', async (t) => {
@@ -72,7 +53,7 @@ test('creation fails if workspace type is not static or dynamic', async (t) => {
     join(dest, 'platformatic.db.json'))
 
   try {
-    await execa('node', [cliPath, 'gh', '-w', workspaceId, '-t', 'XXXXX'], {
+    await execa('node', [cliPath, 'gh', '-t', 'XXXXX'], {
       cwd: dest
     })
     t.fail('should have failed')
@@ -85,7 +66,7 @@ test('creation fails if no config file is found', async (t) => {
   const dest = join(tmpdir(), `test-cli-gh-${process.pid}-${count++}`)
   await mkdirp(dest)
   try {
-    await execa('node', [cliPath, 'gh', '-w', workspaceId], {
+    await execa('node', [cliPath, 'gh'], {
       cwd: dest
     })
     t.fail('should have failed')
@@ -104,7 +85,7 @@ test('creates a deploy must warn that a .env exists', async (t) => {
 
   await writeFile(join(dest, '.env'), 'TEST=1')
 
-  const { stdout } = await execa('node', [cliPath, 'gh', '-w', workspaceId], {
+  const { stdout } = await execa('node', [cliPath, 'gh'], {
     cwd: dest
   })
   t.ok(stdout.indexOf('Found .env file') !== -1)
