314e91cb9cc2cdd5d2195da127f7d631ced9039c
Matteo Collina <hello@matteocollina.com>
2023-05-26 12:10:35 +0200

    Restore watch: true by default in service
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/service/test/watch.test.js
+++ packages/service/test/watch.test.js
@@ -180,3 +180,69 @@ test('should stop watching typescript files after disabling watch option', async
     same(await res.body.json(), { res: 'plugin, version 1' }, 'get rest plugin')
   }
 })
+
+test('should watch files by default', async ({ teardown, equal, pass, same }) => {
+  const tmpDir = await mkdtemp(join(tmpdir(), 'platformatic.service.test-'))
+  const pathToPlugin = join(tmpDir, 'plugin.js')
+  const pathToConfig = join(tmpDir, 'platformatic.service.json')
+
+  teardown(async () => {
+    await rm(tmpDir, { recursive: true, force: true })
+  })
+
+  await writeFile(pathToPlugin, `
+    module.exports = async function plugin (app) {
+      app.get('/test', {}, async function (request, response) {
+        return { res: "plugin, version 1"}
+      })
+    }`)
+
+  const config = {
+    server: {
+      hostname: '127.0.0.1',
+      port: 0
+    },
+    plugins: {
+      paths: [pathToPlugin],
+      stopTimeout: 1000,
+      hotReload: true
+    },
+    metrics: false
+  }
+
+  await writeFile(pathToConfig, JSON.stringify(config, null, 2))
+  const app = await buildServer(pathToConfig)
+
+  teardown(async () => {
+    await app.close()
+  })
+  await app.start()
+
+  {
+    const res = await request(`${app.url}/test`, {
+      method: 'GET'
+    })
+    same(await res.body.json(), { res: 'plugin, version 1' }, 'get rest plugin')
+  }
+
+  equal(app.fileWatcher.isWatching, true)
+
+  await writeFile(pathToPlugin, `
+    module.exports = async function plugin (app) {
+      app.get('/test', {}, async function (request, response) {
+        return { res: "plugin, version 2"}
+      })
+    }`)
+
+  // wait to be sure that app is not watching files anymore
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+
+  {
+    const res = await request(`${app.url}/test`, {
+      method: 'GET'
+    })
+    equal(res.statusCode, 200)
+    // must be unchanged
+    same(await res.body.json(), { res: 'plugin, version 2' })
+  }
+})
