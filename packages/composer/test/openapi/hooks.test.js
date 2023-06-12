8864a0232f8fc7ee8fa0bf2f4d91d9c53ddea3a9
Ivan Tymoshenko <bugagashenkj@gmail.com>
2023-06-12 12:24:19 +0200

    test: add test for multiple route hooks

--- packages/composer/test/openapi/hooks.test.js
+++ packages/composer/test/openapi/hooks.test.js
@@ -36,6 +36,39 @@ test('should add onSend route hook', async (t) => {
   }
 })
 
+test('should add multiple onRoute hooks for one route', async (t) => {
+  const api = await createOpenApiService(t, ['users'])
+  await api.listen({ port: 0 })
+
+  const composer = await createComposer(t,
+    {
+      composer: {
+        services: [
+          {
+            id: 'api1',
+            origin: 'http://127.0.0.1:' + api.server.address().port,
+            openapi: {
+              url: '/documentation/json'
+            }
+          }
+        ]
+      },
+      plugins: {
+        paths: [join(__dirname, './fixtures/plugins/multiple-hooks.js')],
+        hotReload: false
+      }
+    }
+  )
+
+  {
+    const { statusCode, body } = await composer.inject({ method: 'GET', url: '/users/1' })
+    t.equal(statusCode, 200)
+
+    const routeSchema = JSON.parse(body)
+    t.equal(routeSchema.response[200].description, 'This is a test')
+  }
+})
+
 test('should throw an error if addComposerOnRouteHook called when app is ready', async (t) => {
   const api = await createOpenApiService(t, ['users'])
   await api.listen({ port: 0 })
