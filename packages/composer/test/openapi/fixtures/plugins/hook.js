c9d5a2b568dbb0d9d9b9825037d4ff578fdb0b01
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-06-20 13:47:30 +0200

    feat: move composer hook under platformatic namespace

--- packages/composer/test/openapi/fixtures/plugins/hook.js
+++ packages/composer/test/openapi/fixtures/plugins/hook.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = async function (app) {
-  app.addComposerOnRouteHook('/users/{id}', ['GET'], routeOptions => {
+  app.platformatic.addComposerOnRouteHook('/users/{id}', ['GET'], routeOptions => {
     routeOptions.onSend = async (req, reply) => {
       reply.status(304)
       return null
