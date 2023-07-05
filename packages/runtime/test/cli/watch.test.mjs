bc74f615855d4290f5e6f2802b195af851fef772
Matteo Collina <hello@matteocollina.com>
2023-07-05 16:02:17 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/runtime/test/cli/watch.test.mjs
+++ packages/runtime/test/cli/watch.test.mjs
@@ -10,7 +10,6 @@ import { start } from './helper.mjs'
 const fixturesDir = join(desm(import.meta.url), '..', '..', 'fixtures')
 
 const base = join(desm(import.meta.url), '..', 'tmp')
-console.log(base)
 
 try {
   await mkdir(base, { recursive: true })
