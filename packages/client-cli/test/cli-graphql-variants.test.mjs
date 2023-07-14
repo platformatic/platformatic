ae65c412db51699b39f1b6f29838c74ecfa57866
Matteo Collina <hello@matteocollina.com>
2023-07-14 10:52:06 +0200

    Try to fix CI for client-cli (#1257)
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ packages/client-cli/test/cli-graphql-variants.test.mjs
@@ -0,0 +1,306 @@
+import { request, moveToTmpdir } from './helper.js'
+import { test } from 'tap'
+import { buildServer } from '@platformatic/db'
+import { join } from 'path'
+import * as desm from 'desm'
+import { execa } from 'execa'
+import { promises as fs } from 'fs'
+import split from 'split2'
+import graphql from 'graphql'
+import { copy } from 'fs-extra'
+
+test('dashes in name', async ({ teardown, comment, same, equal, match }) => {
+  try {
+    await fs.unlink(desm.join(import.meta.url, 'fixtures', 'movies', 'db.sqlite'))
+  } catch {
+    // noop
+  }
+  const app = await buildServer(desm.join(import.meta.url, 'fixtures', 'movies', 'zero.db.json'))
+
+  await app.start()
+
+  const dir = await moveToTmpdir(teardown)
+
+  comment(`working in ${dir}`)
+  await execa('node', [desm.join(import.meta.url, '..', 'cli.mjs'), app.url + '/graphql', '--name', 'uncanny-movies'])
+
+  const readSDL = await fs.readFile(join(dir, 'uncanny-movies', 'uncanny-movies.schema.graphql'), 'utf8')
+  {
+    const schema = graphql.buildSchema(readSDL)
+    const sdl = graphql.printSchema(schema)
+    equal(sdl, readSDL)
+  }
+
+  comment(`server at ${app.url}`)
+
+  const toWrite = `
+'use strict'
+
+const Fastify = require('fastify')
+const movies = require('./uncanny-movies')
+const app = Fastify({ logger: true })
+
+app.register(movies, { url: '${app.url}' })
+app.post('/', async (request, reply) => {
+  const res = await app.uncannyMovies.graphql({
+    query: 'mutation { saveMovie(input: { title: "foo" }) { id, title } }'
+  })
+  return res
+})
+app.listen({ port: 0 })
+`
+  await fs.writeFile(join(dir, 'index.js'), toWrite)
+
+  const server2 = execa('node', ['index.js'])
+  teardown(() => server2.kill())
+  teardown(async () => { await app.close() })
+
+  const stream = server2.stdout.pipe(split(JSON.parse))
+
+  // this is unfortuate :(
+  const base = 'Server listening at '
+  let url
+  for await (const line of stream) {
+    const msg = line.msg
+    if (msg.indexOf(base) !== 0) {
+      continue
+    }
+    url = msg.slice(base.length)
+    break
+  }
+  if (!url) {
+    throw new Error('no url was found')
+  }
+  const res = await request(url, {
+    method: 'POST'
+  })
+  const body = await res.body.json()
+  match(body, {
+    title: 'foo'
+  })
+})
+
+test('dashes in name (typescript)', async ({ teardown, comment, same, match }) => {
+  try {
+    await fs.unlink(desm.join(import.meta.url, 'fixtures', 'movies', 'db.sqlite'))
+  } catch {
+    // noop
+  }
+  const app = await buildServer(desm.join(import.meta.url, 'fixtures', 'movies', 'zero.db.json'))
+
+  await app.start()
+
+  const dir = await moveToTmpdir(teardown)
+
+  comment(`working in ${dir}`)
+  await execa('node', [desm.join(import.meta.url, '..', 'cli.mjs'), app.url + '/graphql', '--name', 'uncanny-movies'])
+
+  comment(`upstream URL is ${app.url}`)
+
+  const toWrite = `
+import Fastify from 'fastify';
+import movies from './uncanny-movies';
+
+const app = Fastify({ logger: true });
+app.register(movies, {
+  url: '${app.url}'
+});
+
+app.post('/', async () => {
+  const res = await app.uncannyMovies.graphql({
+    query: 'mutation { saveMovie(input: { title: "foo" }) { id, title } }'
+  })
+  return res
+})
+
+app.listen({ port: 0 });
+`
+
+  await fs.writeFile(join(dir, 'index.ts'), toWrite)
+
+  const tsconfig = JSON.stringify({
+    extends: 'fastify-tsconfig',
+    compilerOptions: {
+      outDir: 'build',
+      target: 'es2018',
+      moduleResolution: 'node',
+      lib: ['es2018']
+    }
+  }, null, 2)
+
+  await fs.writeFile(join(dir, 'tsconfig.json'), tsconfig)
+
+  const tsc = desm.join(import.meta.url, '..', 'node_modules', '.bin', 'tsc')
+  await execa(tsc)
+
+  // TODO how can we avoid this copy?
+  await copy(join(dir, 'uncanny-movies'), join(dir, 'build', 'uncanny-movies'))
+
+  const server2 = execa('node', ['build/index.js'])
+  teardown(() => server2.kill())
+  teardown(async () => { await app.close() })
+
+  const stream = server2.stdout.pipe(split(JSON.parse))
+
+  // this is unfortuate :(
+  const base = 'Server listening at '
+  let url
+  for await (const line of stream) {
+    const msg = line.msg
+    if (msg.indexOf(base) !== 0) {
+      continue
+    }
+    url = msg.slice(base.length)
+    break
+  }
+  comment(`client URL is ${url}`)
+  const res = await request(url, {
+    method: 'POST'
+  })
+  const body = await res.body.json()
+  match(body, {
+    title: 'foo'
+  })
+})
+
+test('different folder name', async ({ teardown, comment, same, equal, match }) => {
+  try {
+    await fs.unlink(desm.join(import.meta.url, 'fixtures', 'movies', 'db.sqlite'))
+  } catch {
+    // noop
+  }
+  const app = await buildServer(desm.join(import.meta.url, 'fixtures', 'movies', 'zero.db.json'))
+
+  await app.start()
+
+  const dir = await moveToTmpdir(teardown)
+
+  comment(`working in ${dir}`)
+  await execa('node', [desm.join(import.meta.url, '..', 'cli.mjs'), app.url + '/graphql', '--name', 'movies', '--folder', 'uncanny'])
+
+  const readSDL = await fs.readFile(join(dir, 'uncanny', 'movies.schema.graphql'), 'utf8')
+  {
+    const schema = graphql.buildSchema(readSDL)
+    const sdl = graphql.printSchema(schema)
+    equal(sdl, readSDL)
+  }
+
+  comment(`server at ${app.url}`)
+
+  const toWrite = `
+'use strict'
+
+const Fastify = require('fastify')
+const movies = require('./uncanny')
+const app = Fastify({ logger: true })
+
+app.register(movies, { url: '${app.url}' })
+app.post('/', async (request, reply) => {
+  const res = await app.movies.graphql({
+    query: 'mutation { saveMovie(input: { title: "foo" }) { id, title } }'
+  })
+  return res
+})
+app.listen({ port: 0 })
+`
+  await fs.writeFile(join(dir, 'index.js'), toWrite)
+
+  const server2 = execa('node', ['index.js'])
+  teardown(() => server2.kill())
+  teardown(async () => { await app.close() })
+
+  const stream = server2.stdout.pipe(split(JSON.parse))
+
+  // this is unfortuate :(
+  const base = 'Server listening at '
+  let url
+  for await (const line of stream) {
+    const msg = line.msg
+    if (msg.indexOf(base) !== 0) {
+      continue
+    }
+    url = msg.slice(base.length)
+    break
+  }
+  if (!url) {
+    throw new Error('no url was found')
+  }
+  const res = await request(url, {
+    method: 'POST'
+  })
+  const body = await res.body.json()
+  match(body, {
+    title: 'foo'
+  })
+})
+
+test('tilde in name', async ({ teardown, comment, same, equal, match }) => {
+  try {
+    await fs.unlink(desm.join(import.meta.url, 'fixtures', 'movies', 'db.sqlite'))
+  } catch {
+    // noop
+  }
+  const app = await buildServer(desm.join(import.meta.url, 'fixtures', 'movies', 'zero.db.json'))
+
+  await app.start()
+
+  const dir = await moveToTmpdir(teardown)
+
+  comment(`working in ${dir}`)
+  await execa('node', [desm.join(import.meta.url, '..', 'cli.mjs'), app.url + '/graphql', '--name', 'uncanny~movies'])
+
+  const readSDL = await fs.readFile(join(dir, 'uncanny~movies', 'uncanny~movies.schema.graphql'), 'utf8')
+  {
+    const schema = graphql.buildSchema(readSDL)
+    const sdl = graphql.printSchema(schema)
+    equal(sdl, readSDL)
+  }
+
+  comment(`server at ${app.url}`)
+
+  const toWrite = `
+'use strict'
+
+const Fastify = require('fastify')
+const movies = require('./uncanny~movies')
+const app = Fastify({ logger: true })
+
+app.register(movies, { url: '${app.url}' })
+app.post('/', async (request, reply) => {
+  const res = await app.uncannyMovies.graphql({
+    query: 'mutation { saveMovie(input: { title: "foo" }) { id, title } }'
+  })
+  return res
+})
+app.listen({ port: 0 })
+`
+  await fs.writeFile(join(dir, 'index.js'), toWrite)
+
+  const server2 = execa('node', ['index.js'])
+  teardown(() => server2.kill())
+  teardown(async () => { await app.close() })
+
+  const stream = server2.stdout.pipe(split(JSON.parse))
+
+  // this is unfortuate :(
+  const base = 'Server listening at '
+  let url
+  for await (const line of stream) {
+    const msg = line.msg
+    if (msg.indexOf(base) !== 0) {
+      continue
+    }
+    url = msg.slice(base.length)
+    break
+  }
+  if (!url) {
+    throw new Error('no url was found')
+  }
+  const res = await request(url, {
+    method: 'POST'
+  })
+  const body = await res.body.json()
+  match(body, {
+    title: 'foo'
+  })
+})
