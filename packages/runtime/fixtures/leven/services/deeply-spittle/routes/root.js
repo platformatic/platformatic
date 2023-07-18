c52b802e460b088ad725850efe28df908192ddcd
Matteo Collina <hello@matteocollina.com>
2023-07-18 15:19:09 +0200

    Add a nice error message in case of mispelled client name (#1270)
    * Add a nice error message in case of mispelled client name
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    ---------
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ packages/runtime/fixtures/leven/services/deeply-spittle/routes/root.js
@@ -0,0 +1,8 @@
+/// <reference types="@platformatic/service" />
+'use strict'
+/** @param {import('fastify').FastifyInstance} fastify */
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async (request, reply) => {
+    return { hello: fastify.example }
+  })
+}
