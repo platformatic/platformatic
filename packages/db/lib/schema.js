7d96cb4c4c7248b75b9590d300a4939f9216af4d
Wayne Gibson <wayne.gibson@spacecomx.com>
2023-07-31 04:06:24 -0400

    fix(platformatic db): updated graphql schema object to selectively ignore entity fields (#1317)
    Signed-off-by: waynegibson <wayne.gibson@spacecomx.com>

--- packages/db/lib/schema.js
+++ packages/db/lib/schema.js
@@ -62,9 +62,15 @@ const db = {
           },
           ignore: {
             type: 'object',
-            // TODO add support for column-level ignore
             additionalProperties: {
-              type: 'boolean'
+              anyOf: [{
+                type: 'boolean'
+              }, {
+                type: 'object',
+                additionalProperties: {
+                  type: 'boolean'
+                }
+              }]
             }
           },
           subscriptionIgnore: {
