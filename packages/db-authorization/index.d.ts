791063cee60cabe8980f45f38633508fb46ac69e
Mikael Karon <mikael@karon.se>
2023-08-07 21:34:22 +0200

    feat: generic `user` for `db-authorization`
    Signed-off-by: Mikael Karon <mikael@karon.se>

--- packages/db-authorization/index.d.ts
+++ packages/db-authorization/index.d.ts
@@ -5,8 +5,8 @@ import {
 import { type FastifyPluginAsync } from 'fastify'
 import { type FastifyUserPluginOptions } from 'fastify-user'
 
-export type OperationFunction = (args: {
-  user: any,
+export type OperationFunction<T> = (args: {
+  user: T,
   ctx: PlatformaticContext,
   where: WhereCondition
 }) => WhereCondition
@@ -19,38 +19,38 @@ export interface OperationFields {
   fields?: string[]
 }
 
-export type Operation = boolean | OperationFields & (OperationFunction | OperationChecks)
+export type Operation<T> = boolean | OperationFields & (OperationFunction<T> | OperationChecks)
 
-export type DefaultsFunction = (args: {
-  user: any,
+export type DefaultsFunction<T> = (args: {
+  user: T,
   ctx: PlatformaticContext,
   input: Object[]
 }) => any
-export type Defaults = DefaultsFunction | Record<string, any>
+export type Defaults<T> = DefaultsFunction<T> | Record<string, any>
 
-interface AuthorizationRuleBase {
+interface AuthorizationRuleBase<T> {
   role: string
-  defaults?: Defaults
-  find?: Operation
-  save?: Operation
-  delete?: Operation
+  defaults?: Defaults<T>
+  find?: Operation<T>
+  save?: Operation<T>
+  delete?: Operation<T>
 }
-export interface AuthorizationRuleEntity extends AuthorizationRuleBase {
+export interface AuthorizationRuleEntity<T> extends AuthorizationRuleBase<T> {
   entity: string
 }
-export interface AuthorizationRuleEntities extends AuthorizationRuleBase {
+export interface AuthorizationRuleEntities<T> extends AuthorizationRuleBase<T> {
   entities: string[]
 }
-export type AuthorizationRule = AuthorizationRuleEntity | AuthorizationRuleEntities
+export type AuthorizationRule<T> = AuthorizationRuleEntity<T> | AuthorizationRuleEntities<T>
 
 export type SetupDBAuthorizationUserDecorator = () => Promise<void>
-export type AddRulesForRoles = (rules: Iterable<AuthorizationRule>) => void
+export type AddRulesForRoles = <T>(rules: Iterable<AuthorizationRule<T>>) => void
 
-export interface DBAuthorizationPluginOptions extends FastifyUserPluginOptions {
+export interface DBAuthorizationPluginOptions<T = any> extends FastifyUserPluginOptions {
   adminSecret?: string
   roleKey?: string
   anonymousRole?: string
-  rules: AuthorizationRule[]
+  rules: Array<AuthorizationRule<T>>
 }
 
 declare module '@platformatic/types' {
