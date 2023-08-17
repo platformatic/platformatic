75306f0e860f5a00f0accf6eaa25da0529a8d6ff
marcopiraccini <marco.piraccini@gmail.com>
2023-08-17 10:19:15 +0200

    fixups after review
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- packages/telemetry/lib/formatParamUrl.js
+++ packages/telemetry/lib/formatParamUrl.js
@@ -3,7 +3,28 @@
 // istanbul ignore file
 
 // The above is from: https://github.com/fastify/fastify-swagger/blob/master/lib/util/common.js#L104
-// https://github.com/fastify/fastify-swagger/blob/master/LICENSE
+// With license:
+// MIT License
+
+// Copyright (c) 2017 Fastify
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 // The function is not exported from that module, so we copy it here.
 // For this reason, we exlude this file from code coverage.
