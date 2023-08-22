0cb04db174495c4db34fb7992815767ce10882ea
Leonardo Rossi <leonardo.rossi@gmail.com>
2023-08-22 16:31:21 +0200

    Support name option in frontend client
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- packages/frontend-template/test/openapi.test.js
+++ packages/frontend-template/test/openapi.test.js
@@ -4,9 +4,12 @@ import { test } from 'tap'
 import { buildServer } from '@platformatic/db'
 import { join } from 'path'
 import { processOpenAPI } from '../lib/gen-openapi.mjs'
-import fs from 'fs/promises'
+import fs, { readFile } from 'fs/promises'
 import { request } from 'undici'
 import * as url from 'url'
+import { cliPath, moveToTmpdir } from './helper.js'
+import { execa } from 'execa'
+
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 test('build basic client from url', async ({ teardown, same, match }) => {
@@ -51,4 +54,36 @@ test('build basic client from url', async ({ teardown, same, match }) => {
   }
 }`
   match(implementation, expectedImplementation)
+})
+
+test('generate correct file names', async ({ teardown, ok }) => {
+  try {
+    await fs.unlink(join(__dirname, 'fixtures', 'movies', 'db.sqlite'))
+  } catch {
+    // noop
+  }
+  const app = await buildServer(join(__dirname, 'fixtures', 'sample', 'platformatic.db.json'))
+
+  teardown(async () => {
+    await app.close()
+  })
+
+  await app.start()
+
+  const dir = await moveToTmpdir(teardown)
+
+  // With --name will create foobar.ts and foobar-types.d.ts
+  await execa('node', [cliPath, app.url, 'ts', '--name', 'foobar'])
+  ok(await readFile(join(dir, 'foobar.ts')))
+  ok(await readFile(join(dir, 'foobar-types.d.ts')))
+
+  // Without --name will create api.ts and api-types.d.ts
+  await execa('node', [cliPath, app.url, 'ts'])
+  ok(await readFile(join(dir, 'api.ts')))
+  ok(await readFile(join(dir, 'api-types.d.ts')))
+
+  // Convert dashes to camelCase
+  await execa('node', [cliPath, app.url, 'ts', '--name', 'sample-name'])
+  ok(await readFile(join(dir, 'sampleName.ts')))
+  ok(await readFile(join(dir, 'sampleName-types.d.ts')))
 })
