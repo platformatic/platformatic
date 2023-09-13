75a2a686258d54f6aaec624e8494c06b2f8d6aee
Leonardo Rossi <leonardo.rossi@gmail.com>
2023-09-13 11:24:12 +0200

    Removes db-ra-data-rest pacakge
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,5 @@
 const { exec } = require('child_process')
 
 if (!process.env.CI) {
-  console.log('Running ra-data-rest build script')
-  exec('pnpm run ra-data-rest build').stdout.pipe(process.stdout)
 
 }
