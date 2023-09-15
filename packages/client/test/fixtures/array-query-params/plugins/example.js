c0166ae5b5888664b6f6b5600a011a1623c23787
Roberto Bianchi <roberto.bianchi@spendesk.com>
2023-09-15 13:02:46 +0200

    fix(client): Properly pass array querystring
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/client/test/fixtures/array-query-params/plugins/example.js
+++ packages/client/test/fixtures/array-query-params/plugins/example.js
@@ -5,7 +5,8 @@ module.exports = async function (fastify, opts) {
   fastify.get('/query', async (req, res) => {
     return {
       isArray: Array.isArray(req.query.ids),
-      ids: req.query.ids
+      ids: req.query.ids,
+      stringArrayUnion: req.query.stringArrayUnion
     }
   })
 }
