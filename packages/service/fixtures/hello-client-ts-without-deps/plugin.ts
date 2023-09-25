3dd3b2923dd3a806043a3a5a9796aebcc2339f57
Leonardo Rossi <leonardo.rossi@gmail.com>
2023-09-25 10:02:40 +0200

    update fixtures in service tests
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- packages/service/fixtures/hello-client-ts-without-deps/plugin.ts
+++ packages/service/fixtures/hello-client-ts-without-deps/plugin.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify'
 /// <reference path="./hello" />
 
 export default async function (app: FastifyInstance) {
-  app.get('/', async () => {
-    return app.hello.get({})
+  app.get('/', async (req) => {
+    return req.hello.get({})
   })
 }
