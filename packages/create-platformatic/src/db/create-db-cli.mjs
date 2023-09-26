eaa4e868c16d00fadabd0cb463f87460c907dcb5
Leonardo Rossi <leonardo.rossi@gmail.com>
2023-09-26 18:04:04 +0200

    Remove default true value to boolean args (#1602)
    * Remove default true value to boolean args
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- packages/create-platformatic/src/db/create-db-cli.mjs
+++ packages/create-platformatic/src/db/create-db-cli.mjs
@@ -55,10 +55,7 @@ export function parseDBArgs (_args) {
     default: {
       hostname: '127.0.0.1',
       database: 'sqlite',
-      migrations: 'migrations',
-      plugin: true,
-      types: true,
-      typescript: false
+      migrations: 'migrations'
     },
     alias: {
       h: 'hostname',
@@ -126,23 +123,24 @@ const createPlatformaticDB = async (_args, opts) => {
       break
     }
   }
-
-  const wizardOptions = await inquirer.prompt([{
+  const wizardPrompts = [{
     type: 'list',
     name: 'defaultMigrations',
     message: 'Do you want to create default migrations?',
     default: true,
     choices: [{ name: 'yes', value: true }, { name: 'no', value: false }]
-  }, {
-    type: 'list',
-    name: 'generatePlugin',
-    message: 'Do you want to create a plugin?',
-    default: args.plugin,
-    choices: [{ name: 'yes', value: true }, { name: 'no', value: false }]
-  },
-  getUseTypescript(args.typescript)
-  ])
+  }]
 
+  if (args.plugin === false) {
+    wizardPrompts.push({
+      type: 'list',
+      name: 'generatePlugin',
+      message: 'Do you want to create a plugin?',
+      default: true,
+      choices: [{ name: 'yes', value: true }, { name: 'no', value: false }]
+    })
+  }
+  const wizardOptions = await inquirer.prompt(wizardPrompts, getUseTypescript(args.typescript))
   if (!isRuntimeContext) {
     const { port } = await inquirer.prompt([getPort(args.port)])
     wizardOptions.port = port
