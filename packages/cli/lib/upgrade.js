ea15de14e54c4978341f5fe41a4f0a66b0e24520
Leonardo Rossi <leonardo.rossi@gmail.com>
2023-09-27 11:16:44 +0200

    Notify the user a new version of platformatic has been released
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- packages/cli/lib/upgrade.js
+++ packages/cli/lib/upgrade.js
@@ -3,7 +3,7 @@ import { analyze, write, upgrade as upgradeConfig } from '@platformatic/metaconf
 import parseArgs from 'minimist'
 import { access } from 'fs/promises'
 import { resolve } from 'path'
-
+import { request } from 'undici'
 const configFileNames = ConfigManager.listConfigFiles()
 
 async function isFileAccessible (filename) {
@@ -45,4 +45,46 @@ export async function upgrade (argv) {
 
   await write(meta)
   console.log('Upgraded to', meta.version)
+
+  console.log('Checking latest platformatic version on npm registry...')
+  const latestNpmVersion = await checkNpmVersion()
+  if (latestNpmVersion) {
+    const compareResult = compareVersions(meta.kind, latestNpmVersion)
+    switch (compareResult) {
+      case 0:
+        console.log('✅ You are running the latest Platformatic version!')
+        break
+      case 1:
+        console.log(`✨ Version ${latestNpmVersion} of Platformatic has been released, please update with "npm update -g platformatic"`)
+        break
+    }
+  }
+}
+
+async function checkNpmVersion () {
+  const res = await request('https://registry.npmjs.org/platformatic')
+
+  if (res.statusCode === 200) {
+    const json = await res.body.json()
+    return json['dist-tags'].latest
+  }
+  return null
+}
+
+export function compareVersions (first, second) {
+  const [firstMajor, firstMinor, firstPatch] = first.split('.')
+  const [secondMajor, secondMinor, secondPatch] = second.split('.')
+
+  if (firstMajor < secondMajor) return -1
+  if (firstMajor > secondMajor) return 1
+
+  // firstMajor === secondMajor
+  if (firstMinor < secondMinor) return -1
+  if (firstMinor > secondMinor) return 1
+
+  // firstMinor === secondMinor
+  if (firstPatch < secondPatch) return -1
+  if (firstPatch > secondPatch) return 1
+
+  return 0
 }
