19a0f49e5c197fdec393f49208d1fd5bbd1b48eb
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-09-28 20:57:46 +0200

    debug

--- packages/service/test/cli/compile-2.test.mjs
+++ packages/service/test/cli/compile-2.test.mjs
@@ -126,9 +126,9 @@ test('valid tsconfig file inside an inner folder', async (t) => {
 
       for (const p of level1) {
         console.log('killing', p.pid)
-        await safeKill(p.pid)
-        await execa('kill', ['-9', p.pid])
-        await execa('taskkill', ['/pid', p.pid, '/f', '/t'])
+        // await safeKill(p.pid)
+        // await execa('kill', ['-9', p.pid])
+        // await execa('taskkill', ['/pid', p.pid, '/f', '/t'])
         await execa('wmic', ['process', 'where', `ProcessId=${p.pid}`, 'delete'])
       }
     }, 5000)
