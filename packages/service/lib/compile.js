ac2e2680f193d62e2b79e3968919e6c562c29f5f
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-09-29 00:48:21 +0200

    debug

--- packages/service/lib/compile.js
+++ packages/service/lib/compile.js
@@ -1,84 +1,85 @@
 'use strict'
 
-const { resolve, join, dirname } = require('path')
-const pino = require('pino')
-const pretty = require('pino-pretty')
-const { loadConfig } = require('@platformatic/config')
-const { isFileAccessible } = require('./utils.js')
+// const { resolve, join } = require('path')
+// const pino = require('pino')
+// const pretty = require('pino-pretty')
+// const { loadConfig } = require('@platformatic/config')
+// const { isFileAccessible } = require('./utils.js')
 
-async function getTSCExecutablePath (cwd) {
-  const typescriptPath = require.resolve('typescript')
-  const typescriptPathCWD = require.resolve('typescript', { paths: [process.cwd()] })
+// async function getTSCExecutablePath (cwd) {
+//   const typescriptPath = require.resolve('typescript')
+//   const typescriptPathCWD = require.resolve('typescript', { paths: [process.cwd()] })
 
-  const tscLocalPath = join(typescriptPath, '..', '..', 'bin', 'tsc')
-  const tscGlobalPath = join(typescriptPathCWD, '..', '..', 'bin', 'tsc')
+//   const tscLocalPath = join(typescriptPath, '..', '..', 'bin', 'tsc')
+//   const tscGlobalPath = join(typescriptPathCWD, '..', '..', 'bin', 'tsc')
 
-  const [tscLocalExists, tscGlobalExists] = await Promise.all([
-    isFileAccessible(tscLocalPath),
-    isFileAccessible(tscGlobalPath)
-  ])
+//   const [tscLocalExists, tscGlobalExists] = await Promise.all([
+//     isFileAccessible(tscLocalPath),
+//     isFileAccessible(tscGlobalPath)
+//   ])
 
-  /* c8 ignore next 7 */
-  if (tscLocalExists) {
-    return tscLocalPath
-  }
+//   /* c8 ignore next 7 */
+//   if (tscLocalExists) {
+//     return tscLocalPath
+//   }
 
-  if (tscGlobalExists) {
-    return tscGlobalPath
-  }
-}
+//   if (tscGlobalExists) {
+//     return tscGlobalPath
+//   }
+// }
 
-async function setup (cwd, config, logger) {
-  if (!logger) {
-    logger = pino(
-      pretty({
-        translateTime: 'SYS:HH:MM:ss',
-        ignore: 'hostname,pid'
-      })
-    )
+// async function setup (cwd, config, logger) {
+//   if (!logger) {
+//     logger = pino(
+//       pretty({
+//         translateTime: 'SYS:HH:MM:ss',
+//         ignore: 'hostname,pid'
+//       })
+//     )
 
-    if (config?.server.logger) {
-      logger.level = config.server.logger.level
-    }
-  }
+//     if (config?.server.logger) {
+//       logger.level = config.server.logger.level
+//     }
+//   }
 
-  const { execa } = await import('execa')
+//   const { execa } = await import('execa')
 
-  const tscExecutablePath = await getTSCExecutablePath(cwd)
-  /* c8 ignore next 4 */
-  if (tscExecutablePath === undefined) {
-    const msg = 'The tsc executable was not found.'
-    logger.warn(msg)
-  }
+//   const tscExecutablePath = await getTSCExecutablePath(cwd)
+//   /* c8 ignore next 4 */
+//   if (tscExecutablePath === undefined) {
+//     const msg = 'The tsc executable was not found.'
+//     logger.warn(msg)
+//   }
 
-  const tsConfigPath = config?.plugins?.typescript?.tsConfig || resolve(cwd, 'tsconfig.json')
-  const tsConfigExists = await isFileAccessible(tsConfigPath)
+//   const tsConfigPath = config?.plugins?.typescript?.tsConfig || resolve(cwd, 'tsconfig.json')
+//   const tsConfigExists = await isFileAccessible(tsConfigPath)
 
-  if (!tsConfigExists) {
-    const msg = 'No typescript configuration file was found, skipping compilation.'
-    logger.info(msg)
-  }
+//   if (!tsConfigExists) {
+//     const msg = 'No typescript configuration file was found, skipping compilation.'
+//     logger.info(msg)
+//   }
 
-  return { execa, logger, tscExecutablePath, tsConfigPath, tsConfigExists }
-}
+//   return { execa, logger, tscExecutablePath, tsConfigPath, tsConfigExists }
+// }
 
 async function compile (cwd, config, originalLogger) {
-  const { execa, logger, tscExecutablePath, tsConfigPath, tsConfigExists } = await setup(cwd, config, originalLogger)
+  // const { execa, logger, tscExecutablePath, tsConfigPath, tsConfigExists } = await setup(cwd, config, originalLogger)
   /* c8 ignore next 3 */
-  if (!tscExecutablePath || !tsConfigExists) {
-    return false
-  }
+  // if (!tscExecutablePath || !tsConfigExists) {
+  //   return false
+  // }
 
   try {
-    const tsFlags = config?.plugins?.typescript?.flags || ['--project', tsConfigPath, '--rootDir', '.']
-    console.log('ts flags', tsFlags)
-    console.log('tscExecutablePath', tscExecutablePath)
+    // const tsFlags = config?.plugins?.typescript?.flags || ['--project', tsConfigPath, '--rootDir', '.']
+    // console.log('ts flags', tsFlags)
+    // console.log('tscExecutablePath', tscExecutablePath)
 
     // const child = execa(tscExecutablePath, [], {
     //   cwd,
     //   killSignal: 'SIGKILL'
     // })
 
+    const { execa } = await import('execa')
     const child = execa('pwd', [], {
       // cwd,
       killSignal: 'SIGKILL'
@@ -91,30 +92,30 @@ async function compile (cwd, config, originalLogger) {
 
     await child
 
-    logger.info('Typescript compilation completed successfully.')
+    console.log('Typescript compilation completed successfully.')
     return true
   } catch (error) {
-    logger.error(error.message)
+    console.log(error.message)
     return false
   }
 }
 
 function buildCompileCmd (app) {
   return async function compileCmd (_args) {
-    let fullPath = null
-    let config = null
-    try {
-      const { configManager } = await loadConfig({}, _args, app, {
-        watch: false
-      })
-      await configManager.parseAndValidate()
-      config = configManager.current
-      fullPath = dirname(configManager.fullPath)
-      /* c8 ignore next 4 */
-    } catch (err) {
-      console.error(err)
-      process.exit(1)
-    }
+    const fullPath = null
+    const config = null
+    // try {
+    //   // const { configManager } = await loadConfig({}, _args, app, {
+    //   //   watch: false
+    //   // })
+    //   // await configManager.parseAndValidate()
+    //   // config = configManager.current
+    //   // fullPath = dirname(configManager.fullPath)
+    //   /* c8 ignore next 4 */
+    // } catch (err) {
+    //   console.error(err)
+    //   process.exit(1)
+    // }
 
     if (!await compile(fullPath, config)) {
       process.exit(1)
