dd06c7afc67d5fd013983cfbe1d4865832bee042
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-09-28 17:57:24 +0200

    debug

--- packages/service/test/runner.js
+++ packages/service/test/runner.js
@@ -3,7 +3,7 @@
 const { spawn } = require('node:child_process')
 const glob = require('glob')
 
-const testFiles = glob.sync('test/**/*compile-{2}*.test.{js,mjs}')
+const testFiles = glob.sync('test/**/*compile-2*.test.{js,mjs}')
 
 spawn(process.execPath, ['--test', ...testFiles], { stdio: 'inherit' })
   .on('exit', process.exit)
