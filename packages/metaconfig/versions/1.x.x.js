0e658fabbf141f2254787a81ba9c49cfd0818562
Matteo Collina <hello@matteocollina.com>
2023-10-03 16:53:37 +0200

    Bumped v1.1.0
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/metaconfig/versions/1.x.x.js
+++ packages/metaconfig/versions/1.x.x.js
@@ -1,11 +1,34 @@
 'use strict'
 
 const SimpleZeroConfig = require('./simple-zero-config.js')
+const { version } = require('../package.json')
+const semver = require('semver')
+const rfdc = require('rfdc')()
 
 class OneSeries extends SimpleZeroConfig {
   constructor (opts) {
     super(opts)
     this.config.$schema = `https://platformatic.dev/schemas/v${this.version}/${this.kind}`
+
+    let increment = 'minor'
+    if (semver.satisfies(version, '~' + this.version)) {
+      if (!semver.eq(version, this.version)) {
+        increment = 'patch'
+      } else {
+        return
+      }
+    } else if (semver.gt(this.version, version)) {
+      return
+    }
+
+    this.up = () => {
+      const original = this.config
+      const config = rfdc(original)
+      const version = semver.inc(this.version, increment)
+      config.$schema = `https://platformatic.dev/schemas/v${version}/${this.kind}`
+
+      return new OneSeries({ config, path: this.path, format: this.format, version })
+    }
   }
 }
 
