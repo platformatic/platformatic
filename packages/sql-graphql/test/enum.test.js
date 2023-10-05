a802aec54a4e95539f9b75a9ea07bd5b9808b734
Tyson <tyson@TNSE-2200-MBP.local>
2023-10-04 17:47:47 -0700

    fixes(#1641) sql-graphql enum values may contain spaces
    Signed-off-by: Tyson <tysonsworld@gmail.com>

--- packages/sql-graphql/test/enum.test.js
+++ packages/sql-graphql/test/enum.test.js
@@ -47,6 +47,47 @@ test('should properly setup the enum types', { skip: isSQLite }, async ({ pass, 
   }
 })
 
+test('should not fail if enum value contains a space ', { skip: isSQLite }, async ({ pass, teardown, same }) => {
+  const app = fastify()
+  app.register(sqlMapper, {
+    ...connInfo,
+    async onDatabaseLoad (db, sql) {
+      pass('onDatabaseLoad called')
+
+      await clear(db, sql)
+
+      if (isPg) {
+        await db.query(sql`
+        CREATE TYPE custom_enum AS ENUM('Field 1', 'Field .', '. Field', ' f 1 ');
+
+        CREATE TABLE enum_tests (
+          id INTEGER NOT NULL,
+          test_enum custom_enum,
+          PRIMARY KEY (id)
+        );`)
+      } else {
+        await db.query(sql`
+        CREATE TABLE enum_tests (
+          id INTEGER NOT NULL,
+          test_enum ENUM ('Field 1', 'Field .', '. Field', ' f 1 ') DEFAULT NULL,
+          PRIMARY KEY (id)
+        );`)
+      }
+    }
+  })
+
+  app.register(sqlGraphQL)
+  teardown(app.close.bind(app))
+
+  try {
+    await app.ready()
+    same(true, true, 'Enum with spaces are properly interpreted')
+  } catch (err) {
+    console.log(err)
+    same(true, false, 'Previous call should never fail')
+  }
+})
+
 test('should not fail if tables have duplicate enum names', { skip: isSQLite }, async ({ pass, teardown, same }) => {
   const app = fastify()
   app.register(sqlMapper, {
