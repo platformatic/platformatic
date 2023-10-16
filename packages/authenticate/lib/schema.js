9589cba552fd5a3dc22351fb72df2dc86e29b87d
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-10-16 14:21:57 +0200

    feat: update plt login command

--- packages/authenticate/lib/schema.js
+++ packages/authenticate/lib/schema.js
@@ -1,7 +1,25 @@
-export default {
-  $id: 'https://schemas.platformatic.dev/auth',
+import { fileURLToPath } from 'node:url'
+import { readFileSync } from 'node:fs'
+
+const pkg = JSON.parse(readFileSync(new URL('../package.json', import.meta.url)))
+const version = 'v' + pkg.version
+
+const loginSchema = {
+  $id: `https://platformatic.dev/schemas/${version}/login`,
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  title: 'Platformatic authorization config schema',
   type: 'object',
   properties: {
-    accessToken: { type: 'string' }
-  }
+    $schema: { type: 'string' },
+    userApiKey: { type: 'string' }
+  },
+  required: ['userApiKey'],
+  additionalProperties: false
+}
+
+export default loginSchema
+
+/* c8 ignore next 3 */
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  console.log(JSON.stringify(loginSchema, null, 2))
 }
