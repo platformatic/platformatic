5120fb9b95a20383f7ffd6a215f379648828de00
Simone Sanfratello <simone.sanfra@gmail.com>
2023-10-19 14:58:41 +0200

    Add graphql dependency in cli
    Signed-off-by: Simone Sanfratello <simone.sanfra@gmail.com>

--- packages/composer/test/graphql/watch.test.js
+++ packages/composer/test/graphql/watch.test.js
@@ -5,38 +5,9 @@ const { test } = require('node:test')
 const { setTimeout } = require('node:timers/promises')
 const path = require('node:path')
 const dedent = require('dedent')
-const { createGraphqlService, createComposer } = require('../helper')
+const { createGraphqlService, createComposer, createLoggerSpy } = require('../helper')
 
 const REFRESH_TIMEOUT = 500
-
-// TODO move to helper or so
-function createLoggerSpy () {
-  return {
-    _trace: [],
-    _debug: [],
-    _info: [],
-    _warn: [],
-    _error: [],
-    _fatal: [],
-
-    trace: function (...args) { this._trace.push(args) },
-    debug: function (...args) { this._debug.push(args) },
-    info: function (...args) { this._info.push(args) },
-    warn: function (...args) { this._warn.push(args) },
-    error: function (...args) { this._error.push(args) },
-    fatal: function (...args) { this._fatal.push(args) },
-    child: function () { return this },
-
-    reset: function () {
-      this._trace = []
-      this._debug = []
-      this._info = []
-      this._warn = []
-      this._error = []
-      this._fatal = []
-    }
-  }
-}
 
 test('should restart composer if a service has been changed, and update the schema', async (t) => {
   const schema1 = dedent`
