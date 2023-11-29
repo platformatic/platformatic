0411392b087a779fb64db1b5cd493f5f5a4fbaa4
Leonardo Rossi <leonardo.rossi@gmail.com>
2023-11-29 15:50:50 +0100

    Adds simple types for runtime
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- packages/runtime/index.d.ts
+++ packages/runtime/index.d.ts
@@ -1,6 +1,7 @@
 import { InjectOptions, LightMyRequestResponse } from 'fastify'
 import { FastifyError } from '@fastify/error'
-
+import { BaseLogger } from 'pino'
+import { RuntimeGenerator } from './lib/generator/runtime-generator'
 export type pltRuntimeBuildServer = {
   address: string
   port: number
@@ -11,6 +12,12 @@ export type pltRuntimeBuildServer = {
 
 declare module '@platformatic/runtime' {
   export function buildServer(opts: object): Promise<pltRuntimeBuildServer>
+  export function start(args: object): Promise<object>
+  export function startCommand(args: object): Promise<void>
+  export function loadConfig(minimistConfig: object, args: object, store: object, overrides: object, replaceEnv: boolean): void
+  export function compile(argv: string[], logger: BaseLogger): void
+  export function platformaticRuntime(): Promise<void>
+  export const Generator: RuntimeGenerator
 }
 
 /**
