d0c86c185082955f8fb003507e045109bd7d3638
Matteo Collina <hello@matteocollina.com>
2023-11-30 14:03:09 +0100

    added runtime test
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/create-platformatic-auto/test/cli/runtime.test.mjs
+++ packages/create-platformatic-auto/test/cli/runtime.test.mjs
@@ -20,52 +20,26 @@ test.afterEach(async () => {
   }
 })
 
-test('Creates a Platformatic Runtime with two Services', { timeout, skip: true }, async () => {
+test('Creates a Platformatic Runtime with two Services', { timeout }, async () => {
   // The actions must match IN ORDER
   const actions = [{
-    match: 'Which kind of project do you want to create?',
-    do: [keys.DOWN, keys.DOWN, keys.DOWN, keys.ENTER] // Runtime
-  }, {
     match: 'Where would you like to create your project?',
     do: [keys.ENTER]
   }, {
-    match: 'Where would you like to load your services from?',
-    do: [keys.ENTER]
+    match: 'Which kind of project do you want to create?',
+    do: [keys.ENTER] // Service
   }, {
-    match: 'Do you want to create the github action to deploy',
-    do: [keys.DOWN, keys.ENTER] // no
-  }, {
-    match: 'Do you want to enable PR Previews in your application',
-    do: [keys.DOWN, keys.ENTER] // no
-  }, {
-    match: 'Do you want to init the git repository',
-    do: [keys.DOWN, keys.ENTER] // yes
-  }, {
-    // FIRST SERVICE
     match: 'What is the name of the service?',
     do: [keys.ENTER]
-  }, {
-    match: 'Which kind of project do you want to create?',
-    do: [keys.DOWN, keys.ENTER] // Service
-  }, {
-    match: 'Do you want to use TypeScript',
-    do: [keys.DOWN, keys.ENTER], // no
-    waitAfter: 5000 // here generates teh types for the service, so we need to wait more
   }, {
     match: 'Do you want to create another service?',
     do: [keys.ENTER] // yes
   }, {
-    // SECOND SERVICE
+    match: 'Which kind of project do you want to create?',
+    do: [keys.ENTER] // Service
+  }, {
     match: 'What is the name of the service?',
     do: [keys.ENTER]
-  }, {
-    match: 'Which kind of project do you want to create?',
-    do: [keys.DOWN, keys.ENTER] // Service
-  }, {
-    // NOTE THAT HERE THE DEFAULT OPTION FOR SERVICE IS "YES"
-    match: 'Do you want to use TypeScript',
-    do: [keys.DOWN, keys.ENTER], // no
-    waitAfter: 5000 // here generates teh types for the service, so we need to wait more
   }, {
     match: 'Do you want to create another service?',
     do: [keys.DOWN, keys.ENTER] // no
@@ -73,41 +47,43 @@ test('Creates a Platformatic Runtime with two Services', { timeout, skip: true }
     match: 'Which service should be exposed?',
     do: [keys.ENTER]
   }, {
+    match: 'Do you want to use TypeScript',
+    do: [keys.DOWN, keys.ENTER] // yes
+  }, {
     match: 'What port do you want to use?',
     do: [keys.ENTER]
+  }, {
+    match: 'Do you want to init the git repository',
+    do: [keys.DOWN, keys.ENTER] // yes
   }]
-  await executeCreatePlatformatic(tmpDir, actions, 'All done!')
+  await executeCreatePlatformatic(tmpDir, actions, 'You are all set!')
 
-  const baseProjectDir = join(tmpDir, 'platformatic-runtime')
+  const baseProjectDir = join(tmpDir, 'platformatic')
   const files = await walk(baseProjectDir)
   console.log('==> created files', files)
   equal(await isFileAccessible(join(baseProjectDir, '.gitignore')), true)
   equal(await isFileAccessible(join(baseProjectDir, '.env')), true)
   equal(await isFileAccessible(join(baseProjectDir, '.env.sample')), true)
-  equal(await isFileAccessible(join(baseProjectDir, 'platformatic.runtime.json')), true)
+  equal(await isFileAccessible(join(baseProjectDir, 'platformatic.json')), true)
   equal(await isFileAccessible(join(baseProjectDir, 'README.md')), true)
-  equal(await isFileAccessible(join(baseProjectDir, '.github', 'workflows', 'platformatic-dynamic-workspace-deploy.yml')), false)
-  equal(await isFileAccessible(join(baseProjectDir, '.github', 'workflows', 'platformatic-static-workspace-deploy.yml')), false)
   equal(await isFileAccessible(join(baseProjectDir, '.git', 'config')), true)
 
   // Here check the generated services
   const services = await getServices(join(baseProjectDir, 'services'))
   equal(services.length, 2)
   const baseService0Dir = join(baseProjectDir, 'services', services[0])
-  equal(await isFileAccessible(join(baseService0Dir, '.env.sample')), true)
-  equal(await isFileAccessible(join(baseService0Dir, 'platformatic.service.json')), true)
+  equal(await isFileAccessible(join(baseService0Dir, 'platformatic.json')), true)
   equal(await isFileAccessible(join(baseService0Dir, 'README.md')), true)
-  equal(await isFileAccessible(join(baseService0Dir, 'routes', 'root.js')), true)
-  equal(await isFileAccessible(join(baseService0Dir, 'plugins', 'example.js')), true)
+  equal(await isFileAccessible(join(baseService0Dir, 'routes', 'root.ts')), true)
+  equal(await isFileAccessible(join(baseService0Dir, 'plugins', 'example.ts')), true)
   // TODO: re-enable when we fix this https://github.com/platformatic/platformatic/issues/1657
   // equal(await isFileAccessible(join(baseService0Dir, 'global.d.ts')), true)
 
   const baseService1Dir = join(baseProjectDir, 'services', services[1])
-  equal(await isFileAccessible(join(baseService1Dir, '.env.sample')), true)
-  equal(await isFileAccessible(join(baseService1Dir, 'platformatic.service.json')), true)
+  equal(await isFileAccessible(join(baseService1Dir, 'platformatic.json')), true)
   equal(await isFileAccessible(join(baseService1Dir, 'README.md')), true)
-  equal(await isFileAccessible(join(baseService1Dir, 'routes', 'root.js')), true)
-  equal(await isFileAccessible(join(baseService1Dir, 'plugins', 'example.js')), true)
+  equal(await isFileAccessible(join(baseService1Dir, 'routes', 'root.ts')), true)
+  equal(await isFileAccessible(join(baseService1Dir, 'plugins', 'example.ts')), true)
   // TODO: re-enable when we fix this https://github.com/platformatic/platformatic/issues/1657
   // equal(await isFileAccessible(join(baseService1Dir, 'global.d.ts')), true)
 })
