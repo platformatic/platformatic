72be92e03ea5d35a2f14e697a6fa3a9da6fbed34
Matteo Collina <hello@matteocollina.com>
2023-12-05 10:15:23 +0100

    exit with code 1 on failure
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/composer/test/runner.js
+++ packages/composer/test/runner.js
@@ -15,8 +15,14 @@ const files = [
   ...glob(path.join(__dirname, 'cli', '*.test.js'))
 ]
 
-run({
+const stream = run({
   files,
   concurrency: 1,
   timeout: 30000
-}).compose(reporter).pipe(process.stdout)
+})
+
+stream.on('test:fail', () => {
+  process.exitCode = 1
+})
+
+stream.compose(reporter).pipe(process.stdout)
