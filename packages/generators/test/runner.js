934155c37a9e2a9a490eb1f84ba144467c82410d
Matteo Collina <hello@matteocollina.com>
2023-12-05 10:18:10 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/generators/test/runner.js
+++ packages/generators/test/runner.js
@@ -13,8 +13,14 @@ const files = [
   ...glob(path.join(__dirname, 'cli', '*.test.mjs'))
 ]
 
-run({
+const stream = run({
   files,
   concurrency: 1,
   timeout: 30000
-}).compose(reporter).pipe(process.stdout)
+})
+
+stream.on('test:fail', () => {
+  process.exitCode = 1
+})
+
+stream.compose(reporter).pipe(process.stdout)
