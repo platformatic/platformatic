555674d29cdb626f24a8e60c807b397b39e605c3
Leonardo Rossi <leonardo.rossi@gmail.com>
2023-12-12 16:25:01 +0100

    Remove error and add test
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

--- packages/db/lib/errors.js
+++ packages/db/lib/errors.js
@@ -9,7 +9,6 @@ module.exports = {
   UknonwnDatabaseError: createError(`${ERROR_PREFIX}_UNKNOWN_DATABASE_ERROR`, 'Unknown database'),
   MigrateMissingMigrationsDirError: createError(`${ERROR_PREFIX}_MIGRATE_ERROR`, 'Migrations directory %s does not exist'),
   MissingSeedFileError: createError(`${ERROR_PREFIX}_MISSING_SEED_FILE_ERROR`, 'Missing seed file'),
-  MigrationsToApplyError: createError(`${ERROR_PREFIX}_MIGRATIONS_TO_APPLY_ERROR`, 'You have migrations to apply. Please run `platformatic db migrations apply` first.'),
-  MigrateUnknownDatabaseError: createError(`${ERROR_PREFIX}_UNKNOWN_DATABASE_ERROR`, 'Unknown database %s')
+  MigrationsToApplyError: createError(`${ERROR_PREFIX}_MIGRATIONS_TO_APPLY_ERROR`, 'You have migrations to apply. Please run `platformatic db migrations apply` first.')
 
 }
