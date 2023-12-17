ae98e733889b2e3a98556fa8db0b5c240b8fdc85
AbhilakshSinghReen <abhilakshreen@gmail.com>
2023-12-17 21:01:41 +0530

    Added test to check support for basic auth when metrics are being server on the parent server.
    Signed-off-by: AbhilakshSinghReen <abhilakshreen@gmail.com>

--- packages/service/test/metrics.test.js
+++ packages/service/test/metrics.test.js
@@ -155,6 +155,56 @@ test('has /metrics endpoint on parent server', async (t) => {
   testPrometheusOutput(body)
 })
 
+test('support basic auth with metrics on parent server', async (t) => {
+  const app = await buildServer({
+    server: {
+      hostname: '127.0.0.1',
+      port: 3042
+    },
+    metrics: {
+      server: 'parent',
+      auth: {
+        username: 'foo',
+        password: 'bar'
+      }
+    }
+  })
+
+  t.after(async () => {
+    await app.close()
+  })
+  await app.start()
+
+  {
+    const res = await (request('http://127.0.0.1:3042/metrics'))
+    assert.strictEqual(res.statusCode, 401)
+    assert.match(res.headers['content-type'], /^application\/json/)
+  }
+
+  {
+    // wrong credentials
+    const res = await (request('http://127.0.0.1:3042/metrics', {
+      headers: {
+        authorization: `Basic ${Buffer.from('bar:foo').toString('base64')}`
+      }
+    }))
+    assert.strictEqual(res.statusCode, 401)
+    assert.match(res.headers['content-type'], /^application\/json/)
+  }
+
+  {
+    const res = await (request('http://127.0.0.1:3042/metrics', {
+      headers: {
+        authorization: `Basic ${Buffer.from('foo:bar').toString('base64')}`
+      }
+    }))
+    assert.strictEqual(res.statusCode, 200)
+    assert.match(res.headers['content-type'], /^text\/plain/)
+    const body = await res.body.text()
+    testPrometheusOutput(body)
+  }
+})
+
 test('do not error on restart', async (t) => {
   const app = await buildServer({
     server: {
