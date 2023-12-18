8b347bed622d31fb203a7a809db33408f8e7ec52
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-12-18 14:41:12 +0100

    debug

--- packages/runtime/test/cli/watch-5.test.mjs
+++ packages/runtime/test/cli/watch-5.test.mjs
@@ -4,7 +4,6 @@ import { join } from 'node:path'
 import { test, beforeEach, afterEach } from 'node:test'
 import { setTimeout as sleep } from 'node:timers/promises'
 import desm from 'desm'
-import { request } from 'undici'
 import { start } from './helper.mjs'
 
 beforeEach(async (t) => {
@@ -41,27 +40,38 @@ function saferm (path) {
   return rm(path, { recursive: true, force: true }).catch(() => {})
 }
 
-test('should not watch files if watch = false', async (t) => {
+test('watches CommonJS files with hotreload on a single service', { timeout: 60000 }, async (t) => {
   const tmpDir = await mkdtemp(join(base, 'watch-'))
   t.after(() => saferm(tmpDir))
   t.diagnostic(`using ${tmpDir}`)
-  const configFileSrc = join(fixturesDir, 'configs', 'monorepo-watch.json')
-  const configFileDst = join(tmpDir, 'configs', 'monorepo-watch.json')
-  const appSrc = join(fixturesDir, 'monorepo-watch')
-  const appDst = join(tmpDir, 'monorepo-watch')
-  const cjsPluginFilePath = join(appDst, 'service1', 'plugin.js')
+  const appSrc = join(fixturesDir, 'monorepo', 'serviceAppWithLogger')
+  const appDst = join(tmpDir)
+  const cjsPluginFilePath = join(appDst, 'plugin.js')
 
   await Promise.all([
-    cp(configFileSrc, configFileDst),
     cp(appSrc, appDst, { recursive: true })
   ])
 
   await writeFile(cjsPluginFilePath, createCjsLoggingPlugin('v1', false))
-  const { child, url } = await start('-c', configFileDst)
+  const { child } = await start('-c', join(appDst, 'platformatic.service.json'))
   t.after(() => child.kill('SIGINT'))
+
   await writeFile(cjsPluginFilePath, createCjsLoggingPlugin('v2', true))
-  await sleep(5000)
-  const res = await request(`${url}/version`)
-  const version = await res.body.text()
-  assert.strictEqual(version, 'v1')
+
+  let restartedSecondTime = false
+  let restartedThirdTime = false
+
+  for await (const log of child.ndj) {
+    if (log.msg === 'RELOADED v2') {
+      restartedSecondTime = true
+    } else if (log.msg === 'RELOADED v3') {
+      assert.ok(restartedSecondTime)
+      restartedThirdTime = true
+      break
+    } else if (log.msg?.match(/listening/)) {
+      await writeFile(cjsPluginFilePath, createCjsLoggingPlugin('v3', true))
+    }
+  }
+
+  assert.ok(restartedThirdTime)
 })
