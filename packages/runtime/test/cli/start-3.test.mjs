f231d2d672f8fb67f76cfb3cc1286b993770526f
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-12-19 11:07:14 +0100

    debug

--- packages/runtime/test/cli/start-3.test.mjs
+++ packages/runtime/test/cli/start-3.test.mjs
@@ -12,24 +12,44 @@ setTimeout(() => {
 }, 40000).unref()
 
 test('stackable', async () => {
+  console.log('-------------start-cli 3.1 started')
   const config = join(import.meta.url, '..', '..', 'fixtures', 'stackables', 'platformatic.json')
+  console.log('-------------start-cli 3.1.1')
   const { child, url } = await start('-c', config)
+  console.log('-------------start-cli 3.1.2')
   const res = await request(url + '/foo')
 
+  console.log('-------------start-cli 3.1.3')
+
   assert.strictEqual(res.statusCode, 200)
+  console.log('-------------start-cli 3.1.4')
+
   assert.deepStrictEqual(await res.body.text(), 'Hello World')
+  console.log('-------------start-cli 3.1.5')
+
   child.kill('SIGINT')
+  console.log('-------------start-cli 3.1.6')
+
   await child.catch((error) => {
-    console.log('error1', error)
+    console.log('start 3.1 error', error)
   })
+  console.log('-------------start-cli 3.1 finished')
 })
 
 test('use runtime server', async () => {
+  console.log('-------------start-cli 3.2 started')
   const config = join(import.meta.url, '..', '..', 'fixtures', 'server', 'runtime-server', 'platformatic.runtime.json')
+  console.log('-------------start-cli 3.2.1')
   const { child, url } = await start('-c', config)
+  console.log('-------------start-cli 3.2.2')
   assert.strictEqual(url, 'http://127.0.0.1:14242')
+  console.log('-------------start-cli 3.2.3')
+
   child.kill('SIGINT')
+  console.log('-------------start-cli 3.2.4')
+
   await child.catch((error) => {
     console.log('error2', error)
   })
+  console.log('-------------start-cli 3.2 finished')
 })
--- packages/runtime/test/start-4.test.js
+++ packages/runtime/test/start-4.test.js
@@ -20,20 +20,32 @@ setTimeout(() => {
 test('supports logging using a transport', async (t) => {
   console.log('start-4 started')
   const configFile = join(fixturesDir, 'server', 'logger-transport', 'platformatic.runtime.json')
+  console.log('start-4 1.1')
   const dest = join(tmpdir, `logger-transport-${process.pid}.log`)
+  console.log('start-4 1.2')
   t.after(async function () {
+    console.log('start-4 1.3')
     await fs.unlink(dest)
+    console.log('start-4 1.4')
   })
+  console.log('start-4 1.5')
   const config = await loadConfig({}, ['-c', configFile], platformaticRuntime)
+  console.log('start-4 1.6')
   config.configManager.current.server.logger.transport.options = {
     path: dest
   }
+  console.log('start-4 1.7')
   const app = await buildServer(config.configManager.current)
+  console.log('start-4 1.8')
   await app.start()
+  console.log('start-4 1.9')
   await app.close()
+  console.log('start-4 1.10')
 
   const written = await fs.readFile(dest, 'utf8')
+  console.log('start-4 1.11')
   const parsed = JSON.parse(written)
+  console.log('start-4 1.12')
 
   assert.strictEqual(parsed.fromTransport, true)
   console.log('start-4 finished')
