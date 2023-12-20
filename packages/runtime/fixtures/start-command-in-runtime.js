ca71e084f5e90d8adaf5aa13b89cba12dcc0a833
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-12-20 11:47:54 +0100

    debug

--- packages/runtime/fixtures/start-command-in-runtime.js
+++ packages/runtime/fixtures/start-command-in-runtime.js
@@ -1,5 +1,23 @@
 'use strict'
 
+/*
+class FakeFinalizationRegistry {
+  constructor () {
+    process._rawDebug('FakeFinalizationRegistry: constructor', new Error().stack)
+  }
+  register () {}
+  unregister () {}
+}
+
+global.FinalizationRegistry = FakeFinalizationRegistry
+
+const oldFetch = global.fetch
+function fakefetch (...args) {
+  process._rawDebug('fakefetch', args, new Error().stack)
+  return oldFetch(...args)
+}
+*/
+
 const assert = require('node:assert')
 const { request, setGlobalDispatcher, Agent } = require('undici')
 const { startCommand } = require('..')
@@ -11,6 +29,7 @@ setGlobalDispatcher(new Agent({
 
 async function main () {
   const entrypoint = await startCommand(['-c', process.argv[2]])
+  process._rawDebug('start-command-in-runtime.js: started')
   const endpoint = process.argv[3] ?? '/'
   const res = await request(entrypoint + endpoint)
 
@@ -18,6 +37,7 @@ async function main () {
 
   // Consume the body so the service can end
   await res.body.text()
+  process._rawDebug('start-command-in-runtime.js: exiting')
   process.exit(42)
 }
 
