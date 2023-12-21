103099899a3ae6f4549bbc0e9caae9826cc62913
Matteo Collina <hello@matteocollina.com>
2023-12-21 15:47:52 +0100

    use SIGKILL
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/runtime/test/cli/start/start.9.test.mjs
+++ packages/runtime/test/cli/start/start.9.test.mjs
@@ -7,5 +7,5 @@ test('the runtime server overrides the entrypoint server', async () => {
   const config = join(import.meta.url, '..', '..', '..', 'fixtures', 'server', 'overrides-service', 'platformatic.runtime.json')
   const { child, url } = await start('-c', config)
   assert.strictEqual(url, 'http://127.0.0.1:14242')
-  child.kill('SIGINT')
+  child.kill('SIGKILL')
 })
