451070eed6a86b57b457dd3878816fd56c4ada10
Matteo Collina <hello@matteocollina.com>
2023-12-21 11:22:40 +0100

    db: load authorization before user plugins
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/db/test/authorization-setup.test.js
+++ packages/db/test/authorization-setup.test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const assert = require('node:assert/strict')
+const { join } = require('node:path')
 const { test } = require('node:test')
 const { buildServer } = require('..')
 const { buildConfigManager, getConnectionInfo, createBasicPages } = require('./helper')
@@ -73,4 +74,34 @@ test('configure authorizations works even with empty object', async (t) => {
       ]
     }, 'savePage response')
   }
+})
+
+test('addCustomRule', async (t) => {
+  const { connectionInfo, dropTestDB } = await getConnectionInfo()
+
+  const config = {
+    server: {
+      hostname: '127.0.0.1',
+      port: 0
+    },
+    authorization: {},
+    db: {
+      ...connectionInfo,
+      async onDatabaseLoad (db, sql) {
+        await createBasicPages(db, sql)
+      }
+    },
+    plugins: {
+      paths: [join(__dirname, 'fixtures', 'auth-in-code.js')]
+    }
+  }
+
+  const configManager = await buildConfigManager(config)
+  const app = await buildServer({ configManager })
+
+  t.after(async () => {
+    await app.close()
+    await dropTestDB()
+  })
+  await app.start()
 })
