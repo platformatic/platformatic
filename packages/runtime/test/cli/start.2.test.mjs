c297a4bd3a5a681d0573b31de7d24be65dc86df8
Matteo Collina <hello@matteocollina.com>
2023-12-21 14:20:46 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/runtime/test/cli/start.2.test.mjs
+++ packages/runtime/test/cli/start.2.test.mjs
@@ -1,5 +1,4 @@
 import assert from 'node:assert'
-import { once } from 'node:events'
 import { test } from 'node:test'
 import fs from 'node:fs/promises'
 import { join } from 'desm'
@@ -39,20 +38,4 @@ test('start command with js file', async (t) => {
   assert.deepStrictEqual(await res.body.json(), { hello: 'hello123' })
   child.kill('SIGINT')
   await child.catch(() => {})
-})
-
-test('handles uncaughtException', async (t) => {
-  const config = join(import.meta.url, '..', '..', 'fixtures', 'dbApp', 'platformatic.db.json')
-  const { child, url } = await start('-c', config)
-
-  t.after(async () => {
-    child.kill('SIGINT')
-  })
-  const res = await request(url + '/async_crash')
-
-  assert.strictEqual(res.statusCode, 200)
-  assert.deepStrictEqual(await res.body.text(), 'ok')
-
-  const [code] = await once(child, 'exit')
-  assert.strictEqual(code, 1)
 })
---
+++ packages/runtime/test/cli/start.3.test.mjs
@@ -0,0 +1,22 @@
+import assert from 'node:assert'
+import { once } from 'node:events'
+import { test } from 'node:test'
+import { join } from 'desm'
+import { request } from 'undici'
+import { start } from './helper.mjs'
+
+test('handles uncaughtException', async (t) => {
+  const config = join(import.meta.url, '..', '..', 'fixtures', 'dbApp', 'platformatic.db.json')
+  const { child, url } = await start('-c', config)
+
+  t.after(async () => {
+    child.kill('SIGINT')
+  })
+  const res = await request(url + '/async_crash')
+
+  assert.strictEqual(res.statusCode, 200)
+  assert.deepStrictEqual(await res.body.text(), 'ok')
+
+  const [code] = await once(child, 'exit')
+  assert.strictEqual(code, 1)
+})
