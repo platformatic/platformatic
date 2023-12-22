38178c12b7c0eb9e3b9daad367b3ad2b8c9b8d3d
Ivan Tymoshenko <ivan@tymoshenko.me>
2023-12-22 11:49:42 +0100

    debug

--- packages/db/test/config.test.js
+++ packages/db/test/config.test.js
@@ -6,6 +6,7 @@ const { buildServer } = require('..')
 const { buildConfigManager, getConnectionInfo } = require('./helper')
 
 test('should set pluginTimeout to 60s by default', async (t) => {
+  console.log('--------------> 1')
   const { connectionInfo, dropTestDB } = await getConnectionInfo()
 
   const config = {
@@ -18,15 +19,25 @@ test('should set pluginTimeout to 60s by default', async (t) => {
     }
   }
 
+  console.log('--------------> 2')
   const configManager = await buildConfigManager(config)
+  console.log('--------------> 3')
   const app = await buildServer({ configManager })
 
   t.after(async () => {
+    console.log('--------------> 7')
     await app.close()
+    console.log('--------------> 8')
     await dropTestDB()
+    console.log('--------------> 9')
   })
+
+  console.log('--------------> 4')
   await app.start()
 
+  console.log('--------------> 5')
   const appConfig = app.platformatic.configManager.current
   assert.equal(appConfig.server.pluginTimeout, 60 * 1000)
+
+  console.log('--------------> 6')
 })
