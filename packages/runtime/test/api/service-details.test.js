18c8e7347a8c7126c656bf5106af887cdda4334d
Matteo Collina <hello@matteocollina.com>
2024-02-04 01:13:22 +0100

    Hopefully green runtime CI now (#2053)
    * Hopefully green runtime CI now
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    * fixup
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>
    
    ---------
    
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ packages/runtime/test/api/service-details.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const assert = require('node:assert')
+const { join } = require('node:path')
+const { test } = require('node:test')
+
+const { loadConfig } = require('@platformatic/config')
+const { buildServer, platformaticRuntime } = require('../..')
+const fixturesDir = join(__dirname, '..', '..', 'fixtures')
+
+test('should get service details', async (t) => {
+  const configFile = join(fixturesDir, 'configs', 'monorepo.json')
+  const config = await loadConfig({}, ['-c', configFile], platformaticRuntime)
+  const app = await buildServer(config.configManager.current)
+
+  await app.start()
+
+  t.after(async () => {
+    await app.close()
+  })
+
+  const serviceDetails = await app.getServiceDetails('with-logger')
+  assert.deepStrictEqual(serviceDetails, {
+    id: 'with-logger',
+    status: 'started',
+    entrypoint: false,
+    localUrl: 'http://with-logger.plt.local',
+    dependencies: []
+  })
+})
