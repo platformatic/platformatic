825b6ee4be99e249803b3ab0208c2f26e24ee490
Leonardo Rossi <leonardo.rossi@gmail.com>
2024-02-14 11:48:09 +0100

    adds runtime fixups and renames test files
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>

---
+++ packages/runtime/test/start/trustProxy-config.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const assert = require('node:assert')
+const { join } = require('node:path')
+const { test } = require('node:test')
+const { loadConfig } = require('@platformatic/config')
+const { buildServer, platformaticRuntime } = require('../..')
+// const antani = require('../..')
+const fixturesDir = join(__dirname, '..', '..', 'fixtures')
+
+test('all services have trustProxy = true in server config (except entrypoint)', async (t) => {
+  const configFile = join(fixturesDir, 'configs', 'monorepo-composer.json')
+  const config = await loadConfig({}, ['-c', configFile], platformaticRuntime)
+  const app = await buildServer(config.configManager.current)
+  await app.start()
+  const services = await app.getServices()
+
+  for (const s of services.services) {
+    const config = await app.getServiceConfig(s.id)
+    console.log(s.entrypoint, config.server)
+    if (s.entrypoint) {
+      assert.equal(config.server.trustProxy, undefined)
+    } else {
+      assert.equal(config.server.trustProxy, true)
+    }
+  }
+  t.after(async () => {
+    await app.close()
+  })
+})
