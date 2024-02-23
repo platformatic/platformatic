2fd0a995b32ba79af1ee357173007eb3a30046e8
Matteo Collina <hello@matteocollina.com>
2024-02-23 15:46:58 +0100

    Added test
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ packages/runtime/test/cli/do-not-crash-on-syntax-error.test.mjs
@@ -0,0 +1,57 @@
+import { cp, writeFile, mkdtemp, mkdir, rm } from 'node:fs/promises'
+import { join } from 'node:path'
+import { test } from 'node:test'
+import desm from 'desm'
+import { start, createCjsLoggingPlugin } from './helper.mjs'
+
+const fixturesDir = join(desm(import.meta.url), '..', '..', 'fixtures')
+
+const base = join(desm(import.meta.url), '..', 'tmp')
+
+try {
+  await mkdir(base, { recursive: true })
+} catch {
+}
+
+function saferm (path) {
+  return rm(path, { recursive: true, force: true }).catch(() => {})
+}
+
+test('watches CommonJS files', async (t) => {
+  const tmpDir = await mkdtemp(join(base, 'watch-'))
+  t.after(() => saferm(tmpDir))
+  console.log(`using ${tmpDir}`)
+  const configFileSrc = join(fixturesDir, 'configs', 'monorepo.json')
+  const configFileDst = join(tmpDir, 'configs', 'monorepo.json')
+  const appSrc = join(fixturesDir, 'monorepo')
+  const appDst = join(tmpDir, 'monorepo')
+  const cjsPluginFilePath = join(appDst, 'serviceAppWithLogger', 'plugin.js')
+
+  await Promise.all([
+    cp(configFileSrc, configFileDst),
+    cp(appSrc, appDst, { recursive: true })
+  ])
+
+  await writeFile(cjsPluginFilePath, createCjsLoggingPlugin('v1', false))
+  const { child } = await start('-c', configFileDst)
+  t.after(() => child.kill('SIGKILL'))
+
+  await writeFile(cjsPluginFilePath, `\
+    module.exports = async (app) => {
+      app.get('/version', () => 'v2')
+  `) // This has a syntax error
+
+  for await (const log of child.ndj.iterator({ destroyOnReturn: false })) {
+    if (log.msg?.includes('Unexpected') >= 0) {
+      break
+    }
+  }
+
+  await writeFile(cjsPluginFilePath, createCjsLoggingPlugin('v2', true))
+
+  for await (const log of child.ndj) {
+    if (log.msg === 'RELOADED v2') {
+      break
+    }
+  }
+})
