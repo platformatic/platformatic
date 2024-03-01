90eaa5f3d2811640d62bd75e5cc316df920a9704
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-03-01 12:13:31 +0100

    feat: add user transport support

--- packages/runtime/test/management-api/logs.test.js
+++ packages/runtime/test/management-api/logs.test.js
@@ -1,23 +1,79 @@
 'use strict'
 
-const { platform } = require('node:os')
+const assert = require('node:assert')
+const { platform, tmpdir } = require('node:os')
 const { join } = require('node:path')
 const { test } = require('node:test')
+const { readFile, writeFile, rm } = require('node:fs/promises')
 const WebSocket = require('ws')
 
 const { buildServer } = require('../..')
 const fixturesDir = join(__dirname, '..', '..', 'fixtures')
 
-test('should get runtime logs via management api', async (t) => {
-  const projectDir = join(fixturesDir, 'management-api')
-  const configFile = join(projectDir, 'platformatic.json')
-  const app = await buildServer(configFile)
+// test('should get runtime logs via management api', async (t) => {
+//   const projectDir = join(fixturesDir, 'management-api')
+//   const configFile = join(projectDir, 'platformatic.json')
+//   const app = await buildServer(configFile)
 
+//   await app.start()
+
+//   t.after(async () => {
+//     await app.close()
+//     await app.managementApi.close()
+//   })
+
+//   const socketPath = app.managementApi.server.address()
+
+//   const protocol = platform() === 'win32' ? 'ws+unix:' : 'ws+unix://'
+//   const webSocket = new WebSocket(protocol + socketPath + ':/api/logs')
+
+//   return new Promise((resolve, reject) => {
+//     const timeout = setTimeout(() => {
+//       reject(new Error('Timeout'))
+//     }, 10000)
+
+//     webSocket.on('error', (err) => {
+//       reject(err)
+//     })
+
+//     webSocket.on('message', (data) => {
+//       if (data.includes('Server listening at')) {
+//         clearTimeout(timeout)
+//         webSocket.close()
+//         resolve()
+//       }
+//     })
+//   })
+// })
+
+test('should support custom use transport with a message port logging', async (t) => {
+  const projectDir = join(fixturesDir, 'management-api')
+  const configPath = join(projectDir, 'platformatic.json')
+  const configFile = await readFile(configPath, 'utf8')
+  const config = JSON.parse(configFile)
+
+  const logsPath = join(tmpdir(), 'platformatic-management-api-logs.txt')
+  await rm(logsPath).catch(() => {})
+
+  config.server.logger = {
+    level: 'trace',
+    transport: {
+      target: 'pino/file',
+      options: { destination: logsPath }
+    }
+  }
+
+  const configWithLoggerPath = join(projectDir, 'platformatic-custom-logger.json')
+  await writeFile(configWithLoggerPath, JSON.stringify(config, null, 2))
+
+  const app = await buildServer(configWithLoggerPath)
   await app.start()
 
   t.after(async () => {
     await app.close()
     await app.managementApi.close()
+    await rm(configWithLoggerPath)
+    await rm(logsPath)
   })
 
   const socketPath = app.managementApi.server.address()
@@ -25,7 +81,7 @@ test('should get runtime logs via management api', async (t) => {
   const protocol = platform() === 'win32' ? 'ws+unix:' : 'ws+unix://'
   const webSocket = new WebSocket(protocol + socketPath + ':/api/logs')
 
-  return new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     const timeout = setTimeout(() => {
       reject(new Error('Timeout'))
     }, 10000)
@@ -42,4 +98,7 @@ test('should get runtime logs via management api', async (t) => {
       }
     })
   })
+
+  const logs = await readFile(logsPath, 'utf8')
+  assert.ok(logs.includes('Server listening at'))
 })
