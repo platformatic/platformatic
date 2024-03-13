d1a9979f328986f78c3ae5d1cc1d97fc7faa0102
Matteo Collina <hello@matteocollina.com>
2024-03-13 14:51:07 +0100

    dropped checking of platformatic version
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/cli/lib/upgrade.js
+++ packages/cli/lib/upgrade.js
@@ -1,14 +1,12 @@
 import { Store, getStringifier } from '@platformatic/config'
 import parseArgs from 'minimist'
-import { writeFile, readFile } from 'fs/promises'
-import { getLatestNpmVersion } from '@platformatic/utils'
+import { writeFile } from 'fs/promises'
 import { platformaticService } from '@platformatic/service'
 import { platformaticDB } from '@platformatic/db'
 import { platformaticComposer } from '@platformatic/composer'
 import { platformaticRuntime } from '@platformatic/runtime'
 import pino from 'pino'
 import pretty from 'pino-pretty'
-import { join } from 'desm'
 
 export async function upgrade (argv) {
   const args = parseArgs(argv, {
@@ -22,7 +20,6 @@ export async function upgrade (argv) {
     ignore: 'hostname,pid'
   }))
   try {
-    await upgradeSystem(logger)
     await upgradeApp(args.config, logger)
   } catch (err) {
     console.log(err)
@@ -59,39 +56,4 @@ async function upgradeApp (config, logger) {
   await writeFile(configManager.fullPath, newConfig, 'utf8')
 
   logger.info(`✅ Updated ${configManager.fullPath}`)
-}
-
-async function upgradeSystem (logger) {
-  logger.info('Checking latest platformatic version on npm registry...')
-  const { version: currentRunningVersion } = JSON.parse(await readFile(join(import.meta.url, '..', 'package.json')))
-  const latestNpmVersion = await getLatestNpmVersion('platformatic')
-  if (latestNpmVersion) {
-    const compareResult = compareVersions(currentRunningVersion, latestNpmVersion)
-    switch (compareResult) {
-      case 0:
-        logger.info(`✅ You are running the latest Platformatic version v${latestNpmVersion}!`)
-        break
-      case -1:
-        logger.info(`✨ Version ${latestNpmVersion} of Platformatic has been released, please update with "npm update platformatic"`)
-        break
-    }
-  }
-}
-
-export function compareVersions (first, second) {
-  const [firstMajor, firstMinor, firstPatch] = first.split('.')
-  const [secondMajor, secondMinor, secondPatch] = second.split('.')
-
-  if (firstMajor < secondMajor) return -1
-  if (firstMajor > secondMajor) return 1
-
-  // firstMajor === secondMajor
-  if (firstMinor < secondMinor) return -1
-  if (firstMinor > secondMinor) return 1
-
-  // firstMinor === secondMinor
-  if (firstPatch < secondPatch) return -1
-  if (firstPatch > secondPatch) return 1
-
-  return 0
 }
