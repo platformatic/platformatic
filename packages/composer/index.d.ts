b19b9caf4089d3f19c93cf176cdedaeab772bd60
Matteo Collina <hello@matteocollina.com>
2024-03-13 10:20:48 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/composer/index.d.ts
+++ packages/composer/index.d.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from 'fastify'
-import { HttpsPlatformaticDevSchemasV1144Composer } from './config'
+import { PlatformaticComposer } from './config'
 
 export { PlatformaticApp } from '@platformatic/service'
-export type PlatformaticComposerConfig = HttpsPlatformaticDevSchemasV1144Composer
+export type PlatformaticComposerConfig = PlatformaticComposer
 
 export function buildServer (opts: object, app?: object, ConfigManagerContructor?: object): Promise<FastifyInstance>
