204ca4dc556a58bd3557752c71eb2426bfc58db4
Matteo Collina <hello@matteocollina.com>
2024-03-15 22:56:50 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/runtime/test/management-api/logs-live.test.js
+++ packages/runtime/test/management-api/logs-live.test.js
@@ -30,7 +30,7 @@ test('should get runtime logs via management api', async (t) => {
   return new Promise((resolve, reject) => {
     const timeout = setTimeout(() => {
       reject(new Error('Timeout'))
-    }, 10000)
+    }, 100000)
 
     webSocket.on('error', (err) => {
       reject(err)
@@ -84,7 +84,7 @@ test('should support custom use transport with a message port logging', async (t
   await new Promise((resolve, reject) => {
     const timeout = setTimeout(() => {
       reject(new Error('Timeout'))
-    }, 10000)
+    }, 100000)
 
     webSocket.on('error', (err) => {
       reject(err)
