19a766b60f44e50dcf78cf9ae2736b7860d6d4f9
Matteo Collina <hello@matteocollina.com>
2024-03-15 19:50:43 +0100

    Do not force enable metrics unless the managementAPI is enabled
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/runtime/test/api/service-config.test.js
+++ packages/runtime/test/api/service-config.test.js
@@ -9,7 +9,7 @@ const { buildServer, platformaticRuntime } = require('../..')
 const fixturesDir = join(__dirname, '..', '..', 'fixtures')
 
 test('should get service config', async (t) => {
-  const configFile = join(fixturesDir, 'configs', 'monorepo.json')
+  const configFile = join(fixturesDir, 'configs', 'monorepo-with-management-api.json')
   const config = await loadConfig({}, ['-c', configFile], platformaticRuntime)
   const app = await buildServer(config.configManager.current)
 
@@ -23,11 +23,20 @@ test('should get service config', async (t) => {
 
   delete serviceConfig.$schema
 
+  let logger = {}
+  if (process.stdout.isTTY) {
+    logger = {
+      transport: {
+        target: 'pino-pretty'
+      }
+    }
+  }
+
   assert.deepStrictEqual(serviceConfig, {
     server: {
       hostname: '127.0.0.1',
       port: 0,
-      logger: {},
+      logger,
       keepAliveTimeout: 5000,
       trustProxy: true
     },
@@ -46,6 +55,53 @@ test('should get service config', async (t) => {
         enabled: false
       },
       prefix: 'with_logger_'
+    }
+  })
+})
+
+test('do not force enable metrics without the management api', async (t) => {
+  const configFile = join(fixturesDir, 'configs', 'monorepo.json')
+  const config = await loadConfig({}, ['-c', configFile], platformaticRuntime)
+  const app = await buildServer(config.configManager.current)
+
+  await app.start()
+
+  t.after(async () => {
+    await app.close()
+  })
+
+  const serviceConfig = await app.getServiceConfig('with-logger')
+
+  delete serviceConfig.$schema
+
+  let logger = {}
+  if (process.stdout.isTTY) {
+    logger = {
+      transport: {
+        target: 'pino-pretty'
+      }
+    }
+  }
+
+  assert.deepStrictEqual(serviceConfig, {
+    server: {
+      hostname: '127.0.0.1',
+      port: 0,
+      logger,
+      keepAliveTimeout: 5000,
+      trustProxy: true
+    },
+    service: { openapi: true },
+    plugins: {
+      paths: [
+        join(fixturesDir, 'monorepo', 'serviceAppWithLogger', 'plugin.js')
+      ]
+    },
+    watch: {
+      enabled: false
+    },
+    metrics: {
+      server: 'parent'
     }
   })
 })
