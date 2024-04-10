dabe392f8dc79060014fc6262fd8c0586d8aa66f
Matteo Collina <hello@matteocollina.com>
2024-04-10 09:17:33 +0200

    Revert "fixup"
    This reverts commit 0937075d22c7dfc7cc5eddec857102905e5fce6e.

--- packages/runtime/test/management-api/clean-logs.test.js
+++ packages/runtime/test/management-api/clean-logs.test.js
@@ -40,8 +40,8 @@ test('should clean the logs after reaching a limit', async (t) => {
     (file) => file.startsWith('logs')
   )
   assert.deepStrictEqual(runtimeLogFiles, [
-    'logs.10',
     'logs.11',
-    'logs.12'
+    'logs.12',
+    'logs.13'
   ])
 })
