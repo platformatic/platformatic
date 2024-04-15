325dd14ac3b53638673bf39f45ef0021020ec45d
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-04-15 03:52:09 -0700

    fix: runtime live logs api (#2396)

--- packages/runtime/test/management-api/logs-live.test.js
+++ packages/runtime/test/management-api/logs-live.test.js
@@ -5,6 +5,8 @@ const { platform, tmpdir } = require('node:os')
 const { join } = require('node:path')
 const { test } = require('node:test')
 const { readFile, writeFile, rm } = require('node:fs/promises')
+const { setTimeout: sleep } = require('node:timers/promises')
+const { Client } = require('undici')
 const WebSocket = require('ws')
 
 const { buildServer } = require('../..')
@@ -38,6 +40,72 @@ test('should get runtime logs via management api', async (t) => {
 
     webSocket.on('message', (data) => {
       if (data.includes('Server listening at')) {
+        clearTimeout(timeout)
+        webSocket.close()
+        resolve()
+      }
+    })
+  })
+})
+
+test('should get runtime logs via management api (with a start index)', async (t) => {
+  const projectDir = join(fixturesDir, 'management-api')
+  const configFile = join(projectDir, 'platformatic.json')
+  const app = await buildServer(configFile)
+
+  await app.start()
+
+  t.after(async () => {
+    await app.close()
+    await app.managementApi.close()
+  })
+
+  const res = await app.inject('service-1', {
+    method: 'GET',
+    url: '/large-logs'
+  })
+  assert.strictEqual(res.statusCode, 200)
+
+  // Wait for logs to be written
+  await sleep(3000)
+
+  const socketPath = app.managementApi.server.address()
+
+  const client = new Client({
+    hostname: 'localhost',
+    protocol: 'http:'
+  }, {
+    socketPath,
+    keepAliveTimeout: 10,
+    keepAliveMaxTimeout: 10
+  })
+
+  const { statusCode, body } = await client.request({
+    method: 'GET',
+    path: '/api/v1/logs/indexes'
+  })
+  assert.strictEqual(statusCode, 200)
+
+  const { indexes } = await body.json()
+  const startLogId = indexes[1]
+
+  const protocol = platform() === 'win32' ? 'ws+unix:' : 'ws+unix://'
+  const webSocket = new WebSocket(
+    protocol + socketPath + ':/api/v1/logs/live' + `?start=${startLogId}`
+  )
+
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      reject(new Error('Timeout'))
+    }, 100000)
+
+    webSocket.on('error', (err) => {
+      reject(err)
+    })
+
+    let counter = 0
+    webSocket.on('message', () => {
+      if (counter++ > 10) {
         clearTimeout(timeout)
         webSocket.close()
         resolve()
