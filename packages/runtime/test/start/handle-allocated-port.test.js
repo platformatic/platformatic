04435e5c954c3a4303d503ce06334aef58de9662
louisvenhoff <Louis.venhoff@gmx.net>
2024-06-12 18:31:34 +0200

    Worked on test setup
    Signed-off-by: louisvenhoff <Louis.venhoff@gmx.net>

--- packages/runtime/test/start/handle-allocated-port.test.js
+++ packages/runtime/test/start/handle-allocated-port.test.js
@@ -1,16 +1,33 @@
 'use strict'
 const assert = require('node:assert')
-const { platformaticRuntime } = require('../..')
-const { join } = require('node:path')
+// const { platformaticRuntime } = require('../..')
+// const { join } = require('node:path')
 const { test } = require('node:test')
-const { loadConfig } = require('@platformatic/config')
-const fixturesDir = join(__dirname, '..', '..', 'fixtures')
-const { buildRuntime } = require('../../lib/start')
+// const { loadConfig } = require('@platformatic/config')
+// const fixturesDir = join(__dirname, '..', '..', 'fixtures')
+// const { buildRuntime } = require('../../lib/start')
+const http = require('http')
+const { startCommand } = require('@platformatic/runtime')
+
+let dummyPort = 0
+
+test.beforeEach(() => {
+  const dummyServer = http.createServer(function (req, res) {
+    console.log('Server starting!')
+    res.write('test')
+    res.end()
+  })
+
+  dummyServer.listen(0, () => {
+    dummyPort = dummyServer.address().port
+    process.env.PORT = dummyPort
+  })
+})
 
 test('startes with port +1 when current port is allocated', async () => {
-  const configFile = join(fixturesDir, 'configs', 'monorepo-port-3000.json')
-  const config = await loadConfig({}, ['-c', configFile], platformaticRuntime)
-  const app = await buildRuntime(config.configManager)
-  const res = await app.start()
-  assert.strictEqual('Hello', res)
+  // const configFile = join(fixturesDir, 'configs', 'service-with-env-port.json')
+  // const config = await loadConfig({}, ['-c', configFile], platformaticRuntime)
+  await startCommand([])
+  console.log(dummyPort)
+  assert.strictEqual(process.env.PORT, 1)
 })
