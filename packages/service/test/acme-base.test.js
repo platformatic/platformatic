98fadfe75c376b57f9bc95879c1319d6771b6b27
Paolo Insogna <paolo@cowtech.it>
2024-06-26 16:37:52 +0200

    chore: Make tests more resilient when run alone.
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>
    
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>
    
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/service/test/acme-base.test.js
+++ packages/service/test/acme-base.test.js
@@ -3,6 +3,7 @@
 const assert = require('node:assert')
 const { test } = require('node:test')
 const { join } = require('node:path')
+const { existsSync } = require('node:fs')
 
 test('stackable example', async t => {
   const { execa } = await import('execa')
@@ -23,7 +24,14 @@ test('stackable in typescript', async t => {
 
   const cwd = join(__dirname, '..', 'fixtures', 'acme-base-ts')
 
-  await execa(join(__dirname, '..', 'node_modules', '.bin', 'tsc'), { cwd, stdio: 'inherit' })
+  let tscPath = join(__dirname, '..', 'node_modules', '.bin', 'tsc')
+
+  // If the local npm installation should use global tsc in the root
+  if (!existsSync(tscPath)) {
+    tscPath = join(__dirname, '../../..', 'node_modules', '.bin', 'tsc')
+  }
+
+  await execa(tscPath, { cwd, stdio: 'inherit' })
 
   // if this fails, it will throw an error
   await execa('node', ['--test'], {
