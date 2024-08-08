027313aad8de04c0a64d391c35ac2f8355d8cd69
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-08-08 13:05:32 +0200

    fix: catch runtime service stop error

--- packages/runtime/fixtures/monorepo/serviceAppWithLogger/plugin.js
+++ packages/runtime/fixtures/monorepo/serviceAppWithLogger/plugin.js
@@ -5,4 +5,16 @@ module.exports = async function (app) {
   app.get('/', async () => {
     return { hello: 'world' }
   })
+
+  let crashOnClose = false
+  app.get('/crash-on-close', async () => {
+    crashOnClose = true
+  })
+
+  app.addHook('onClose', async () => {
+    if (crashOnClose) {
+      app.log.info('Crashing process on purpose')
+      throw new Error('Crashing process on purpose')
+    }
+  })
 }
