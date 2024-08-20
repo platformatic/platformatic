499918c99c7c351cd8560c2b9cf17269880b13f0
Paolo Insogna <paolo@cowtech.it>
2024-08-20 16:54:41 +0200

    fix: Fixed tests.
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/runtime/test/start/logs-errors-during-migrations.test.js
+++ packages/runtime/test/start/logs-errors-during-migrations.test.js
@@ -11,10 +11,11 @@ const { wrapConfigInRuntimeConfig } = require('../..')
 const { buildRuntime } = require('../../lib/start')
 const fixturesDir = join(__dirname, '..', '..', 'fixtures')
 
-test('logs errors during db migrations', async (t) => {
+test('logs errors during db migrations', async t => {
   const configFile = join(fixturesDir, 'dbAppWithMigrationError', 'platformatic.db.json')
   const config = await loadConfig({}, ['-c', configFile], platformaticDB)
   const runtimeConfig = await wrapConfigInRuntimeConfig(config)
+  runtimeConfig.current.restartOnError = 1000
 
   const runtime = await buildRuntime(runtimeConfig)
   t.after(async () => {
@@ -25,19 +26,16 @@ test('logs errors during db migrations', async (t) => {
     await runtime.start()
   }, /The service mysimplename exited prematurely with error code 1/)
 
-  const client = new Client(
-    { hostname: 'localhost', protocol: 'http:' },
-    { socketPath: runtime.getManagementApiUrl() }
-  )
+  const client = new Client({ hostname: 'localhost', protocol: 'http:' }, { socketPath: runtime.getManagementApiUrl() })
 
-  await sleep(5000)
+  await sleep(3000)
 
   const { statusCode, body } = await client.request({ method: 'GET', path: '/api/v1/logs/all' })
   assert.strictEqual(statusCode, 200)
   const messages = (await body.text()).trim().split('\n').map(JSON.parse)
 
   assert.ok(messages.some(m => m.msg.match(/running 001.do.sql/)))
-  assert.ok(messages.some(m => m.payload?.msg?.match(/near "fiddlesticks": syntax error/)))
+  assert.ok(messages.some(m => m.msg?.match(/near "fiddlesticks": syntax error/)))
 
   await startPromise
 })
