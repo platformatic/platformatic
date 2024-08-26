42cbea07fc01dcd12c89f064697ab7076087a2f9
Paolo Insogna <paolo@cowtech.it>
2024-08-26 11:09:15 +0200

    chore: Regenerate schema after bumping version. (#2999)

--- packages/runtime/config.d.ts
+++ packages/runtime/config.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export type HttpsSchemasPlatformaticDevPlatformaticRuntime200Alpha5Json = {
+export type HttpsSchemasPlatformaticDevPlatformaticRuntime200Alpha6Json = {
   [k: string]: unknown;
 } & {
   $schema?: string;
