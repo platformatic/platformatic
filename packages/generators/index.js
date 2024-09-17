a031ca1545b9f9f2ae98a69e99a40fc1435f1e6e
Matteo Collina <hello@matteocollina.com>
2024-09-17 09:47:09 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/generators/index.js
+++ packages/generators/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const { BaseGenerator } = require('./lib/base-generator')
-const { StackableGenerator } = require('./lib/stackable-generator')
 const { generateTests } = require('./lib/create-plugin')
 const utils = require('./lib/utils')
 module.exports = {
