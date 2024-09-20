b15b96aa0468daac18bacd1001b25b3456a55ec9
mcollina <hello@matteocollina.com>
2024-09-20 17:09:56 +0000

    Bumped v2.0.0-alpha.18

--- packages/wattpm/config.d.ts
+++ packages/wattpm/config.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export type HttpsSchemasPlatformaticDevWattpm200Alpha17Json = {
+export type HttpsSchemasPlatformaticDevWattpm200Alpha18Json = {
   [k: string]: unknown;
 } & {
   $schema?: string;
