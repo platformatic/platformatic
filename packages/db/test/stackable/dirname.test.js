c61c0384d2d21a7b5a374d873318e16af8ed5b55
Matteo Collina <hello@matteocollina.com>
2024-09-20 17:06:16 +0200

    Reuse configManager when creating a stackable
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ packages/db/test/stackable/dirname.test.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const assert = require('node:assert')
+const { test } = require('node:test')
+const { join } = require('node:path')
+const { buildStackable } = require('../..')
+
+const pltVersion = require('../../package.json').version
+
+test('get service info via stackable api', async (t) => {
+  const projectRoot = join(__dirname, '..', '..', 'fixtures', 'sqlite')
+  const config = join(projectRoot, 'platformatic.db.json')
+
+  process.env.DATABASE_URL = 'sqlite://:memory:'
+  const stackable = await buildStackable({ config })
+  t.after(async () => {
+    await stackable.stop()
+  })
+  await stackable.start()
+
+  assert.strictEqual(stackable.app.platformatic.configManager.dirname, projectRoot)
+})
