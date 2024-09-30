cf5adb0bb7f2d8458825ccd17ae830a42f3b8fff
Paolo Insogna <paolo@cowtech.it>
2024-09-30 19:58:38 +0200

    fix: Use build information in Vite SSR.
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/vite/index.js
+++ packages/vite/index.js
@@ -285,9 +285,19 @@ export class ViteSSRStackable extends NodeStackable {
 
     if (this.isProduction) {
       const clientDirectory = config.vite.ssr.clientDirectory
-      this.verifyOutputDirectory(
-        resolve(this.root, clientDirectory, config.application.outputDirectory, clientDirectory)
-      )
+      const clientOutDir = resolve(this.root, clientDirectory, config.application.outputDirectory, clientDirectory)
+
+      this.verifyOutputDirectory(clientOutDir)
+
+      const buildInfoPath = resolve(clientOutDir, '.platformatic-build.json')
+      if (!this.#basePath && existsSync(buildInfoPath)) {
+        try {
+          const buildInfo = JSON.parse(await readFile(buildInfoPath, 'utf-8'))
+          this.#basePath = buildInfo.basePath
+        } catch (e) {
+          console.log(e)
+        }
+      }
     }
 
     await super.start({ listen })
@@ -359,6 +369,7 @@ export class ViteSSRStackable extends NodeStackable {
     const config = vite?.devServer?.config ?? vite?.config.vite
     const applicationBasePath = config?.base
 
+    process._rawDebug(this.basePath, applicationBasePath, this.#basePath)
     composer = {
       tcp: typeof this.url !== 'undefined',
       url: this.url,
