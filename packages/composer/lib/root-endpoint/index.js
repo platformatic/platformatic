8361e7944d837eed258ef8cdfc095aaf7986c8d2
Leonardo Rossi <leonardo.rossi@gmail.com>
2024-10-29 18:55:50 +0100

    New composer home page (#3385)
    * first draft of composer home page
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>
    
    * removes dep
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>
    
    * fixup and updates test
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>
    
    * fixup tests
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>
    
    * Update packages/composer/test/openapi/root-endpoint.test.js
    
    * fixup tests
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>
    
    ---------
    
    Signed-off-by: Leonardo Rossi <leonardo.rossi@gmail.com>
    Co-authored-by: Matteo Collina <hello@matteocollina.com>

--- packages/composer/lib/root-endpoint/index.js
+++ packages/composer/lib/root-endpoint/index.js
@@ -8,20 +8,67 @@ module.exports = async (app, opts) => {
   app.register(fastifyStatic, {
     root: join(__dirname, 'public'),
   })
-
+  app.register(require('@fastify/view'), {
+    engine: {
+      nunjucks: require('nunjucks')
+    },
+    root: join(__dirname, 'public')
+  })
   // root endpoint
   app.route({
     method: 'GET',
     path: '/',
     schema: { hide: true },
-    handler: (req, reply) => {
+    handler: async (req, reply) => {
       const uaString = req.headers['user-agent']
+      let hasOpenAPIServices = false
+      let hasGraphQLServices = false
       if (uaString) {
         const parsed = userAgentParser(uaString)
         if (parsed.browser.name !== undefined) {
-          return reply.sendFile('./index.html')
+          const serviceTypes = {
+            proxy: {
+              title: 'Reverse Proxy',
+              icon: './images/reverse-proxy.svg',
+              services: []
+            },
+            openapi: {
+              title: 'OpenAPI',
+              icon: './images/openapi.svg',
+              services: []
+            },
+            graphql: {
+              title: 'GraphQL',
+              icon: './images/graphql.svg',
+              services: []
+            }
+          }
+
+          app.platformatic.config.composer.services.forEach((s) => {
+            if (s.openapi) {
+              hasOpenAPIServices = true
+              serviceTypes.openapi.services.push(s)
+            }
+            if (s.graphql) {
+              hasGraphQLServices = true
+              serviceTypes.graphql.services.push(s)
+            }
+            if (s.proxy) {
+              serviceTypes.proxy.services.push({
+                ...s,
+                externalLink: `${s.proxy.prefix}`
+              })
+            }
+          })
+
+          return reply.view('index.njk', {
+            hasGraphQLServices,
+            hasOpenAPIServices,
+            services: serviceTypes
+          })
         }
       }
+      // Load services
       return { message: 'Welcome to Platformatic! Please visit https://docs.platformatic.dev' }
     },
   })
