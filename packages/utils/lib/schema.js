494296242fdf2684320f3a171fb9d648ad5489d3
Ivan Tymoshenko <ivan@tymoshenko.me>
2024-11-07 14:27:18 +0100

    fix: pino options schema

--- packages/utils/lib/schema.js
+++ packages/utils/lib/schema.js
@@ -129,8 +129,10 @@ const logger = {
                 type: 'object',
                 properties: {
                   target: {
-                    type: 'string',
-                    resolveModule: true
+                    anyOf: [
+                      { type: 'string', resolveModule: true },
+                      { type: 'string', resolvePath: true }
+                    ]
                   },
                   options: {
                     type: 'object'
