005bbdc38b4c051392fb4012e3cec8b0d44761b2
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-11-11 16:51:47 +0100

    feat/client/pass-custom-undici-dispatcher:
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/client/index.d.ts
+++ packages/client/index.d.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsync, FastifyReply, FastifyRequest } from 'fastify'
 import { FastifyError } from '@fastify/error'
+import { Dispatcher } from 'undici';
 
 type CodeClasses = 1 | 2 | 3 | 4 | 5;
 type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
@@ -33,7 +34,8 @@ interface PlatformaticClientOptions {
   bodyTimeout?: number;
   headersTimeout?: number;
   validateResponse?: boolean;
-  queryParser?: (query: URLSearchParams) => string
+  queryParser?: (query: URLSearchParams) => string;
+  dispatcher?: Dispatcher;
 }
 
 type BuildOpenAPIClientOptions  = PlatformaticClientOptions & {
