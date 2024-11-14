bc6b662818492aab93f3ef3f3f9e2fc78ddfed0f
marcopiraccini <marco.piraccini@gmail.com>
2024-11-14 21:35:22 +0100

    Set telemetry config (with serviceName) on nodejs services
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

---
+++ packages/node/test/telemetry.test.js
@@ -0,0 +1,41 @@
+import assert from 'node:assert'
+import { resolve } from 'node:path'
+import { test } from 'node:test'
+import { createRuntime, setFixturesDir } from '../../basic/test/helper.js'
+import { Client } from 'undici'
+
+process.setMaxListeners(100)
+
+setFixturesDir(resolve(import.meta.dirname, './fixtures'))
+
+test('should set telemtry in config on all the node services', async t => {
+  const { runtime } = await createRuntime(t, 'express-api-with-telemetry')
+
+  const client = new Client(
+    {
+      hostname: 'localhost',
+      protocol: 'http:',
+    },
+    {
+      socketPath: runtime.getManagementApiUrl(),
+      keepAliveTimeout: 10,
+      keepAliveMaxTimeout: 10,
+    }
+  )
+
+  const { statusCode, body } = await client.request({
+    method: 'GET',
+    path: '/api/v1/services/api/config',
+  })
+
+  assert.strictEqual(statusCode, 200)
+
+  const serviceConfig = await body.json()
+  assert.deepEqual(serviceConfig.telemetry, {
+    serviceName: 'test-service-api',
+    version: '1.0.0',
+    exporter: {
+      type: 'memory'
+    }
+  })
+})
