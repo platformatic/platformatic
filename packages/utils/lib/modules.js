a1f15580adff320d189f73f05612402d9d269b2b
Paolo Insogna <paolo@cowtech.it>
2024-12-10 16:51:31 +0100

    fixup
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/utils/lib/modules.js
+++ packages/utils/lib/modules.js
@@ -1,23 +1,56 @@
 'use strict'
 
 const { createRequire: nodeCreateRequire } = require('node:module')
-const { basename, dirname, resolve } = require('node:path')
+const { basename, dirname, resolve: resolvePaths } = require('node:path')
 const { fileURLToPath } = require('node:url')
 
-function createRequire (path) {
-  if (path instanceof URL || path.startsWith('file://')) {
-    path = fileURLToPath(path)
+// This is needed until https://github.com/nodejs/node/issues/55417 is sorted out
+function createRequire (originalPath) {
+  // Create a new path which includes node_modules to suppress the warning
+  let noWarningPath = originalPath
+
+  if (noWarningPath instanceof URL || noWarningPath.startsWith('file://')) {
+    noWarningPath = fileURLToPath(noWarningPath)
   }
 
-  // This is needed until https://github.com/nodejs/node/issues/55417 is sorted out
-  if (!path.includes('node_modules')) {
-    const folder = dirname(path)
-    const name = basename(path)
+  if (!noWarningPath.includes('node_modules')) {
+    const folder = dirname(noWarningPath)
+    const name = basename(noWarningPath)
 
-    path = resolve(folder, 'node_modules/platformatic', name)
+    noWarningPath = resolvePaths(folder, 'node_modules/platformatic', name)
   }
 
-  return nodeCreateRequire(path)
+  const originalRequire = nodeCreateRequire(originalPath)
+  const noWarningRequire = nodeCreateRequire(noWarningPath)
+
+  const { cache, extensions, main, resolve: noWarningResolve } = noWarningRequire
+
+  // Call the patched require function, it if fails faill back to the original one
+  const require = function require (...args) {
+    try {
+      return noWarningRequire(...args)
+    } catch (e) {
+      return originalRequire(...args)
+    }
+  }
+
+  // Call the patched require.resolve function, it if fails faill back to the original one
+  const resolve = function require (...args) {
+    try {
+      return noWarningResolve(...args)
+    } catch (e) {
+      return originalRequire.resolve(...args)
+    }
+  }
+  resolve.paths = noWarningResolve.paths
+
+  // Assign other properties
+  require.cache = cache
+  require.extensions = extensions
+  require.main = main
+  require.resolve = resolve
+
+  return require
 }
 
 async function loadModule (require, path) {
