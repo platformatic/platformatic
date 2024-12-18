e97dc800c8cbdb39bb062fd42fdce87d663efd1c
Matteo Collina <hello@matteocollina.com>
2024-12-18 17:32:12 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/node/index.js
+++ packages/node/index.js
@@ -168,7 +168,7 @@ export class NodeStackable extends BaseStackable {
     const command = config.commands.build
 
     if (command) {
-      return this.buildWithCommand(command, null, { disableChildManager: config.disablePlatformaticInBuild })
+      return this.buildWithCommand(command, null, { disableChildManager: this.configManager.current.node?.disablePlatformaticInBuild })
     }
 
     // If no command was specified, we try to see if there is a build script defined in package.json.
