ddf375dbe46e078f57600f4edc20934099c6b73f
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-12-21 14:46:04 +0100

    fix(test): update
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/control/test/metrics.test.mjs
+++ packages/control/test/metrics.test.mjs
@@ -10,23 +10,22 @@ import { ok, strictEqual } from 'node:assert'
 const cliPath = desm.join(import.meta.url, '..', 'control.js')
 const fixturesDir = desm.join(import.meta.url, 'fixtures')
 
-test('should return runtime metrics when passing no options', async (t) => {
+test('should return runtime metrics when passing no format option', async (t) => {
   const projectDir = join(fixturesDir, 'runtime-1')
   const configFile = join(projectDir, 'platformatic.json')
   const { runtime } = await startRuntime(configFile)
   t.after(() => kill(runtime))
 
-  const child = await execa('node', [cliPath, 'metrics'])
+  const child = await execa('node', [cliPath, 'metrics', '-p', runtime.pid])
   strictEqual(child.exitCode, 0)
 
-  ok(child.stdout.includes('shared_trusted_large_object'))
-  ok(child.stdout.includes('nodejs_version_info'))
-  ok(child.stdout.includes('http_request_duration_seconds'))
-  ok(child.stdout.includes('http_cache_hit_count'))
-  ok(child.stdout.includes('process_cpu_system_seconds_total'))
+  ok(child.stdout.includes('"nodejs_version_info"'))
+  ok(child.stdout.includes('"http_request_duration_seconds"'))
+  ok(child.stdout.includes('"http_cache_hit_count"'))
+  ok(child.stdout.includes('"process_cpu_system_seconds_total"'))
 })
 
-test('should return runtime metrics when passing options', async (t) => {
+test('should return runtime metrics when passing text option', async (t) => {
   const projectDir = join(fixturesDir, 'runtime-1')
   const configFile = join(projectDir, 'platformatic.json')
   const { runtime } = await startRuntime(configFile)
@@ -35,9 +34,8 @@ test('should return runtime metrics when passing options', async (t) => {
   const child = await execa('node', [cliPath, 'metrics', '-p', runtime.pid, '-f', 'text'])
   strictEqual(child.exitCode, 0)
 
-  ok(child.stdout.includes('shared_trusted_large_object'))
-  ok(child.stdout.includes('nodejs_version_info'))
-  ok(child.stdout.includes('http_request_duration_seconds'))
-  ok(child.stdout.includes('http_cache_hit_count'))
-  ok(child.stdout.includes('process_cpu_system_seconds_total'))
+  ok(child.stdout.includes('# TYPE nodejs_version_info gauge'))
+  ok(child.stdout.includes('# TYPE http_request_all_duration_seconds histogram'))
+  ok(child.stdout.includes('# TYPE http_cache_hit_count counter'))
+  ok(child.stdout.includes('# TYPE thread_cpu_seconds_total counter'))
 })
