f14e0674517ff7f8897f9990838b12619258b13d
Roberto Bianchi <roberto.bianchi@spendesk.com>
2024-12-21 14:30:02 +0100

    fix(test): update
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/control/lib/metrics.js
+++ packages/control/lib/metrics.js
@@ -1,12 +1,24 @@
 'use strict'
 
+const { parseArgs } = require('node:util')
+
 const RuntimeApiClient = require('./runtime-api-client')
 
-async function runtimeMetricsCommand () {
+async function runtimeMetricsCommand (argv) {
+  const args = parseArgs({
+    args: argv,
+    options: {
+      pid: { type: 'string', short: 'p' },
+      format: { type: 'string', short: 'f', default: 'json' }
+    },
+    strict: false,
+  }).values
+
   const client = new RuntimeApiClient()
-  const runtime = await client.getMatchingRuntime([])
-  const metrics = await client.getRuntimeMetrics(runtime.pid, { format: 'json' })
-  console.log(JSON.stringify(metrics, null, 2))
+  const runtimePid = args.pid || (await client.getMatchingRuntime([])).pid
+  const metrics = await client.getRuntimeMetrics(runtimePid, { format: args.format })
+  const result = args.format === 'text' ? metrics : JSON.stringify(metrics, null, 2)
+  console.log(result)
   await client.close()
 }
 
--- packages/control/test/metrics.test.mjs
+++ packages/control/test/metrics.test.mjs
@@ -5,31 +5,39 @@ import { join } from 'node:path'
 import { execa } from 'execa'
 import * as desm from 'desm'
 import { startRuntime, kill } from './helper.mjs'
+import { ok, strictEqual } from 'node:assert'
 
 const cliPath = desm.join(import.meta.url, '..', 'control.js')
 const fixturesDir = desm.join(import.meta.url, 'fixtures')
 
-test('should return runtime metrics', async (t) => {
+test('should return runtime metrics when passing no options', async (t) => {
   const projectDir = join(fixturesDir, 'runtime-1')
   const configFile = join(projectDir, 'platformatic.json')
   const { runtime } = await startRuntime(configFile)
   t.after(() => kill(runtime))
 
-  const child = execa('node', [cliPath, 'metrics'])
-  t.after(() => kill(child))
+  const child = await execa('node', [cliPath, 'metrics'])
+  strictEqual(child.exitCode, 0)
 
-  const errorTimeout = setTimeout(() => {
-    throw new Error('Couldn\'t start server')
-  }, 30000)
+  ok(child.stdout.includes('shared_trusted_large_object'))
+  ok(child.stdout.includes('nodejs_version_info'))
+  ok(child.stdout.includes('http_request_duration_seconds'))
+  ok(child.stdout.includes('http_cache_hit_count'))
+  ok(child.stdout.includes('process_cpu_system_seconds_total'))
+})
 
-  return new Promise((resolve) => {
-    let output = ''
-    child.stdout.on('data', (data) => {
-      output += data.toString()
-      if (output.includes('shared_trusted_large_object') && output.includes('nodejs_version_info') && output.includes('http_request_duration_seconds') && output.includes('http_cache_hit_count') && output.includes('process_cpu_system_seconds_total')) {
-        clearTimeout(errorTimeout)
-        resolve()
-      }
-    })
-  })
+test('should return runtime metrics when passing options', async (t) => {
+  const projectDir = join(fixturesDir, 'runtime-1')
+  const configFile = join(projectDir, 'platformatic.json')
+  const { runtime } = await startRuntime(configFile)
+  t.after(() => kill(runtime))
+
+  const child = await execa('node', [cliPath, 'metrics', '-p', runtime.pid, '-f', 'text'])
+  strictEqual(child.exitCode, 0)
+
+  ok(child.stdout.includes('shared_trusted_large_object'))
+  ok(child.stdout.includes('nodejs_version_info'))
+  ok(child.stdout.includes('http_request_duration_seconds'))
+  ok(child.stdout.includes('http_cache_hit_count'))
+  ok(child.stdout.includes('process_cpu_system_seconds_total'))
 })
