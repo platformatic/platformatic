03753071ff5f513c4f3b4917661e8ce16549a135
Paolo Insogna <paolo@cowtech.it>
2025-01-14 10:41:17 +0100

    chore: Added documentation.
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/globals/test/index.test.js
+++ packages/globals/test/index.test.js
@@ -2,7 +2,7 @@ import { deepStrictEqual } from 'node:assert'
 import { test } from 'node:test'
 import { getGlobal } from '../lib/index.js'
 
-test('should return the global object', t => {
+test('getGlobal should return the global object', t => {
   globalThis.platformatic = 'platformatic'
   deepStrictEqual(getGlobal(), 'platformatic')
 
