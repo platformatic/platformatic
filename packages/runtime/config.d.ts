c7fd24f6411bb87ce3e95c71ecd16d5a0df92dd4
mcollina <hello@matteocollina.com>
2025-01-24 09:29:34 +0000

    Bumped v2.40.0

--- packages/runtime/config.d.ts
+++ packages/runtime/config.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export type HttpsSchemasPlatformaticDevPlatformaticRuntime2391Json = {
+export type HttpsSchemasPlatformaticDevPlatformaticRuntime2400Json = {
   [k: string]: unknown;
 } & {
   $schema?: string;
--- packages/wattpm/config.d.ts
+++ packages/wattpm/config.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export type HttpsSchemasPlatformaticDevWattpm2391Json = {
+export type HttpsSchemasPlatformaticDevWattpm2400Json = {
   [k: string]: unknown;
 } & {
   $schema?: string;
