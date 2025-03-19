5746ac331e16af880c7b20b8fb781358c25527d9
Roberto Bianchi <roberto.bianchi@spendesk.com>
2025-03-19 16:29:14 +0100

    fix(client cli): use proper type for 204
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/client-cli/lib/responses-writer.mjs
+++ packages/client-cli/lib/responses-writer.mjs
@@ -35,9 +35,9 @@ function responsesWriter (operationId, responsesObject, isFullResponse, writer, 
       const isStatusCodeRange = lowerStatusCode === '1xx' || lowerStatusCode === '2xx' || lowerStatusCode === '3xx' || lowerStatusCode === '4xx' || lowerStatusCode === '5xx'
       if (statusCode === '204') {
         if (isFullResponse) {
-          typeName = undefined
+          typeName = 'unknown'
         } else {
-          return 'undefined'
+          return 'unknown'
         }
       }
       if (isResponseArray) typeName = `Array<${typeName}>`
