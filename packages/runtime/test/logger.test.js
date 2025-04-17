af4907d4b650fbd4bc067abbf6c54b9dfd38d7a4
Simone Sanfratello <simone.sanfra@gmail.com>
2025-04-17 14:08:50 +0200

    feat: logger options inherited from runtime

--- packages/runtime/test/logger.test.js
+++ packages/runtime/test/logger.test.js
@@ -9,7 +9,7 @@ const { request } = require('undici')
 const { tmpdir } = require('node:os')
 const { buildServer } = require('..')
 
-test('logger options', async t => {
+test('should use full logger options - formatters, timestamp, redaction', async t => {
   process.env.LOG_DIR = path.join(tmpdir(), 'test-logs', Date.now().toString())
   process.env.PLT_RUNTIME_LOGGER_STDOUT = 1
   const file = path.join(process.env.LOG_DIR, 'service.log')
@@ -28,16 +28,83 @@ test('logger options', async t => {
   const content = readFileSync(file, 'utf8')
   const logs = content.split('\n').filter(line => line.trim() !== '').map(line => JSON.parse(line))
 
-  assert.ok(logs.find(log => log.level === 'DEBUG' &&
+  assert.ok(logs.find(log => log.level === 'INFO' &&
     log.time.length === 24 && // isotime
     log.name === 'service' &&
     log.msg === 'Starting the service "app"...'))
-  assert.ok(logs.find(log => log.level === 'DEBUG' &&
+  assert.ok(logs.find(log => log.level === 'INFO' &&
     log.time.length === 24 && // isotime
     log.name === 'service' &&
     log.msg === 'Started the service "app"...'))
-  assert.ok(logs.find(log => log.level === 'DEBUG' &&
+  assert.ok(logs.find(log => log.level === 'INFO' &&
     log.time.length === 24 && // isotime
     log.name === 'service' &&
     log.msg.startsWith('Platformatic is now listening at http://127.0.0.1:')))
+})
+
+test('should inherit full logger options from runtime to services', async t => {
+  process.env.LOG_DIR = path.join(tmpdir(), 'test-logs', Date.now().toString())
+  process.env.PLT_RUNTIME_LOGGER_STDOUT = 1
+  const file = path.join(process.env.LOG_DIR, 'service.log')
+  const serviceRoot = path.join(__dirname, '..', 'fixtures', 'logger-options')
+
+  const app = await buildServer(path.join(serviceRoot, 'platformatic.json'))
+  t.after(async () => {
+    await app.close()
+  })
+  const url = await app.start()
+
+  await request(url, { path: '/logs' })
+  // wait for logger flush
+  await wait(500)
+
+  const content = readFileSync(file, 'utf8')
+  const logs = content.split('\n').filter(line => line.trim() !== '').map(line => JSON.parse(line))
+
+  // assert.ok(logs.find(log => log.level === 'INFO' &&
+  //   log.time.length === 24 && // isotime
+  //   log.name === 'service' &&
+  //   log.msg === 'Starting the service "app"...'))
+  // assert.ok(logs.find(log => log.level === 'INFO' &&
+  //   log.time.length === 24 && // isotime
+  //   log.name === 'service' &&
+  //   log.msg === 'Started the service "app"...'))
+  // assert.ok(logs.find(log => log.level === 'INFO' &&
+  //   log.time.length === 24 && // isotime
+  //   log.name === 'service' &&
+  //   log.msg.startsWith('Platformatic is now listening at http://127.0.0.1:')))
+
+  assert.ok(logs.find(log => log.level === 'DEBUG' &&
+    log.time.length === 24 && // isotime
+    log.name === 'service' &&
+    log.msg === 'Loading envfile...'))
+
+  assert.ok(logs.find(log => log.level === 'INFO' &&
+    log.time.length === 24 && // isotime
+    log.name === 'service' &&
+    log.msg === 'Starting the service "app"...'))
+
+  assert.ok(logs.find(log => log.level === 'INFO' &&
+    log.time.length === 24 && // isotime
+    log.name === 'service' &&
+    log.msg === 'Started the service "app"...'))
+
+  assert.ok(logs.find(log => log.level === 'INFO' &&
+    log.time.length === 24 && // isotime
+    log.name === 'service' &&
+    log.msg.startsWith('Platformatic is now listening at http://127.0.0.1:')))
+
+  assert.ok(logs.find(log => {
+    if (log.level === 'INFO' &&
+    log.time.length === 24 && // isotime
+    log.name === 'app') {
+      const msg = JSON.parse(log.msg)
+      return msg.level === 'DEBUG' &&
+        msg.time.length === 24 && // isotime
+        msg.name === 'service' &&
+        msg.secret === 'foo' &&
+        msg.msg === 'call route /logs'
+    }
+    return false
+  }))
 })
