50dd53931b74886012acff4a7d2a51466304c0e3
Matteo Collina <hello@matteocollina.com>
2025-04-17 08:08:33 +0200

    This makes thread-cpu-usage an optional dependency
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/metrics/index.js
+++ packages/metrics/index.js
@@ -77,8 +77,18 @@ async function collectMetrics (serviceId, workerId, metricsConfig = {}, registry
 }
 
 async function collectThreadCpuMetrics (registry) {
-  // We need until we switch to 22 as thread-cpu-usage is ESM
-  const { threadCpuUsage } = await import('thread-cpu-usage')
+  let threadCpuUsage
+
+  try {
+    // We need until we switch to 22 as thread-cpu-usage is ESM
+    const res = await import('thread-cpu-usage')
+    threadCpuUsage = res.threadCpuUsage
+  } catch {
+    // We ignore the loading error, as this might
+    // happen if the library has failed to compile
+    // on this platform.
+    return
+  }
 
   let lastSample = process.hrtime.bigint()
   let lastUsage = threadCpuUsage()
