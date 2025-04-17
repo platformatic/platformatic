5607faf45ededab9702f624bee9828e8fe36925b
Simone Sanfratello <simone.sanfra@gmail.com>
2025-04-17 17:11:02 +0200

    feat: logger options inherited from runtime

--- packages/runtime/test/logger.test.js
+++ packages/runtime/test/logger.test.js
@@ -42,7 +42,7 @@ test('should use full logger options - formatters, timestamp, redaction', async 
     log.msg.startsWith('Platformatic is now listening at http://127.0.0.1:')))
 })
 
-test('should inherit full logger options from runtime to services', async t => {
+test('should inherit full logger options from runtime to a platformatic/service', async t => {
   process.env.LOG_DIR = path.join(tmpdir(), 'test-logs', Date.now().toString())
   process.env.PLT_RUNTIME_LOGGER_STDOUT = 1
   const file = path.join(process.env.LOG_DIR, 'service.log')
@@ -60,19 +60,6 @@ test('should inherit full logger options from runtime to services', async t => {
 
   const content = readFileSync(file, 'utf8')
   const logs = content.split('\n').filter(line => line.trim() !== '').map(line => JSON.parse(line))
-
-  // assert.ok(logs.find(log => log.level === 'INFO' &&
-  //   log.time.length === 24 && // isotime
-  //   log.name === 'service' &&
-  //   log.msg === 'Starting the service "app"...'))
-  // assert.ok(logs.find(log => log.level === 'INFO' &&
-  //   log.time.length === 24 && // isotime
-  //   log.name === 'service' &&
-  //   log.msg === 'Started the service "app"...'))
-  // assert.ok(logs.find(log => log.level === 'INFO' &&
-  //   log.time.length === 24 && // isotime
-  //   log.name === 'service' &&
-  //   log.msg.startsWith('Platformatic is now listening at http://127.0.0.1:')))
 
   assert.ok(logs.find(log => log.level === 'DEBUG' &&
     log.time.length === 24 && // isotime
@@ -96,8 +83,8 @@ test('should inherit full logger options from runtime to services', async t => {
 
   assert.ok(logs.find(log => {
     if (log.level === 'INFO' &&
-    log.time.length === 24 && // isotime
-    log.name === 'app') {
+      log.time.length === 24 && // isotime
+      log.name === 'app') {
       const msg = JSON.parse(log.msg)
       return msg.level === 'DEBUG' &&
         msg.time.length === 24 && // isotime
@@ -107,4 +94,34 @@ test('should inherit full logger options from runtime to services', async t => {
     }
     return false
   }))
+})
+
+test('should inherit full logger options from runtime to different services', async t => {
+  process.env.LOG_DIR = path.join(tmpdir(), 'test-logs', Date.now().toString())
+  process.env.PLT_RUNTIME_LOGGER_STDOUT = 1
+  const file = path.join(process.env.LOG_DIR, 'service.log')
+  const serviceRoot = path.join(__dirname, '..', 'fixtures', 'logger-options-all')
+
+  const app = await buildServer(path.join(serviceRoot, 'platformatic.json'))
+  t.after(async () => {
+    await app.close()
+  })
+  const url = await app.start()
+
+  await request(url, { path: '/logs' })
+  // wait for logger flush
+  await wait(2_000)
+
+  const content = readFileSync(file, 'utf8')
+  const logs = content.split('\n').filter(line => line.trim() !== '').map(line => JSON.parse(line))
+
+  // console.log(logs)
+
+  for (const t of ['composer', 'service', 'node']) {
+    assert.ok(logs.find(log => log.level === 'INFO' &&
+      log.time.length === 24 && // isotime
+      log.name === 'service' &&
+      log.msg === `Started the service "${t}"...`))
+  }
+
 })
