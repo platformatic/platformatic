e0cf7b2ad5e2af82e03f94fd7e0c330c1010f171
Simone Sanfratello <simone.sanfra@gmail.com>
2025-06-06 18:03:12 +0200

    fix: schema types

--- packages/db/config.d.ts
+++ packages/db/config.d.ts
@@ -605,7 +605,7 @@ export interface PlatformaticDB {
       maxELU?: number | string;
       maxHeapUsed?: number | string;
       maxHeapTotal?: number | string;
-      maxYoungGeneration?: number;
+      maxYoungGeneration?: number | string;
     };
     undici?: {
       agentOptions?: {
