dfaf0ae9f8a124054f197b22a6fe6dfefcb01b31
Simone Sanfratello <simone.sanfra@gmail.com>
2025-06-06 11:59:52 +0200

    fix: test schemas

--- packages/composer/test/schema.test.js
+++ packages/composer/test/schema.test.js
@@ -18,5 +18,5 @@ test('root schema file', async (t) => {
   const schemaFile = await readFile(schemaPath, 'utf8')
   const rootSchema = JSON.parse(schemaFile)
 
-  assert.deepEqual(rootSchema, schema)
+  assert.equal(JSON.stringify(rootSchema), JSON.stringify(schema))
 })
