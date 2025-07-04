93f4a9ac01fbd673e528fdd3d97472466ba5d355
Matteo Collina <hello@matteocollina.com>
2025-07-04 08:46:48 +0200

    Try a new port if the error code is EACCESS
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- packages/runtime/lib/start.js
+++ packages/runtime/lib/start.js
@@ -98,7 +98,7 @@ async function setupAndStartRuntime (config) {
     try {
       address = await runtime.start()
     } catch (err) {
-      if (err.code === 'EADDRINUSE') {
+      if (err.code === 'EADDRINUSE' || err.code === 'EACCESS') {
         // Get the actual port from the error message if original port was 0
         if (!port) {
           const mo = err.message.match(/ address already in use (.+)/)
