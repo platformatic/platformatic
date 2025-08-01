7652cdab80f03b0100b1ae991f5260342ac4a2d9
Roberto Bianchi <roberto.bianchi@spendesk.com>
2025-08-01 12:10:29 +0200

    feat(client cli): flip props (#4158)
    Signed-off-by: Roberto Bianchi <roberto.bianchi@spendesk.com>

--- packages/client-cli/test/watt-integration.test.mjs
+++ packages/client-cli/test/watt-integration.test.mjs
@@ -14,7 +14,7 @@ test('should create a client for a service inside a Watt runtime', async t => {
 
   await cp(join(import.meta.dirname, 'fixtures', 'watt'), dir, { recursive: true })
 
-  await execa('node', [join(import.meta.dirname, '..', 'cli.mjs'), '--name', 'client-1', '--runtime', 'first'], {
+  await execa('node', [join(import.meta.dirname, '..', 'cli.mjs'), '--name', 'client-1', '--runtime', 'first', '--skip-config-update', 'false'], {
     cwd: secondDir
   })
 
@@ -48,7 +48,9 @@ test('should create a client for a service inside a Watt runtime and update the 
       '--runtime',
       'first',
       '--config',
-      '../whatever.json'
+      '../whatever.json',
+      '--skip-config-update',
+      'false'
     ],
     { cwd: secondDir }
   )
