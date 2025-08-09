664604a914ee4b831b4112a4c9c88dfc741646d6
tawseefnabi <tawseefnabi9@gamil.com>
2025-08-09 06:52:27 +0530

    fix(node): allow boolean flags to be set to false in CLI
    Fix #2583 - Update the CLI to properly handle boolean flags like --plugin, --tests, and --localSchema so they can be explicitly set to false.
    
    - Added parseBoolean helper function for consistent boolean parsing
    - Updated CLI options to properly handle boolean values
    - Added help output to document available options
    - Added test script to verify boolean argument parsing

--- packages/node/lib/generator.js
+++ packages/node/lib/generator.js
@@ -6,7 +6,7 @@ import { basename, dirname, sep } from 'node:path'
 const indexFile = `
 import { createServer } from 'node:http'
 
-export function create() {
+export function create () {
   return createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'application/json', connection: 'close' })
     res.end(JSON.stringify({ hello: 'world' }))
