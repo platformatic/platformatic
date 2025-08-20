f29303e7f86f2560a9e47edc283d1e778a065732
Paolo Insogna <paolo@cowtech.it>
2025-08-20 17:04:42 +0200

    fixup
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/runtime/test/management-api/defaults.test.js
+++ packages/runtime/test/management-api/defaults.test.js
@@ -8,6 +8,33 @@ const { Client } = require('undici')
 const { createRuntime } = require('../helpers.js')
 const fixturesDir = join(__dirname, '..', '..', 'fixtures')
 
+// Make sure this test is run first otherwise it might fail on the CI
+test('should disable the management API if requested to', async t => {
+  const projectDir = join(fixturesDir, 'management-api-defaults')
+  const configFile = join(projectDir, 'platformatic-no-api.json')
+  const app = await createRuntime(configFile)
+
+  await app.start()
+
+  const client = new Client(
+    {
+      hostname: 'localhost',
+      protocol: 'http:'
+    },
+    {
+      socketPath: app.getManagementApiUrl(),
+      keepAliveTimeout: 10,
+      keepAliveMaxTimeout: 10
+    }
+  )
+
+  t.after(async () => {
+    await Promise.all([client.close(), app.close()])
+  })
+
+  await rejects(() => client.request({ method: 'GET', path: '/api/v1/config' }), { code: /ECONNREFUSED/ })
+})
+
 test('should enable the management API by default', async t => {
   const projectDir = join(fixturesDir, 'management-api-defaults')
   const configFile = join(projectDir, 'platformatic.json')
@@ -36,30 +63,4 @@ test('should enable the management API by default', async t => {
 
   const runtimeConfig = await body.json()
   strictEqual(runtimeConfig.entrypoint, 'main')
-})
-
-test('should disable the management API if requested to', async t => {
-  const projectDir = join(fixturesDir, 'management-api-defaults')
-  const configFile = join(projectDir, 'platformatic-no-api.json')
-  const app = await createRuntime(configFile)
-
-  await app.start()
-
-  const client = new Client(
-    {
-      hostname: 'localhost',
-      protocol: 'http:'
-    },
-    {
-      socketPath: app.getManagementApiUrl(),
-      keepAliveTimeout: 10,
-      keepAliveMaxTimeout: 10
-    }
-  )
-
-  t.after(async () => {
-    await Promise.all([client.close(), app.close()])
-  })
-
-  await rejects(() => client.request({ method: 'GET', path: '/api/v1/config' }), { code: /ECONNREFUSED/ })
 })
