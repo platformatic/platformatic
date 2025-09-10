882d11d6164324568ad4217534eb49030deba64c
mcollina <hello@matteocollina.com>
2025-09-10 14:44:48 +0000

    Bumped v2.75.2

--- packages/runtime/config.d.ts
+++ packages/runtime/config.d.ts
@@ -5,7 +5,7 @@
  * and run json-schema-to-typescript to regenerate this file.
  */
 
-export type HttpsSchemasPlatformaticDevPlatformaticRuntime2751Json = {
+export type HttpsSchemasPlatformaticDevPlatformaticRuntime2752Json = {
   [k: string]: unknown;
 } & {
   $schema?: string;
