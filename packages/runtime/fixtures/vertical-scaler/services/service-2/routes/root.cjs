9ce5de5168dc34737f27664dde3949464249ff23
Ivan Tymoshenko <ivan@tymoshenko.me>
2025-10-06 15:35:38 +0200

    use atomic-sleep

--- packages/runtime/fixtures/vertical-scaler/services/service-2/routes/root.cjs
+++ packages/runtime/fixtures/vertical-scaler/services/service-2/routes/root.cjs
@@ -1,13 +1,12 @@
 'use strict'
 
+const sleep = require('atomic-sleep')
+
 module.exports = async function (fastify) {
   fastify.post('/cpu-intensive', async (req) => {
     // Simulate a CPU intensive operation
     const timeout = req.body.timeout ?? 1000
-    const start = Date.now()
-
-    // eslint-disable-next-line no-empty
-    while (Date.now() - start < timeout) {}
+    sleep(timeout)
 
     return { status: 'ok' }
   })
