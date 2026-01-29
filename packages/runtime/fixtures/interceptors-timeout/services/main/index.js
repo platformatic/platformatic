1042b0640573163a7cde067d5a8e64850b97f387
Paolo Insogna <paolo@cowtech.it>
2026-01-29 12:39:17 +0100

    fixup
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/runtime/fixtures/interceptors-timeout/services/main/index.js
+++ packages/runtime/fixtures/interceptors-timeout/services/main/index.js
@@ -2,7 +2,6 @@ import { createServer } from 'node:http'
 
 let server
 
-const original = globalThis.platformatic.capability.getDispatchTarget
 globalThis.platformatic.capability.getDispatchTarget = function () {}
 
 export function create () {
