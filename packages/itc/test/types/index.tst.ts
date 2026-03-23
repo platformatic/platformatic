d241145a4e9b182f228d826f87824818d31c8a0f
mrazauskas <tom@mrazauskas.de>
2026-03-23 10:29:26 +0200

    fix: fix typings of `ITCConstructorOptions`
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

---
+++ packages/itc/test/types/index.tst.ts
@@ -0,0 +1,45 @@
+import type { EventEmitter } from 'node:events'
+import type { MessagePort } from 'node:worker_threads'
+import { expect, test } from 'tstyche'
+import { type Handler, ITC, type ITCConstructorOptions } from '../../lib/index.js'
+
+declare const port: MessagePort
+
+test('ITCConstructorOptions', () => {
+  expect<ITCConstructorOptions>().type.toBeAssignableFrom({ port, name: 'worker' })
+  expect<ITCConstructorOptions>().type.not.toBeAssignableFrom({})
+
+  const syncHandler: Handler = (data) => {
+    expect(data).type.toBe<any>()
+  }
+  const asyncHandler: Handler = (data) => Promise.resolve(
+    expect(data).type.toBe<any>()
+  )
+  const handlers: Record<string, Handler> = { syncHandler, asyncHandler }
+
+  expect<ITCConstructorOptions>().type.toBeAssignableFrom({ port, name: 'worker', handlers })
+  expect<ITCConstructorOptions>().type.toBeAssignableFrom({ port, name: 'worker', throwOnMissingHandler: true })
+})
+
+test('ITC', () => {
+  const itc = new ITC({ port, name: 'worker' })
+
+  expect(itc).type.toBeAssignableTo<EventEmitter>()
+
+  expect(itc.getHandler('message')).type.toBe<Handler | undefined>()
+
+  expect(itc.send('message', { key: 'value' })).type.toBe<Promise<any>>()
+  expect(itc.send('message', { key: 'value' }, { timeout: 1000 })).type.toBe<Promise<any>>()
+  expect(itc.send).type.not.toBeCallableWith(123, { key: 'value' })
+
+  expect(itc.notify('message', { key: 'value' })).type.toBe<void>()
+  expect(itc.notify('message', { key: 'value' }, { timeout: 1000 })).type.toBe<void>()
+  expect(itc.notify).type.not.toBeCallableWith(123, { key: 'value' })
+
+  expect(itc.handle('message', () => ({ key: 'value' }))).type.toBe<void>()
+  expect(itc.handle('message', async () => ({ key: 'value' }))).type.toBe<void>()
+  expect(itc.handle).type.not.toBeCallableWith(123, () => ({ key: 'value' }))
+
+  expect(itc.listen()).type.toBe<void>()
+  expect(itc.close()).type.toBe<void>()
+})
